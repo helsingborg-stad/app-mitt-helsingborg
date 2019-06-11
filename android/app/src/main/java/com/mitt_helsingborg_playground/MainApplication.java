93b9687839a8adea7a76a11854876f22d36d7fe3
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-06-11 11:19:00 +0200

    Build files

--- android/app/src/main/java/com/mitt_helsingborg_playground/MainApplication.java
+++ android/app/src/main/java/com/mitt_helsingborg_playground/MainApplication.java
@@ -3,6 +3,7 @@ package com.mitt_helsingborg_playground;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new RNScreensPackage(),
             new RNGestureHandlerPackage()
       );
