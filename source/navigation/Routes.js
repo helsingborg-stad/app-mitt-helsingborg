d4a02b561e87fcbe34e5d466f222a05b72caa38b
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-06-11 23:13:33 +0200

    Added login logic to routes

--- source/navigation/Routes.js
+++ source/navigation/Routes.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-native';
-import Home from "../components/Home";
-import Login from "../components/login/Login";
-import EditLogin from "../components/login/EditLogin";
+import { Route, Redirect } from 'react-router-native';
+import DashboardScreen from "../components/DashboardScreen";
+import LoginScreen from "../components/login/LoginScreen";
+import EditLoginScreen from "../components/login/EditLoginScreen";
 
 class Routes extends Component {
+
     render() {
+        const { isAuthed, user, appSettings, setPno, setUser } = this.props;
+
         return (
             <>
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/editlogin" component={EditLogin} />
+                <Route exact path="/" render={(props) =>
+                    (isAuthed ? (
+                        <DashboardScreen {...props}
+                            isAuthed={isAuthed}
+                            user={user}
+                            appSettings={appSettings}
+                        />
+                    ) : (<Redirect to="/login" />)
+                    )} />
+
+                <Route path="/login" render={(props) =>
+                    (!isAuthed ? (
+                        <LoginScreen {...props}
+                            isAuthed={isAuthed}
+                            user={user}
+                            appSettings={appSettings}
+                            setUser={setUser} />
+                    ) : (<Redirect to="/" />)
+                    )
+                } />
+
+                <Route path="/editlogin" render={(props) =>
+                    <EditLoginScreen {...props}
+                        isAuthed={isAuthed}
+                        user={user}
+                        appSettings={appSettings}
+                        setPno={setPno} />
+                } />
             </>
         );
     }
