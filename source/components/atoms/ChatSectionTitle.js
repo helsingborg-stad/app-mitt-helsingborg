abcdd0ef295e71c03d99aea861205bfa947877a4
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-09-18 15:53:06 +0200

    Added border to section title

--- source/components/atoms/ChatSectionTitle.js
+++ source/components/atoms/ChatSectionTitle.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ChatSectionTitle = ({ content }) =>
-    <Text style={styles.sectionTitle}>{content}</Text>
+    <View style={styles.sectionTitleWrapper}>
+        <Text style={styles.sectionTitle}>{content}</Text>
+    </View>;
+
 
 export default ChatSectionTitle;
 
 const styles = StyleSheet.create({
-    sectionTitle: {
+    sectionTitleWrapper: {
+        borderBottomColor: 'gainsboro',
+        borderBottomWidth: 1,
+        paddingBottom: 8,
         marginBottom: 16,
+    },
+    sectionTitle: {
         color: '#565656',
         fontSize: 15,
         fontWeight: 'bold',
