d26e3cae2862e9add391955ce10709865e852781
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-09-24 10:39:33 +0200

    Personal info action component

---
+++ source/components/molecules/PersonalInfoAction.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Button } from '../Components';
+
+class PersonalInfoAction extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onClick = () => {
+        this.props.addMessages(
+            MESSAGES
+        );
+
+        this.props.setActions(
+            ACTIONS
+        );
+    }
+
+    render() {
+        return <Button
+            value={'Mina personuppgifter?'}
+            onClick={this.onClick} />;
+    }
+};
+
+export default PersonalInfoAction;
+
+const MESSAGES = [
+    {
+        type: 'chatBubble',
+        modifiers: ['user'],
+        value: "Berätta mer om mina personuppgifter"
+    },
+    {
+        type: 'chatBubble',
+        modifiers: ['automated'],
+        value: "Info om personuppgifter kommer här"
+    }
+];
+
+const ACTIONS = [
+    {
+        type: 'chatSectionTitle',
+        value: "Lär dig mer om Mitt Helsingborg"
+    },
+    {
+        type: 'component',
+        value: 'acceptingOnLoginAction'
+    }
+]
