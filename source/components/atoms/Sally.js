bc39bc7e18c47265fa8dc3045d896d243b05cad0
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-09-24 10:41:05 +0200

    Sally icon component

---
+++ source/components/atoms/Sally.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Sally = () =>
+    <View style={styles.sally}>
+        <Text style={styles.sallyText}>Sally</Text>
+    </View>
+
+export default Sally;
+
+const styles = StyleSheet.create({
+    sally: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 40,
+        height: 40,
+        borderRadius: 40 / 2,
+        backgroundColor: 'darkorange',
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        shadowColor: '#000',
+        shadowOffset: { height: 3, width: 2 },
+    },
+    sallyText: {
+        color: 'white',
+    }
+});
