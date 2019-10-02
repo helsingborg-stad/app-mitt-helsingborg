812f5f18305059f0375fc9f7cae9469650b0dbd2
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-10-02 12:41:12 +0200

    Add ChatWrapper, ChatFooter & ChatBody

---
+++ source/components/atoms/ChatFooter.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+
+const ChatFooter = styled.View``;
+
+export default ChatFooter;
