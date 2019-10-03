dde7c41f7f06c1e4db933acfed8d1ecfde595f95
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-10-03 20:28:30 +0200

    Formatting

--- storybook/storybook.js
+++ storybook/storybook.js
@@ -1,17 +1,17 @@
 import { AppRegistry } from "react-native";
-    import { getStorybookUI, configure } from "@storybook/react-native";
-    import { loadStories } from "./storyLoader";
+import { getStorybookUI, configure } from "@storybook/react-native";
+import { loadStories } from "./storyLoader";
 
-    configure(() => {
-      loadStories();
-    }, module);
+configure(() => {
+  loadStories();
+}, module);
 
-    const StorybookUI = getStorybookUI({
-      port: 7007,
-      host: "localhost",
-      onDeviceUI: true,
-      resetStorybook: true,
-      shouldDisableKeyboardAvoidingView: true
-    });
+const StorybookUI = getStorybookUI({
+  port: 7007,
+  host: "localhost",
+  onDeviceUI: true,
+  resetStorybook: true,
+  shouldDisableKeyboardAvoidingView: true
+});
 
     export { StorybookUI as default };
