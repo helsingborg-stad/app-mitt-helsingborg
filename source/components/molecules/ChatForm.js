55853451efcd227f1b8cb770356d3ab770f605bb
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-10 12:27:37 +0200

    Added input actions to chat form component

--- source/components/molecules/ChatForm.js
+++ source/components/molecules/ChatForm.js
@@ -1,40 +1,57 @@
 import React from 'react';
-import { TextInput, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { TextInput, View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import ChatSubmitButton from '../atoms/ChatSubmitButton';
 
 const ChatForm = props => {
-    return (
-        <View style={styles.chatForm}>
-            <TextInput
-                {...props}
-                style={styles.chatTextInput}
-                value={props.inputValue}
-                onChangeText={props.changeHandler}
-                onSubmitEditing={props.submitHandler}
-            />
-            <ChatSubmitButton submitHandler={props.submitHandler} submitText={props.submitText} />
+
+  renderItem = ({ item, index }) => {
+    const { Component, componentProps } = item;
+    return <Component {...componentProps} addMessages={props.chat.addMessages} />;
+  }
+
+  return (
+    <View>
+      <View style={styles.chatForm}>
+        <TextInput
+          {...props}
+          style={styles.chatTextInput}
+          value={props.inputValue}
+          onChangeText={props.changeHandler}
+          onSubmitEditing={props.submitHandler}
+        />
+        <ChatSubmitButton submitHandler={props.submitHandler} submitText={props.submitText} />
+      </View>
+      {props.chat.inputActions &&
+        <View>
+          <FlatList
+            scrollEnabled={false}
+            data={props.chat.inputActions} renderItem={(item, index) => this.renderItem(item, index)}
+            keyExtractor={(item, index) => index.toString()}
+          />
         </View>
-    );
+      }
+    </View>
+  );
 }
 
 export default ChatForm;
 
 const styles = StyleSheet.create({
-    chatForm: {
-        flexDirection: 'row',
-        borderTopWidth: 0,
-        backgroundColor: 'rgb(236, 240, 241)',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingLeft: 16
-    },
-    chatTextInput: {
-        flex: 1,
-        height: 32,
-        padding: 8,
-        backgroundColor: 'rgb(236, 240, 241)',
-        borderColor: 'rgb(189, 195, 199)',
-        borderTopWidth: 0,
-        borderRadius: 4
-    },
+  chatForm: {
+    flexDirection: 'row',
+    borderTopWidth: 0,
+    backgroundColor: 'rgb(236, 240, 241)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 16
+  },
+  chatTextInput: {
+    flex: 1,
+    height: 32,
+    padding: 8,
+    backgroundColor: 'rgb(236, 240, 241)',
+    borderColor: 'rgb(189, 195, 199)',
+    borderTopWidth: 0,
+    borderRadius: 4
+  },
 });
