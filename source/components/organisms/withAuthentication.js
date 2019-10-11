9b245fc7c1dd663f5439a234269196e7e862f918
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-11 16:43:17 +0200

    Indent lines

--- source/components/organisms/withAuthentication.js
+++ source/components/organisms/withAuthentication.js
@@ -24,143 +24,143 @@ const USERKEY = 'user';
  *
  */
 const withAuthentication = (WrappedComponent) => {
-    return class WithAuthentication extends PureComponent {
-        constructor(props) {
-            super(props);
+  return class WithAuthentication extends PureComponent {
+    constructor(props) {
+      super(props);
 
-            this.state = {
-                user: {},
-                isLoading: false,
-                isBankidInstalled: false,
-            };
+      this.state = {
+        user: {},
+        isLoading: false,
+        isBankidInstalled: false,
+      };
+    }
+
+    componentDidMount() {
+      this._setUserAsync();
+      this._isBankidInstalled();
+    }
+
+    /**
+     * Make authenticate request and log in user
+     */
+    loginUser = async (personalNumber) => {
+      try {
+        this.setState({ isLoading: true });
+        // TODO: Safe to keep in production?
+        if (personalNumber === FAKE_PERSONAL_NUMBER && env.APP_ENV === 'development') {
+          return await this._fakeLogin(personalNumber);
         }
 
-        componentDidMount() {
-            this._setUserAsync();
-            this._isBankidInstalled();
+        const authResponse = await authorize(personalNumber);
+        if (authResponse.ok !== true) {
+          throw new Error(authResponse.data);
         }
 
-        /**
-         * Make authenticate request and log in user
-         */
-        loginUser = async (personalNumber) => {
-            try {
-                this.setState({ isLoading: true });
-                // TODO: Safe to keep in production?
-                if (personalNumber === FAKE_PERSONAL_NUMBER && env.APP_ENV === 'development') {
-                    return await this._fakeLogin(personalNumber);
-                }
-
-                const authResponse = await authorize(personalNumber);
-                if (authResponse.ok !== true) {
-                    throw new Error(authResponse.data);
-                }
-
-                try {
-                    const { user, accessToken } = authResponse.data;
-                    await Auth.logIn(user, accessToken);
-                } catch (error) {
-                    throw new Error("Login failed");
-                }
-
-                return authResponse.data;
-            } catch (error) {
-                // TODO: Add dynamic error messages
-                console.log("Authentication error: ", error);
-
-                throw error;
-            } finally {
-                this.setState({ isLoading: false });
-                // Reset cancel variable when done
-                resetCancel();
-            }
-        };
-
-        /**
-         * Cancel BankID login request
-         */
-        cancelLogin = async () => {
-            try {
-                cancelRequest();
-                return true;
-            } catch (error) {
-                console.log(error);
-            } finally {
-                // Clears access token and reset state
-                Auth.logOut();
-                this.setState({ isLoading: false });
-            }
-        };
-
-        /**
-         * Remove user from state, to be able to login as another user
-         */
-        resetUser = async () => {
-            this.setState({ user: {} });
-        };
-
-        /**
-         * Simulate login using fake user
-         */
-        _fakeLogin = async (personalNumber) => {
-            try {
-                const response = await bypassBankid(personalNumber);
-                const { user } = response.data;
-
-                try {
-                    await Auth.logIn(
-                        user,
-                        FAKE_TOKEN
-                    );
-                } catch (e) {
-                    // BY PASS REJECTION
-                    // throw "Login failed";
-                }
-
-                return { user, FAKE_TOKEN };
-            } catch (e) {
-                throw (e);
-            }
+        try {
+          const { user, accessToken } = authResponse.data;
+          await Auth.logIn(user, accessToken);
+        } catch (error) {
+          throw new Error("Login failed");
         }
 
-        /**
-         * Get user from async storage and add to state
-         */
-        _setUserAsync = async () => {
-            try {
-                const user = await StorageService.getData(USERKEY);
-                if (typeof user !== 'undefined' && user !== null) {
-                    this.setState({ user });
-                    // Login the user automatically
-                    // TODO: Fix if user is already logged in
-                    // this.loginUser(user.personalNumber);
-                }
-            } catch (error) {
-                console.log("Something went wrong", error);
-            }
-        };
+        return authResponse.data;
+      } catch (error) {
+        // TODO: Add dynamic error messages
+        console.log("Authentication error: ", error);
 
-        /**
-         * Check if BankID app is installed on this machine
-         */
-        _isBankidInstalled = async () => {
-            const isBankidInstalled = await canOpenUrl('bankid:///');
-
-            if (isBankidInstalled) {
-                this.setState({ isBankidInstalled: true });
-            }
-        };
-
-        render() {
-            const { state, props, loginUser, cancelLogin, resetUser } = this;
-            const instanceMethods = { loginUser, cancelLogin, resetUser };
-            const injectProps = { ...instanceMethods, ...state };
-
-            return (
-                <WrappedComponent authentication={{ ...injectProps }} {...props} />
-            )
-        }
+        throw error;
+      } finally {
+        this.setState({ isLoading: false });
+        // Reset cancel variable when done
+        resetCancel();
+      }
     };
+
+    /**
+     * Cancel BankID login request
+     */
+    cancelLogin = async () => {
+      try {
+        cancelRequest();
+        return true;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        // Clears access token and reset state
+        Auth.logOut();
+        this.setState({ isLoading: false });
+      }
+    };
+
+    /**
+     * Remove user from state, to be able to login as another user
+     */
+    resetUser = async () => {
+      this.setState({ user: {} });
+    };
+
+    /**
+     * Simulate login using fake user
+     */
+    _fakeLogin = async (personalNumber) => {
+      try {
+        const response = await bypassBankid(personalNumber);
+        const { user } = response.data;
+
+        try {
+          await Auth.logIn(
+            user,
+            FAKE_TOKEN
+          );
+        } catch (e) {
+          // BY PASS REJECTION
+          // throw "Login failed";
+        }
+
+        return { user, FAKE_TOKEN };
+      } catch (e) {
+        throw (e);
+      }
+    }
+
+    /**
+     * Get user from async storage and add to state
+     */
+    _setUserAsync = async () => {
+      try {
+        const user = await StorageService.getData(USERKEY);
+        if (typeof user !== 'undefined' && user !== null) {
+          this.setState({ user });
+          // Login the user automatically
+          // TODO: Fix if user is already logged in
+          // this.loginUser(user.personalNumber);
+        }
+      } catch (error) {
+        console.log("Something went wrong", error);
+      }
+    };
+
+    /**
+     * Check if BankID app is installed on this machine
+     */
+    _isBankidInstalled = async () => {
+      const isBankidInstalled = await canOpenUrl('bankid:///');
+
+      if (isBankidInstalled) {
+        this.setState({ isBankidInstalled: true });
+      }
+    };
+
+    render() {
+      const { state, props, loginUser, cancelLogin, resetUser } = this;
+      const instanceMethods = { loginUser, cancelLogin, resetUser };
+      const injectProps = { ...instanceMethods, ...state };
+
+      return (
+        <WrappedComponent authentication={{ ...injectProps }} {...props} />
+      )
+    }
+  };
 }
 
 export default withAuthentication;
