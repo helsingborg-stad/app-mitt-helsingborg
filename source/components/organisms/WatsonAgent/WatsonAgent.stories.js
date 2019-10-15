750c0f9da0700cf85fd9f54eef9162aeac63f0d0
Teddy Andersson <teddyandersson95@gmail.com>
2019-10-15 11:23:03 +0200

    Removed unused classes
    Removed classes that was unused in the stories.

--- source/components/organisms/WatsonAgent/WatsonAgent.stories.js
+++ source/components/organisms/WatsonAgent/WatsonAgent.stories.js
@@ -6,13 +6,6 @@ import EventHandler, { EVENT_USER_MESSAGE } from '../../../helpers/EventHandler'
 import withChatForm from '../withChatForm';
 
 import StoryWrapper from '../../molecules/StoryWrapper';
-import ChatForm from '../../molecules/ChatForm';
-import ChatMessages from '../../molecules/ChatMessages';
-
-import ChatBody from '../../atoms/ChatBody';
-import ChatWrapper from '../../atoms/ChatWrapper';
-import ChatFooter from '../../atoms/ChatFooter';
-import ChatBubble from '../../atoms/ChatBubble';
 
 import WatsonAgent from './WatsonAgent';
 import FormAgent from '../FormAgent/FormAgent';
@@ -58,79 +51,6 @@ class ParrotAgent extends Component {
 
     render() {
         return null;
-    }
-}
-
-class ChatScreen extends Component {
-    state = {
-        messages: [],
-        ChatUserInput: withChatForm(ChatForm),
-        ChatAgent: ParrotAgent,
-    };
-
-    addMessages = (objects) => {
-        const array = Array.isArray(objects) ? objects : [objects];
-
-        this.setState((state, props) => {
-            let { messages } = state;
-            array.forEach(object => { messages.push(object) });
-
-            return {messages};
-        }, () => {
-            const lastMsg = this.state.messages.slice(-1)[0].componentProps;
-
-            if (lastMsg.modifiers[0] === 'human') {
-                // console.log(lastMsg.content);
-                EventHandler.dispatch(EVENT_USER_MESSAGE, lastMsg.content);
-            }
-
-        });
-    };
-
-    switchAgent = (agent, props = {}) => {
-        switch (agent) {
-            case "form":
-                this.setState({
-                    ChatAgent: <FormAgent {...props}/>
-                });
-                break;
-            default:
-                this.setState({
-                    ChatAgent: WatsonAgent
-                });
-                break;
-        }
-    };
-
-    switchUserInput = (UserInputComponent) => {
-        this.setState({
-            ChatUserInput: UserInputComponent
-        });
-    };
-
-    render() {
-        const { messages, ChatAgent, ChatUserInput } = this.state;
-        const { addMessages, switchAgent } = this;
-
-        const instanceMethods = { addMessages, switchAgent };
-
-        return (
-            <StoryWrapper>
-                <ChatWrapper keyboardVerticalOffset={24} >
-                    {ChatAgent ?
-                        <ChatAgent chat={{...instanceMethods, ...this.state}} />
-                    : null}
-                    <ChatBody>
-                        <ChatMessages messages={messages} />
-                    </ChatBody>
-                    <ChatFooter>
-                        {ChatUserInput ?
-                            <ChatUserInput chat={{...instanceMethods, ...this.state}} />
-                        : null}
-                    </ChatFooter>
-                </ChatWrapper>
-            </StoryWrapper>
-        );
     }
 }
 
