aa50e63f7f2a928bb90b00496dd84a8cee71f4af
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2019-10-16 12:04:46 +0200

    Added dependencies for navigation tabs.

--- android/app/src/main/java/com/mitt_helsingborg_playground/MainApplication.java
+++ android/app/src/main/java/com/mitt_helsingborg_playground/MainApplication.java
@@ -3,6 +3,7 @@ package com.mitt_helsingborg_playground;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new VectorIconsPackage(),
             new ReactNativePushNotificationPackage(),
             new AsyncStoragePackage(),
