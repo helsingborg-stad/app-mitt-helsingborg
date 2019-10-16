2ab8f988e89babd08533683e75c4de6ab06ece2d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-10-16 16:54:18 +0200

    Implement LoginAgent and show tabs when user is logged in

--- source/components/screens/ChatScreen.js
+++ source/components/screens/ChatScreen.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
 import ScreenWrapper from '../molecules/ScreenWrapper'
 import Chat from '../organisms/Chat';
-import ExampleAgent from '../organisms/ExampleAgent';
+import LoginAgent from '../organisms/LoginAgent';
 import withChatForm from '../organisms/withChatForm';
 import ChatForm from '../molecules/ChatForm';
+import styled from 'styled-components/native';
 class ChatScreen extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        tabBarVisible: navigation.state.params.tabBarVisible
+    });
+
+    toggleTabs = () => {
+        const {navigation} = this.props;
+        navigation.setParams({tabBarVisible: navigation.getParam('tabBarVisible') === true ? false : true});
+    }
+
     render() {
         return (
-            <ScreenWrapper>
-                <Chat ChatAgent={ExampleAgent} ChatUserInput={withChatForm(ChatForm)} />       
-            </ScreenWrapper>
+            <ChatScreenWrapper>
+                <Chat 
+                    ChatAgent={props => (<LoginAgent {...props} 
+                    onUserLogin={this.toggleTabs} />)} 
+                    ChatUserInput={false} 
+                />       
+            </ChatScreenWrapper>
         )
     }
 }
+
+const ChatScreenWrapper = styled(ScreenWrapper)`
+    padding-left: 0;
+    padding-right: 0;
+`;
 
 export default ChatScreen;
