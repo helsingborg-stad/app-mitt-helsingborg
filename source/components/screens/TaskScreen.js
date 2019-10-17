e66e75ee12f6d1de7411db2c36252adc318ed94d
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-17 16:31:19 +0200

    Added header to taskscreen

--- source/components/screens/TaskScreen.js
+++ source/components/screens/TaskScreen.js
@@ -1,23 +1,59 @@
 import React, { Component } from 'react';
-import ScreenWrapper from '../molecules/ScreenWrapper';
+import styled from 'styled-components/native';
+import { View, ScrollView } from 'react-native';
 import GroupedList from '../molecules/GroupedList';
+import Header from '../molecules/Header';
+import StorageService, { USER_KEY } from '../../services/StorageService';
 
 class TaskScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {}
+        };
+    }
+
+    componentDidMount() {
+        this.getUser();
+    }
+
+    getUser = async () => {
+        try {
+            const user = await StorageService.getData(USER_KEY);
+            this.setState({ user });
+        } catch (error) {
+            console.log("User not found", error);
+        }
+    };
+
     render() {
+        const { givenName } = this.state.user;
+
         return (
-            <ScreenWrapper>
-                <GroupedList
-                    heading="Avslutade"
-                    icon="chevron-right"
-                    iconPosition="right"
-                    items={COMPLETED_TASKS}
+            <View>
+                <Header
+                    title="Mitt Helsingborg"
+                    message={givenName ? `Hej ${givenName}!` : 'Hej!'}
+                    themeColor="purple"
                 />
-            </ScreenWrapper>
+                <Container>
+                    <GroupedList
+                        heading="Avslutade"
+                        icon="chevron-right"
+                        iconPosition="right"
+                        items={COMPLETED_TASKS}
+                    />
+                </Container>
+            </View>
         );
     }
 }
 
 export default TaskScreen;
+
+const Container = styled(ScrollView)`
+    padding: 16px;
+`;
 
 const COMPLETED_TASKS = [
     {
