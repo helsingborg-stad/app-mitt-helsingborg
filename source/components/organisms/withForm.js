519441876bbbfae7f65343c99c6f5ee7c4909dfa
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-10-23 09:59:14 +0200

    Allow override of inputValue in submit

--- source/components/organisms/withForm.js
+++ source/components/organisms/withForm.js
@@ -10,16 +10,17 @@ const withForm = (WrappedComponent, onSubmit) => {
             this.setState({inputValue: value});
         }
     
-        submitHandler = () => {
+        submitHandler = (argValue) => {
             const { inputValue } = this.state;
-    
-            if (inputValue.length <= 0) {
+            const value = typeof argValue === 'string' ? argValue : inputValue;
+
+            if (value.length <= 0) {
                 return;
             }
     
             this.setState({ inputValue: '' }, () => {
                 if (typeof onSubmit === 'function') {
-                    onSubmit(inputValue);
+                    onSubmit(value);
                 }
             });
         }
