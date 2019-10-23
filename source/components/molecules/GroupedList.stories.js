62882e647ff007d1446e6d24b604289365aad923
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-23 11:31:10 +0200

    Updated grouped list story

--- source/components/molecules/GroupedList.stories.js
+++ source/components/molecules/GroupedList.stories.js
@@ -4,50 +4,41 @@ import StoryWrapper from './StoryWrapper';
 import GroupedList from './GroupedList';
 
 storiesOf('Grouped list', module)
-  .add('Icon right', props => (
-    <StoryWrapper {...props}>
-      <GroupedList
-        heading="Avslutade"
-        icon="chevron-right"
-        iconPosition="right"
-        items={COMPLETED_TASKS}
-      />
-    </StoryWrapper>
-  )).add('Icon left', props => (
-    <StoryWrapper {...props}>
-      <GroupedList
-        heading="Kommande"
-        icon="perm-contact-calendar"
-        iconPosition="left"
-        items={COMPLETED_TASKS}
-      />
-    </StoryWrapper>
-  ))
+    .add('Default', props => (
+        <StoryWrapper {...props}>
+            <GroupedList
+                heading="Avslutade"
+                items={COMPLETED_TASKS}
+            />
+        </StoryWrapper>
+    ))
 
 const COMPLETED_TASKS = [
-  {
-    heading: 'TISDAG 3 NOVEMBER',
-    data: [
-      {
-        id: 'bd7acbea',
-        title: 'Skolskjuts',
-        text: 'Skolskjuts beställd',
-      }
-    ]
-  },
-  {
-    heading: 'FREDAG 10 NOVEMBER',
-    data: [
-      {
-        id: '3ac68afc',
-        title: 'Avfallshämtning',
-        text: 'Avfallshämtning beställd',
-      },
-      {
-        id: '58694a0f',
-        title: 'Bygglov',
-        text: 'Bygglov godkänt',
-      },
-    ]
-  },
+    {
+        heading: 'TISDAG 3 NOVEMBER',
+        data: [
+            {
+                id: 'bd7acbea',
+                title: 'Skolskjuts',
+                text: 'Skolskjuts beställd',
+            }
+        ]
+    },
+    {
+        heading: 'FREDAG 10 NOVEMBER',
+        data: [
+            {
+                id: '3ac68afc',
+                title: 'Avfallshämtning',
+                text: 'Avfallshämtning beställd',
+                iconName: 'perm-contact-calendar'
+            },
+            {
+                id: '58694a0f',
+                title: 'Bygglov',
+                text: 'Bygglov godkänt',
+                iconName: 'perm-contact-calendar'
+            },
+        ]
+    },
 ];
