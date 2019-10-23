45638db730fda5ac953b0b60d965516b0b8eb7d8
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-23 19:38:38 +0200

    Renamed list story

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -12,11 +12,11 @@ function loadStories() {
   require('../source/components/atoms/Input.stories');
   require('../source/components/atoms/Select.stories');
   require('../source/components/atoms/Text.stories');
-  require('../source/components/molecules/GroupedList.stories');
+  require('../source/components/molecules/Lists.stories');
   require('../source/components/organisms/FormAgent/FormAgent.stories');
   require('../source/components/organisms/LoginAgent.stories');
   require('../source/components/organisms/WatsonAgent.stories');
-  
+
 }
 
 const stories = [
@@ -27,11 +27,11 @@ const stories = [
   '../source/components/atoms/Input.stories',
   '../source/components/atoms/Select.stories',
   '../source/components/atoms/Text.stories',
-  '../source/components/molecules/GroupedList.stories',
+  '../source/components/molecules/List.stories',
   '../source/components/organisms/FormAgent/FormAgent.stories',
   '../source/components/organisms/LoginAgent.stories',
   '../source/components/organisms/WatsonAgent.stories',
-  
+
 ];
 
 module.exports = {
