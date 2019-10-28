846385fa85ab718d99ad66a0db8e2ecd77329a6e
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-28 16:28:08 +0100

    Added keyboard event listeners to form

--- source/components/organisms/withForm.js
+++ source/components/organisms/withForm.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 
 const withForm = (WrappedComponent, onSubmit) => {
     return class WithForm extends Component {
         state = {
-            inputValue: ''  
+            inputValue: '',
+            isFocused: false,
         };
-    
+
         changeHandler = value => {
             this.setState({inputValue: value});
         }
-    
+
+        componentDidMount() {
+            this.keyboardWillShowListener = Keyboard.addListener(
+                'keyboardWillShow',
+                () => this.setState({ isFocused: true }),
+            );
+            this.keyboardWillHideListener = Keyboard.addListener(
+                'keyboardWillHide',
+                () => this.setState({ isFocused: false }),
+            );
+        }
+
+        componentWillUnmount() {
+            this.keyboardWillShowListener.remove();
+            this.keyboardWillHideListener.remove();
+        }
+
         submitHandler = (argValue) => {
             const { inputValue } = this.state;
             const value = typeof argValue === 'string' ? argValue : inputValue;
@@ -17,14 +35,14 @@ const withForm = (WrappedComponent, onSubmit) => {
             if (value.length <= 0) {
                 return;
             }
-    
+
             this.setState({ inputValue: '' }, () => {
                 if (typeof onSubmit === 'function') {
                     onSubmit(value);
                 }
             });
         }
-    
+
         render() {
             const { submitHandler, changeHandler } = this;
             const instanceMethods = { submitHandler, changeHandler };
