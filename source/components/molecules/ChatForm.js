996e97da31006ab17589344e7538a12394150684
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-28 16:29:01 +0100

    Hide keyboard button

--- source/components/molecules/ChatForm.js
+++ source/components/molecules/ChatForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { TextInput, View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { Keyboard, TextInput, View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import Input from '../atoms/Input';
 import styled from 'styled-components/native';
 import Button from '../atoms/Button';
 import Icon from '../atoms/Icon';
 
 const ChatForm = props => {
-    const { style, renderFooter, submitHandler, changeHandler, inputValue } = props;
+    const { style, renderFooter, submitHandler, changeHandler, inputValue, isFocused } = props;
     const formProps = {submitHandler, changeHandler, inputValue};
 
     const children = props.children
@@ -16,19 +16,25 @@ const ChatForm = props => {
             }
 
             if (child.type === Input) {
-                return React.createElement(child.type, {onChangeText: changeHandler, value: inputValue, onSubmitEditing: submitHandler, ...child.props }) 
+                return React.createElement(child.type, {onChangeText: changeHandler, value: inputValue, onSubmitEditing: submitHandler, ...child.props })
             }
 
             return React.createElement(child.type, {form: {...formProps}, ...child.props});
-        }) 
+        })
         : false;
-
 
     return (
         <ChatFormWrapper>
             <ChatFormBody style={style}>
+
+                {isFocused &&
+                    <ChatFormButton onClick={Keyboard.dismiss} block={false} z={0}>
+                        <Icon name="keyboard" />
+                    </ChatFormButton>
+                }
+
                 {
-                    children 
+                    children
                     ? children
                     : <Input
                         value={inputValue}
@@ -36,12 +42,13 @@ const ChatForm = props => {
                         onSubmitEditing={submitHandler}
                         placeholder={'Skriv något... '}
                         keyboardType={'default'}
+                        focus
                     />
                 }
-                
+
                 <Button onClick={submitHandler} z={0}>
                     {
-                        props.submitText 
+                        props.submitText
                         ? <Text>{props.submitText}</Text>
                         : <Icon name="send"/>
                     }
@@ -73,3 +80,8 @@ const ChatFormBody = styled.View`
   margin-right: 16px;
   flex-direction: row;
 `;
+const ChatFormButton = styled(Button)`
+    padding: 0px 16px 0px 0px;
+    background: transparent;
+`;
+
