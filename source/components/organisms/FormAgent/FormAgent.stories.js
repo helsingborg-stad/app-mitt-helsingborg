8889087c8cc5e3e039f4b1d9288c0075ec7b0fa9
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-10-29 14:25:19 +0100

    Fix input focus flicker when navigating story

--- source/components/organisms/FormAgent/FormAgent.stories.js
+++ source/components/organisms/FormAgent/FormAgent.stories.js
@@ -6,10 +6,52 @@ import StoryWrapper from '../../molecules/StoryWrapper';
 import Chat from '../Chat';
 import FormAgent from './';
 
+import EventHandler, { EVENT_USER_MESSAGE } from '../../../helpers/EventHandler';
+
+import ChatBubble from '../../atoms/ChatBubble';
+
+class FormAgentInitiator extends Component {
+    componentDidMount() {
+        const { chat } = this.props;
+
+        chat.addMessages({
+            Component: ChatBubble,
+            componentProps: {
+                content: 'Hej! Vill du prova FormAgent?',
+                modifiers: ['automated'],
+            }
+        });
+
+        chat.switchInput({
+            type: 'radio',
+            options: [
+                {
+                    value: 'Ja, starta FormAgent nu!',
+                },
+            ],
+        });
+
+        EventHandler.subscribe(EVENT_USER_MESSAGE, (message) => this.handleMessage(message));
+    }
+
+    componentWillUnmount() {
+        EventHandler.unSubscribe(EVENT_USER_MESSAGE);
+    }
+
+    handleMessage = (message) => {
+        const { chat } = this.props;
+        chat.switchAgent(props => (<FormAgent {...props} formId={1} />));
+    };
+
+    render() {
+        return null;
+    }
+}
+
 
 storiesOf('Chat', module)
     .add('Form agent', () => (
         <StoryWrapper>
-            <Chat ChatAgent={props => (<FormAgent {...props} formId={1} />)} />
+            <Chat ChatAgent={FormAgentInitiator} />
         </StoryWrapper>
     ));
