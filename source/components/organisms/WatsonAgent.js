afa78b108ec42b07b87e44ad0f10c412d5564caa
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-10-31 13:08:52 +0100

    Implemented option buttons to watson responses

--- source/components/organisms/WatsonAgent.js
+++ source/components/organisms/WatsonAgent.js
@@ -3,9 +3,12 @@ import env from 'react-native-config';
 import EventHandler, { EVENT_USER_MESSAGE } from '../../helpers/EventHandler';
 import { sendChatMsg } from '../../services/ChatFormService';
 import ChatBubble from '../atoms/ChatBubble';
-import { Alert } from "react-native";
+import Button from '../atoms/Button';
+import Text from '../atoms/Text';
+import Icon from '../atoms/Icon';
+import { Alert, View } from "react-native";
 import FormAgent from "./FormAgent";
-import StorageService, {USER_KEY} from "../../services/StorageService";
+import StorageService, { USER_KEY } from "../../services/StorageService";
 
 let firstRun = true;
 let conversationId;
@@ -18,7 +21,7 @@ export default class WatsonAgent extends Component {
     componentDidMount() {
         const { chat } = this.props;
 
-        StorageService.getData(USER_KEY).then(({name}) => {
+        StorageService.getData(USER_KEY).then(({ name }) => {
             chat.addMessages({
                 Component: ChatBubble,
                 componentProps: {
@@ -39,9 +42,32 @@ export default class WatsonAgent extends Component {
 
         EventHandler.subscribe(EVENT_USER_MESSAGE, (message) => this.handleHumanChatMessage(message));
     }
+
     componentWillUnmount() {
         EventHandler.unSubscribe(EVENT_USER_MESSAGE);
     }
+
+    /**
+     * Parse metadata found in text strings
+     * @param {string} value
+     */
+    captureMetaData(value) {
+        if (typeof value !== 'string') {
+            return undefined;
+        }
+
+        const match = /{([a-z0-9.,_\'"\[\]:{}]+)}/g.exec(value);
+        let meta = match && typeof match[1] !== 'undefined' ? match[1] : undefined;
+
+        try {
+            meta = JSON.parse(meta);
+        } catch (error) {
+            // Cannot parse json
+        }
+
+        return meta;
+    }
+
     handleHumanChatMessage = async (message) => {
         const { chat } = this.props;
         const workspaceId = env.WATSON_WORKSPACEID;
@@ -50,8 +76,10 @@ export default class WatsonAgent extends Component {
         }
         else {
             let responseText;
+            let options = [];
             try {
                 await sendChatMsg(workspaceId, message, conversationId).then((response) => {
+                    console.log(response);
                     const responseGeneric = response.data.attributes.output.generic;
 
                     if (firstRun) {
@@ -63,11 +91,16 @@ export default class WatsonAgent extends Component {
                     responseGeneric.forEach(elem => {
                         if (elem.response_type === 'text') {
                             responseText = elem.text;
+                        }
 
-                            if (responseText.indexOf('[agent:forms]') !== -1) {
-                                this.setState({disableAgent: true});
-                                chat.switchAgent(props => (<FormAgent {...props} formId={1} />));
-                            }
+                        if (elem.response_type === 'option' && elem.options) {
+                            elem.options.forEach((option) => {
+                                const action = this.captureMetaData(option.value.input.text);
+                                options.push({
+                                    value: option.label,
+                                    action: action
+                                })
+                            });
                         }
                     });
                 });
@@ -85,6 +118,20 @@ export default class WatsonAgent extends Component {
                         modifiers: ['automated'],
                     }
                 });
+
+                const inputArray = [{
+                    type: 'text',
+                    placeholder: 'Skriv något...',
+                }];
+
+                if (options.length > 0) {
+                    inputArray.push({
+                        type: 'radio',
+                        options,
+                    })
+                }
+
+                chat.switchInput(inputArray);
             }
         }
     };
