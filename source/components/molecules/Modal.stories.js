273a5c09efca374c48c86d2651ffa63788e8a457
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-11-06 11:44:12 +0100

    Feature/modal (#55)
    * Base component for modals
    
    * Added react-native-modal package
    
    * H4 heading size
    
    * Added red and green heading colors
    
    * Fixed swipeable and scrollable modal
    
    * Updated modal story
    
    * Updated storyloader
    
    * Updated packages
    
    * Minor improvements

---
+++ source/components/molecules/Modal.stories.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { storiesOf } from '@storybook/react-native';
+import styled from 'styled-components/native';
+import StoryWrapper from '../molecules/StoryWrapper';
+import Modal from './Modal';
+import Button from '../atoms/Button';
+import Text from '../atoms/Text';
+
+class ModalExample extends Component {
+    state = {
+        modal: {
+            visible: false,
+            heading: '',
+            content: ''
+        }
+    };
+
+    changeModal(visible, heading = '', content = '') {
+        this.setState({
+            modal: {
+                visible: visible,
+                heading,
+                content
+            }
+        });
+    }
+
+    render() {
+        const { modal } = this.state;
+        const { visible, heading, content } = modal;
+
+        return (
+            <View>
+                <Modal
+                    color="purple"
+                    visible={visible}
+                    heading={heading}
+                    content={content}
+                    changeModal={(visible) => this.changeModal(visible)}
+                />
+
+                <ModalButton
+                    color={'purple'}
+                    onClick={() => this.changeModal(!visible, 'Modal one', 'Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. \n\nSociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n\nSociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  \n\nSociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing.')}>
+                    <Text>Show modal</Text>
+                </ModalButton>
+
+                <ModalButton
+                    color={'blue'}
+                    onClick={() => this.changeModal(!visible, 'Modal two', 'Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit.')}>
+                    <Text>Show modal</Text>
+                </ModalButton>
+
+            </View>
+        );
+    }
+}
+
+storiesOf('Modal', module)
+    .add('default', () => (
+        <StoryWrapper>
+            <ModalExample />
+        </StoryWrapper>
+    ));
+
+const ModalButton = styled(Button)`
+    margin-top: 24px;
+`;
