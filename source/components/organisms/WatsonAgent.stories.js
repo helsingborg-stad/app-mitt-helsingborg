cea8cd64d7fef2c3d9a90cc396f4474010745454
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-11-11 14:21:08 +0100

    Clean up watson story

--- source/components/organisms/WatsonAgent.stories.js
+++ source/components/organisms/WatsonAgent.stories.js
@@ -1,85 +1,27 @@
 import React, { Component } from 'react';
 import env from 'react-native-config';
 import { storiesOf } from '@storybook/react-native';
+import styled from 'styled-components/native';
 
 import EventHandler, { EVENT_USER_MESSAGE } from '../../helpers/EventHandler';
 
 import withChatForm from './withChatForm';
+import Chat from './Chat';
 
 import StoryWrapper from '../molecules/StoryWrapper';
-import ChatForm from '../molecules/ChatFormDeprecated';
-import ChatMessages from '../molecules/ChatMessages';
+import InputForm from '../molecules/InputForm';
 
-import ChatBody from '../atoms/ChatBody';
-import ChatWrapper from '../atoms/ChatWrapper';
-import ChatFooter from '../atoms/ChatFooter';
-import ParrotAgent from "./ParrotAgent";
-
-class ChatScreen extends Component {
-    state = {
-        messages: [],
-        ChatUserInput: withChatForm(ChatForm),
-        ChatAgent: ParrotAgent,
-    };
-
-    addMessages = (objects) => {
-        const array = Array.isArray(objects) ? objects : [objects];
-
-        this.setState((state, props) => {
-            let { messages } = state;
-            array.forEach(object => { messages.push(object) });
-
-            return {messages};
-        }, () => {
-            const lastMsg = this.state.messages.slice(-1)[0].componentProps;
-
-            if (lastMsg.modifiers[0] === 'user') {
-                // console.log(lastMsg.content);
-                EventHandler.dispatch(EVENT_USER_MESSAGE, lastMsg.content);
-            }
-
-        });
-    };
-
-    switchAgent = (AgentComponent) => {
-        this.setState({
-            ChatAgent: AgentComponent
-        });
-    };
-
-    switchUserInput = (UserInputComponent) => {
-        this.setState({
-            ChatUserInput: UserInputComponent
-        });
-    };
-
-    render() {
-        const { messages, ChatAgent, ChatUserInput } = this.state;
-        const { addMessages, switchAgent } = this;
-
-        const instanceMethods = { addMessages, switchAgent };
-
-        return (
-            <StoryWrapper>
-                <ChatWrapper keyboardVerticalOffset={24} >
-                    {ChatAgent ?
-                        <ChatAgent chat={{...instanceMethods, ...this.state}} />
-                    : null}
-                    <ChatBody>
-                        <ChatMessages messages={messages} />
-                    </ChatBody>
-                    <ChatFooter>
-                        {ChatUserInput ?
-                            <ChatUserInput chat={{...instanceMethods, ...this.state}} />
-                        : null}
-                    </ChatFooter>
-                </ChatWrapper>
-            </StoryWrapper>
-        );
-    }
-}
+import ParrotAgent from './ParrotAgent';
 
 storiesOf('Chat', module)
     .add('Watson agent', () => (
-        <ChatScreen />
+        <ModifiedStoryWrapper>
+            <Chat ChatAgent={ParrotAgent} ChatUserInput={withChatForm(InputForm)} />
+        </ModifiedStoryWrapper>
     ));
+
+const ModifiedStoryWrapper = styled(StoryWrapper)`
+    padding-left: 0;
+    padding-right: 0;
+  `;
+
