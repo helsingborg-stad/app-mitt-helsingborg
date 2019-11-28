4e15302b5984f43ec9fe1725f73c195f447bdae9
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2019-11-28 14:15:26 +0100

    Removed import for task screen story

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -20,8 +20,6 @@ function loadStories() {
   require('../source/components/organisms/LoginAgent.stories');
   require('../source/components/organisms/NavigationTabs.stories');
   require('../source/components/organisms/WatsonAgent.stories');
-  require('../source/components/screens/TaskDetailScreen.stories');
-  
 }
 
 const stories = [
@@ -40,8 +38,6 @@ const stories = [
   '../source/components/organisms/LoginAgent.stories',
   '../source/components/organisms/NavigationTabs.stories',
   '../source/components/organisms/WatsonAgent.stories',
-  '../source/components/screens/TaskDetailScreen.stories',
-  
 ];
 
 module.exports = {
