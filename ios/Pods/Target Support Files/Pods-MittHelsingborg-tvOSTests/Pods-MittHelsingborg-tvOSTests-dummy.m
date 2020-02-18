17b6164806c463db03b30df55f1e53134c051bd6
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-02-18 11:37:09 +0100

    iOS project added.

---
+++ ios/Pods/Target Support Files/Pods-MittHelsingborg-tvOSTests/Pods-MittHelsingborg-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MittHelsingborg_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_MittHelsingborg_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/react-native-network-info/react-native-network-info-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_react_native_network_info : NSObject
+@end
+@implementation PodsDummy_react_native_network_info
+@end
