2863d400411c97a0b2b4c88e8f7fd7c44e553289
Ehsan Zilaei <zzilaei@gmail.com>
2020-02-19 15:13:43 +0100

    Installed storybook loader and added package.json script storybook. (#108)

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -1,41 +1,40 @@
-/* eslint-disable global-require */
 // Auto-generated file created by react-native-storybook-loader
 // Do not edit.
 //
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../source/components/atoms/Button.stories');
-  require('../source/components/atoms/ChatBubble.stories');
-  require('../source/components/atoms/Heading.stories');
-  require('../source/components/atoms/Icon.stories');
-  require('../source/components/atoms/Input.stories');
-  require('../source/components/atoms/Markdown.stories');
-  require('../source/components/atoms/Select.stories');
-  require('../source/components/atoms/Text.stories');
-  require('../source/components/molecules/DateTimePickerForm.stories');
-  require('../source/components/molecules/Lists.stories');
-  require('../source/components/molecules/Modal.stories');
-  require('../source/components/organisms/FormAgent/FormAgent.stories');
-  require('../source/components/organisms/NavigationTabs.stories');
-  require('../source/components/organisms/WatsonAgent.stories');
+	require('../source/components/atoms/Button.stories');
+	require('../source/components/atoms/ChatBubble.stories');
+	require('../source/components/atoms/Heading.stories');
+	require('../source/components/atoms/Icon.stories');
+	require('../source/components/atoms/Input.stories');
+	require('../source/components/atoms/Markdown.stories');
+	require('../source/components/atoms/Select.stories');
+	require('../source/components/atoms/Text.stories');
+	require('../source/components/molecules/DateTimePickerForm.stories');
+	require('../source/components/molecules/Lists.stories');
+	require('../source/components/molecules/Modal.stories');
+	require('../source/components/organisms/FormAgent/FormAgent.stories');
+	require('../source/components/organisms/NavigationTabs.stories');
+	require('../source/components/organisms/WatsonAgent.stories');
 }
 
 const stories = [
-  '../source/components/atoms/Button.stories',
-  '../source/components/atoms/ChatBubble.stories',
-  '../source/components/atoms/Heading.stories',
-  '../source/components/atoms/Icon.stories',
-  '../source/components/atoms/Input.stories',
-  '../source/components/atoms/Markdown.stories',
-  '../source/components/atoms/Select.stories',
-  '../source/components/atoms/Text.stories',
-  '../source/components/molecules/DateTimePickerForm.stories',
-  '../source/components/molecules/Lists.stories',
-  '../source/components/molecules/Modal.stories',
-  '../source/components/organisms/FormAgent/FormAgent.stories',
-  '../source/components/organisms/NavigationTabs.stories',
-  '../source/components/organisms/WatsonAgent.stories',
+	'../source/components/atoms/Button.stories',
+	'../source/components/atoms/ChatBubble.stories',
+	'../source/components/atoms/Heading.stories',
+	'../source/components/atoms/Icon.stories',
+	'../source/components/atoms/Input.stories',
+	'../source/components/atoms/Markdown.stories',
+	'../source/components/atoms/Select.stories',
+	'../source/components/atoms/Text.stories',
+	'../source/components/molecules/DateTimePickerForm.stories',
+	'../source/components/molecules/Lists.stories',
+	'../source/components/molecules/Modal.stories',
+	'../source/components/organisms/FormAgent/FormAgent.stories',
+	'../source/components/organisms/NavigationTabs.stories',
+	'../source/components/organisms/WatsonAgent.stories'
 ];
 
 module.exports = {
