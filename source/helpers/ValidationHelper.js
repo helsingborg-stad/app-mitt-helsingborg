96b3f5c1b91e59628dab86bc69c47b2998ddcce4
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-02-25 09:09:01 +0100

    Eslint fixes

--- source/helpers/ValidationHelper.js
+++ source/helpers/ValidationHelper.js
@@ -1,16 +1,28 @@
-/* eslint-disable eqeqeq */
-/* eslint-disable no-param-reassign */
+/**
+ * Checks if a string is a valid personal identity number
+ * @param {string} pin Personal identity number
+ * @return {boolean} Return true if PIN is valid, else return false
+ */
 export const validatePin = pin => {
   const pinRegex = /^[0-9]{12}$/;
+
   return pinRegex.test(pin);
 };
 
+/**
+ * Sanitizes and prefixes personal identity number with century
+ * @param {string} pin Personal identity number
+ * @return {string} Personal identity number
+ */
 export const sanitizePin = pin => {
   // Remove non digits
-  pin = pin.replace(/\D/g, '');
-  // Automatically prefix personal number with century
-  pin = pin.length === 2 && pin > 19 && pin != 20 ? 19 + pin : pin;
-  pin = pin.length === 2 && pin < 19 ? 20 + pin : pin;
+  let sanitizedPin = pin.replace(/\D/g, '');
+  const pinInt = parseInt(sanitizedPin);
 
-  return pin;
+  sanitizedPin =
+    sanitizedPin.length === 2 && pinInt > 19 && pinInt !== 20 ? `19${sanitizedPin}` : sanitizedPin;
+
+  sanitizedPin = sanitizedPin.length === 2 && pinInt < 19 ? `20${sanitizedPin}` : sanitizedPin;
+
+  return sanitizedPin;
 };
