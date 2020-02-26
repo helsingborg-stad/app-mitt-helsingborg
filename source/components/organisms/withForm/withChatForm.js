06f81aa0d34677ddcb427040e885fc01fa19c482
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-02-26 15:50:05 +0100

    Lint withChatForm

--- source/components/organisms/withForm/withChatForm.js
+++ source/components/organisms/withForm/withChatForm.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/sort-comp */
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/static-property-placement */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
@@ -8,14 +5,10 @@ import withForm from './withForm';
 
 import ChatBubble from '../../atoms/ChatBubble';
 
-const withChatForm = (WrappedComponent, onSubmit) =>
+const withChatForm = (WrappedComponent, onSubmit) => {
   class WithChatForm extends Component {
-    static propTypes = {
-      chat: PropTypes.shape({
-        addMessages: PropTypes.func.isRequired,
-      }),
-      onSubmit: PropTypes.func,
-    };
+    // provides form props to WrappedComponent: submitHandler, changeHandler inputValue
+    WrappedComponentWithForm = withForm(WrappedComponent, this.messageOnSubmit);
 
     messageOnSubmit = inputValue => {
       const { chat } = this.props;
@@ -33,13 +26,20 @@ const withChatForm = (WrappedComponent, onSubmit) =>
       }
     };
 
-    // provides form props to WrappedComponent: submitHandler, changeHandler inputValue
-    WrappedComponentWithForm = withForm(WrappedComponent, this.messageOnSubmit);
-
     render() {
       const { WrappedComponentWithForm } = this;
       return <WrappedComponentWithForm {...this.props} />;
     }
+  }
+
+  WithChatForm.propTypes = {
+    chat: PropTypes.shape({
+      addMessages: PropTypes.func.isRequired,
+    }),
+    onSubmit: PropTypes.func,
   };
+
+  return WithChatForm;
+};
 
 export default withChatForm;
