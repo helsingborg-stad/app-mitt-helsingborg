b33c61c441ceea246247ea39e8f0ddb0eadceb6d
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-02-26 15:19:32 +0100

    Fixed ChatForm eslint-disable.

--- source/components/molecules/ChatForm.js
+++ source/components/molecules/ChatForm.js
@@ -1,20 +1,58 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { Keyboard } from 'react-native';
 import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
 import Button from '../atoms/Button/Button';
 import Icon from '../atoms/Icon';
 import Input, { input as inputStyles } from '../atoms/Input';
 import Text from '../atoms/Text';
 
+const ChatFormWrapper = styled.View``;
+const ChatFormFooter = styled.View``;
+const ChatFormBody = styled.View`
+  margin-top: 12px;
+  margin-bottom: 6px;
+  margin-left: 16px;
+  margin-right: 16px;
+  flex-direction: row;
+`;
+const ChatFormButton = styled(Button)`
+  min-width: auto;
+  padding: 0px 8px 0px 0px;
+  background: transparent;
+`;
+
+const ChatFormButtonIcon = styled(Icon)`
+  color: ${props => props.theme.icon.light};
+`;
+
+const UnStyledInput = styled.TextInput`
+  flex: 1;
+  padding: 8px;
+`;
+
+const InputStyledView = styled.View`
+  ${inputStyles}
+  padding: 8px;
+  flex-direction: row;
+  flex: 1;
+`;
+
 const ChatForm = props => {
-  const { style, renderFooter, submitHandler, changeHandler, inputValue, isFocused } = props;
+  const {
+    style,
+    renderFooter,
+    submitHandler,
+    changeHandler,
+    inputValue,
+    isFocused,
+    submitText,
+    ...other
+  } = props;
   const formProps = { submitHandler, changeHandler, inputValue };
 
-  const children = props.children
-    ? React.Children.map(props.children, (child, index) => {
+  const children = other.children
+    ? React.Children.map(other.children, child => {
         if (!child.type) {
           return child;
         }
@@ -54,7 +92,7 @@ const ChatForm = props => {
           )}
 
           <Button color="purpleLight" size="small" onClick={submitHandler} z={0}>
-            {props.submitText ? <Text>{props.submitText}</Text> : <Text>Skicka</Text>}
+            {submitText ? <Text>{submitText}</Text> : <Text>Skicka</Text>}
           </Button>
         </InputStyledView>
       </ChatFormBody>
@@ -64,35 +102,14 @@ const ChatForm = props => {
   );
 };
 
+ChatForm.propTypes = {
+  style: PropTypes.shape({}),
+  renderFooter: PropTypes.func,
+  submitHandler: PropTypes.func,
+  changeHandler: PropTypes.func,
+  inputValue: PropTypes.string,
+  isFocused: PropTypes.bool,
+  submitText: PropTypes.string,
+};
+
 export default ChatForm;
-
-const ChatFormWrapper = styled.View``;
-const ChatFormFooter = styled.View``;
-const ChatFormBody = styled.View`
-  margin-top: 12px;
-  margin-bottom: 6px;
-  margin-left: 16px;
-  margin-right: 16px;
-  flex-direction: row;
-`;
-const ChatFormButton = styled(Button)`
-  min-width: auto;
-  padding: 0px 8px 0px 0px;
-  background: transparent;
-`;
-
-const ChatFormButtonIcon = styled(Icon)`
-  color: ${props => props.theme.icon.light};
-`;
-
-const UnStyledInput = styled.TextInput`
-  flex: 1;
-  padding: 8px;
-`;
-
-const InputStyledView = styled.View`
-  ${inputStyles}
-  padding: 8px;
-  flex-direction: row;
-  flex: 1;
-`;
