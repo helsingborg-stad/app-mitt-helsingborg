4a9f2a2b01728ca33ca1d5dec45c63d5e4837005
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-03-03 14:10:43 +0100

    Splash navigate after auth

--- source/screens/SplashScreen.js
+++ source/screens/SplashScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import HbgLogo from '../assets/slides/stadsvapen.png';
+import AuthContext from '../store/AuthContext';
 
 const SplashContainer = styled.View`
   flex: 1;
@@ -12,10 +13,26 @@ const Logo = styled.Image`
   width: 100px;
 `;
 
-const SplashScreen = () => (
-  <SplashContainer>
-    <Logo source={HbgLogo} resizeMode="contain" />
-  </SplashContainer>
-);
+function SplashScreen(props) {
+  const { isAuthenticatedLOL } = useContext(AuthContext);
+  console.log("isAuthenticated in OUTISDE'", isAuthenticatedLOL);
+
+  const {
+    navigation: { navigate },
+  } = props;
+
+  useEffect(() => {
+    console.log("isAuthenticated in use effect'", isAuthenticatedLOL);
+    // TODO: wait for response before navigating
+    navigate(isAuthenticatedLOL ? 'Chat' : 'Onboarding');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticatedLOL]);
+
+  return (
+    <SplashContainer>
+      <Logo source={HbgLogo} resizeMode="contain" />
+    </SplashContainer>
+  );
+}
 
 export default SplashScreen;
