1441e93e4caf6b307d26974884d979f8fabd9594
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-03-06 13:28:21 +0100

    Blue color fix

--- source/navigator/BottomBarNavigator.js
+++ source/navigator/BottomBarNavigator.js
@@ -43,7 +43,7 @@ export const BottomBarStack = {
     screen: ProfileScreen,
     navigationOptions: {
       title: 'Profil',
-      tabBarIcon: TabBarIcon('contacts', 'blue'),
+      tabBarIcon: TabBarIcon('contacts', '#0095DB'),
     },
   },
 };
