2e42fdba35eb0caab29bad096b626bb0b2d190d7
Teddy Andersson <teddyandersson95@gmail.com>
2020-05-28 16:23:41 +0200

    Default story for Step Component

---
+++ source/components/organisms/Step/Step.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { StyleSheet } from 'react-native';
+import StoryWrapper from '../../molecules/StoryWrapper';
+import Step from './Step';
+
+const COAPPLICATION = require('source/images/illu_sammanstallning.png');
+
+const styles = StyleSheet.create({
+  smallIcon: {
+    top: 283,
+    right: -30,
+  },
+});
+
+storiesOf('Step', module).add('Default', () => (
+  <StoryWrapper>
+    <Step
+      banner={{
+        height: '327px',
+        imageSrc: COAPPLICATION,
+        imageStyle: styles.smallIcon,
+      }}
+      heading="Step heading"
+      tagline="Step tagline"
+      description={{
+        heading: 'Vill du ansöka om Ekonomiskt bistånd igen?',
+        tagline: 'Ansökan',
+        text: 'Du kommer behöva ange inkomster, utgifter och kontrollera dina boende detaljer.',
+      }}
+    />
+  </StoryWrapper>
+));
