bea88fe90cf2ade1a48bae421c5a3c7e0cdf185c
Teddy Andersson <teddyandersson95@gmail.com>
2020-05-29 09:45:41 +0200

    Added exports for Step component

--- source/components/organisms/index.js
+++ source/components/organisms/index.js
@@ -2,3 +2,4 @@ export { default as Chat } from './Chat';
 export { default as FormAgent } from './FormAgent';
 export { default as WatsonAgent } from './WatsonAgent';
 export { default as withForm } from './withForm';
+export { default as Step } from './Step';
