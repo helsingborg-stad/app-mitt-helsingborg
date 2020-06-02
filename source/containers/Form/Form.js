94af9b56f66bc2d7ed3bfbe1dc682295b34b140f
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-02 10:12:13 +0200

    added Step component to the Form

--- source/containers/Form/Form.js
+++ source/containers/Form/Form.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { styled } from 'styled-components/native';
+import styled from 'styled-components/native';
+import { Step } from 'app/components/organisms';
 import Stepper from '../../components/atoms/Stepper/Stepper';
 import useForm from './hooks/useForm';
 
-const FormContainer = styled.View``;
-const FormStepper = styled(Stepper);
+const FormStepper = styled(Stepper)``;
+const FormContainer = styled.View`
+  flex: 1;
+`;
 
+/**
+ * The Container Component Form allows you to create, process and reuse forms. The Form component
+ * is a tool to help you solve the problem of allowing end-users to interact with the
+ * data and modify the data in your application.
+ */
 function Form({ startAt, steps }) {
   const initialState = {
     counter: startAt,
@@ -17,7 +25,23 @@ function Form({ startAt, steps }) {
 
   return (
     <FormContainer>
-      <FormStepper active={formState.counter}>{/* Render Steps here */}</FormStepper>
+      <FormStepper active={formState.counter}>
+        {formState.steps.map(step => (
+          <Step
+            description={{
+              heading: step.title,
+              tagline: step.group,
+              text: step.description,
+            }}
+            onBack={handleBack}
+            onCancel={handleAbort}
+            isBackBtnVisible={formState.counter !== 1}
+            footer={{
+              buttons: [{ label: 'Nästa', onClick: handleNext }],
+            }}
+          />
+        ))}
+      </FormStepper>
     </FormContainer>
   );
 }
