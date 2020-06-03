fdbcc19e3e88edda07c8a26b814c80d7a588b604
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-03 16:30:28 +0200

    Banner now takes icon and image as seperate props

--- source/components/molecules/Banner/Banner.js
+++ source/components/molecules/Banner/Banner.js
@@ -3,29 +3,55 @@ import { Image } from 'react-native';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const ImageWrapper = styled.View(props => ({
-  top: 0,
-  margin: 0,
-  padding: 0,
-  height: props.height,
-  backgroundColor: props.backgroundColor,
-}));
+const BannerWrapper = styled.View`
+  top: 0;
+  margin: 0;
+  padding: 0;
+  min-height: ${props => (props.image ? '256' : '192')}px;
+  background: ${props => props.backgroundColor};
+  position: relative;
+  justify-content: flex-end;
+`;
 
-const Banner = ({ imageSrc, imageStyle, backgroundColor, height, style }) => (
-  <ImageWrapper style={style} backgroundColor={backgroundColor} height={height}>
-    <Image source={imageSrc} style={imageStyle} />
-  </ImageWrapper>
+const BannerImageIcon = styled(Image)`
+  width: 72px;
+  position: absolute;
+  bottom: -37px;
+  left: 32px;
+`;
+
+const BannerImageWrapper = styled.View`
+  overflow: hidden;
+  position: relative;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  height: auto;
+`;
+const BannerImage = styled(Image)`
+  width: 100%;
+  height: 256px;
+`;
+
+const Banner = ({ imageSrc, iconSrc, backgroundColor, style }) => (
+  <BannerWrapper image={imageSrc} style={style} backgroundColor={backgroundColor}>
+    {imageSrc && (
+      <BannerImageWrapper>
+        <BannerImage resizeMode="contain" source={imageSrc} />
+      </BannerImageWrapper>
+    )}
+    <BannerImageIcon source={iconSrc} />
+  </BannerWrapper>
 );
 
 Banner.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
+  iconSrc: PropTypes.string.isRequired,
   backgroundColor: PropTypes.string,
-  height: PropTypes.string,
   style: PropTypes.object.isRequired,
-  imageStyle: PropTypes.object.isRequired,
 };
 Banner.defaultProps = {
+  imageSrc: undefined,
   backgroundColor: '#FBF7F0',
-  height: '35%',
 };
 export default Banner;
