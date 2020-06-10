2a36419223e489453abff78f958e98dc05387a6e
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-10 15:47:15 +0200

    Added functions for handling navigation events in the form

--- source/containers/Form/hooks/useForm.js
+++ source/containers/Form/hooks/useForm.js
@@ -2,35 +2,54 @@ import { useReducer, useEffect } from 'react';
 import formReducer from './formReducer';
 import { actions } from './formActions';
 
-/**
- * A custom hook to handle state actions and logical behavior for a form.
- * @param { object } initialState The intital state of the hook.
- */
-function useForm(initialState) {
+function useForm(initialState, onAbort) {
   const [formState, dispatch] = useReducer(formReducer, initialState);
 
   useEffect(() => {
     dispatch({ type: actions.replaceFirstNameMarkdownInAllStepTitles });
   }, []);
 
-  const handleNext = () =>
-    /** TO BE IMPLEMENTED */
-    null;
-  const handlePrev = () =>
-    /** TO BE IMPLEMENTED */
-    null;
+  /**
+   * Function for increasing the form counter
+   */
+  const goToNextStep = () => {
+    console.log('next');
+    dispatch({ type: actions.increaseCounter });
+  };
+
+  /**
+   * Function for decreasing the form counter
+   */
+  const goToPreviousStep = () => {
+    console.log('prev');
+    dispatch({ type: actions.decreaseCounter });
+  };
+
+  const isLastStep = () => formState.steps.length === formState.counter;
+
   const handleSkip = () =>
     /** TO BE IMPLEMENTED */
     null;
+
+  /**
+   * Function for state values to the abort function that is passed down thorough props.
+   */
+  const closeForm = callback => {
+    callback({ state: formState }, isLastStep());
+  };
+
   const handleInputChange = () =>
     /** TO BE IMPLEMENTED */
     null;
+
   return {
     formState,
-    handlePrev,
-    handleNext,
+    goToNextStep,
+    goToPreviousStep,
     handleInputChange,
     handleSkip,
+    closeForm,
+    isLastStep,
   };
 }
 
