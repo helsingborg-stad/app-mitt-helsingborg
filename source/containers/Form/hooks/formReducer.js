c1969d2fef096555b505724a1931a15681ed1a2a
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-11 12:14:48 +0200

    Added case to replace title placeholder in step title withvalue

--- source/containers/Form/hooks/formReducer.js
+++ source/containers/Form/hooks/formReducer.js
@@ -1,6 +1,7 @@
-const actions = {
+export const actions = {
   increaseCounter: 'INCREASE_COUNTER',
   decreaseCounter: 'DECREASE_COUNTER',
+  setFirstNameInStepTitles: 'SET_NAME_IN_STEP_TITLES',
 };
 
 function formReducer(state, action) {
@@ -11,6 +12,17 @@ function formReducer(state, action) {
       break;
     case actions.decreaseCounter:
       break;
+    case actions.setFirstNameInStepTitles: {
+      const { steps, user } = state;
+      const updatedSteps = steps.map(s => ({
+        ...s,
+        title: s.title.replace('#firstName', user.givenName),
+      }));
+      return {
+        ...state,
+        steps: updatedSteps,
+      };
+    }
     default:
       return state;
   }
