474f025ab64b6678dcd3376aea21cc072b6a939a
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-11 12:16:15 +0200

    Updated stories to reflect new functionality in component

--- source/containers/Form/Form.stories.js
+++ source/containers/Form/Form.stories.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
+import UserMockData from 'app/assets/mock/user';
 import StoryWrapper from '../../components/molecules/StoryWrapper';
 import Form from './Form';
 
-const formData = {
+const DynamicTitleStoryData = {
   steps: [
     {
-      title: 'Vill du ansöka om Ekonomiskt bistånd igen?',
+      title: 'Hello #firstName!',
       description:
-        'Du kommer behöva ange inkomster, utgifter och kontrollera dina boende detaljer.',
+        'The value #firstName in the step title is changed to the first name of the current user',
       group: 'Ansökan',
       actions: [
         {
@@ -20,9 +21,35 @@ const formData = {
     },
   ],
 };
+const DefaultStoryData = {
+  steps: [
+    {
+      title: 'A step title!',
+      description: 'A step is used to divied fields/questions into sections in a form.',
+      group: 'Ansökan',
+      actions: [
+        {
+          type: 'next',
+          label: 'Go to next step',
+        },
+      ],
+      fields: [],
+    },
+  ],
+};
 
-storiesOf('Form', module).add('Default', () => (
-  <StoryWrapper>
-    <Form steps={formData.steps} />
-  </StoryWrapper>
-));
+const userData = {
+  givenName: 'Fake Name',
+};
+
+storiesOf('Form', module)
+  .add('Default', () => (
+    <StoryWrapper>
+      <Form steps={DefaultStoryData.steps} user={userData} />
+    </StoryWrapper>
+  ))
+  .add('Dynamic Title', () => (
+    <StoryWrapper>
+      <Form steps={DynamicTitleStoryData.steps} user={userData} />
+    </StoryWrapper>
+  ));
