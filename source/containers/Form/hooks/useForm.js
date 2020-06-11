149101402d612ceb17af64bd93501ed96d3b5269
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-11 12:15:26 +0200

    Dispatching the replace value function on intial form render

--- source/containers/Form/hooks/useForm.js
+++ source/containers/Form/hooks/useForm.js
@@ -1,8 +1,17 @@
-import { useReducer } from 'react';
-import formReducer from './formReducer';
+import { useReducer, useEffect } from 'react';
+import formReducer, { actions } from './formReducer';
 
+/**
+ * A custom hook to handle state actions and logical behavior for a form.
+ * @param {obj} initialState The intital state of the hook.
+ * @param {obj} user A object containg information about the user.
+ */
 function useForm(initialState) {
   const [formState, dispatch] = useReducer(formReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: actions.setFirstNameInStepTitles });
+  }, []);
 
   const handleNext = () =>
     /** TO BE IMPLEMENTED */
