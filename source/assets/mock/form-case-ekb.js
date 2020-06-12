be06d52d6147e82b2e505130947a29fcfd8fb420
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-12 12:37:17 +0200

    updated form mock-data

--- source/assets/mock/form-case-ekb.js
+++ source/assets/mock/form-case-ekb.js
@@ -1,3 +1,6 @@
+const ICON_EKB = require('source/assets/images/icons/icn_Main_ekonomiskt-bistand_1x.png');
+const ICON_BREIF = require('source/assets/images/icons/icn_sammanstallning_1x.png');
+
 const formEkbMockData = {
   steps: [
     {
@@ -6,7 +9,7 @@ const formEkbMockData = {
         'Du kommer behöva ange inkomster, utgifter och kontrollera dina boende detaljer.',
       group: 'Ansökan',
       image: '',
-      icon: '',
+      icon: ICON_EKB,
       actions: [
         {
           type: 'next',
@@ -14,6 +17,88 @@ const formEkbMockData = {
         },
       ],
       fields: [],
+      theme: {
+        step: {
+          bg: '#FFAA9B',
+          banner: {},
+          footer: {},
+          text: {
+            colors: {
+              primary: '#00213f',
+              secondary: '#733232',
+            },
+          },
+        },
+      },
+    },
+    {
+      title: 'Hej #firstName!',
+      description: 'Först börjar vi med att bekräfta dina uppgifter. Stämmer det här?',
+      group: 'Ekonomiskt Bistånd',
+      image: '',
+      icon: ICON_BREIF,
+      actions: [
+        {
+          type: 'next',
+          label: 'Ja, allt stämmer',
+        },
+      ],
+      fields: [
+        {
+          fieldType: 'address',
+          title: 'Boende/Tillgånger',
+          inputs: [
+            {
+              key: 'key-1',
+              label: 'Adress',
+              type: 'text',
+              value: 'Storgatan 9, Helsingborg',
+            },
+            {
+              key: 'key-2',
+              label: 'Storlek',
+              type: 'text',
+              value: '1 rum & kök',
+            },
+            {
+              key: 'key-3',
+              label: 'Hyresvärd',
+              type: 'text',
+              value: 'Helsingborgshem',
+            },
+          ],
+        },
+        {
+          fieldType: 'personal',
+          title: 'Mina Uppgifter',
+          inputs: [
+            {
+              key: 'key-1',
+              label: 'Epost',
+              type: 'text',
+              value: 'karin.olsson@epost.com',
+            },
+            {
+              key: 'key-2',
+              label: 'Telefone',
+              type: 'text',
+              value: '760000009',
+            },
+            {
+              key: 'key-3',
+              label: 'Medborgarskap',
+              type: 'text',
+              value: 'Svenskt',
+            },
+            {
+              key: 'key-4',
+              label: 'Sysselsättning',
+              type: 'text',
+              value: 'Arbetssökande',
+            },
+          ],
+        },
+      ],
       theme: {
         step: {
           bg: '#FFAA9B',
