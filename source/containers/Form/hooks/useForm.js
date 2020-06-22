db8ce40ff82af5bbe6c611de88b565220ffb1deb
Teddy Andersson <teddyandersson95@gmail.com>
2020-06-22 19:48:47 +0200

    Calling the start form action and passing down callback

--- source/containers/Form/hooks/useForm.js
+++ source/containers/Form/hooks/useForm.js
@@ -6,20 +6,40 @@ function useForm(initialState) {
   const [formState, dispatch] = useReducer(formReducer, initialState);
 
   useEffect(() => {
-    dispatch({ type: actionTypes.REPLACE_FIRSTNAME_MARKDOWN_IN_ALL_STEP_TITLES });
+    dispatch({
+      type: actionTypes.REPLACE_FIRSTNAME_MARKDOWN_IN_ALL_STEP_TITLES,
+    });
   }, []);
 
   /**
    * Function for increasing the form counter
    */
-  const goToNextStep = () => dispatch({ type: actionTypes.INCREASE_COUNTER });
+  const goToNextStep = () =>
+    dispatch({
+      type: actionTypes.INCREASE_COUNTER,
+    });
 
   /**
    * Function for decreasing the form counter
    */
-  const goToPreviousStep = () => dispatch({ type: actionTypes.DECREASE_COUNTER });
+  const goToPreviousStep = () =>
+    dispatch({
+      type: actionTypes.DECREASE_COUNTER,
+    });
 
   const isLastStep = () => formState.steps.length === formState.counter;
+
+  /**
+   * Function for passing state and step values to the callback function that is passed down
+   * to handle a form start action.
+   * @param {func} callback callback function to be called on when a start action is triggerd
+   */
+  const startForm = callback => {
+    dispatch({
+      type: actionTypes.START_FORM,
+      payload: { callback },
+    });
+  };
 
   const handleSkip = () =>
     /** TO BE IMPLEMENTED */
@@ -43,6 +63,7 @@ function useForm(initialState) {
     handleInputChange,
     handleSkip,
     closeForm,
+    startForm,
     isLastStep,
   };
 }
