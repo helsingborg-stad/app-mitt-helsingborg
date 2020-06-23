fb7700ab768a70d53d811da4c0ffee4a43113d85
Jacob Winding <jacobkwinding@gmail.com>
2020-06-23 16:57:45 +0200

    Working version of Field Label component, with some basic styling and story.

--- source/components/atoms/FieldLabel/FieldLabel.js
+++ source/components/atoms/FieldLabel/FieldLabel.js
@@ -1,62 +1,84 @@
 import styled from 'styled-components/native';
-import React from 'react'; 
+import React from 'react';
 import PropTypes from 'prop-types';
 import Text from '../Text';
 import colors from '../../../styles/colors';
-import SHADOW from '../../../styles/shadow';
+
+const sizes = {
+  small: {
+    font: '12px',
+    paddingBottom: '3px',
+    lineWidth: '2px',
+  },
+  medium: {
+    font: '14px',
+    paddingBottom: '7px',
+    lineWidth: '3px',
+  },
+  large: {
+    font: '18px',
+    paddingBottom: '10px',
+    lineWidth: '4px',
+  },
+};
 
 const FieldLabelText = styled(Text)`
-  font-size: ${props => {
-    if (props.size === 'small') { return '12px'; }
-    if (props.size === 'large') { return '18px';}
-    return '14px';
-  }};
+  font-size: ${props => sizes[props.size].font};
+  color: ${props => colors.fieldLabel[props.color].text};
   text-transform: uppercase;
   font-weight: bold;
   padding-bottom: 7px;
   padding-top: 5px;
 `;
 const FieldLabelBorder = styled.View`
-  padding-bottom: ${props => {
-    if (props.size === 'small') { return '3px'; }
-    if (props.size === 'large') { return '10px';}
-    return '8px';
-  }};
-  border-bottom-color: red;
+  padding-bottom: ${props => sizes[props.size].paddingBottom};
+  border-bottom-color: ${props => colors.fieldLabel[props.color].underline};
   border-bottom-width: ${props => {
-    if (props.size === 'small') { return '2px'; }
-    if (props.size === 'large') { return '4px';}
-    return '3px';
+    if (props.underline === 'false') {
+      return '0px';
+    }
+    return sizes[props.size].lineWidth;
   }};
   align-self: flex-start;
 `;
 
+/**
+ * Simple label for field inputs, that styles the text to all-caps and optionally puts a line under.
+ * @param {*} props 
+ */
 const FieldLabel = props => {
-  const { size, color, underline, ...other } = props;
-    return (
-        <FieldLabelBorder size={size}>
-            <FieldLabelText underline={underline} size={size} color={color}>
-                {other.children}
-            </FieldLabelText>
-        </FieldLabelBorder>
-    )
-}
-
+  const { size, color, underline, style, ...other } = props;
+  return (
+    <FieldLabelBorder size={size} color={color} underline={underline}>
+      <FieldLabelText underline={underline} size={size} color={color} style={style}>
+        {other.children}
+      </FieldLabelText>
+    </FieldLabelBorder>
+  );
+};
 
 FieldLabel.propTypes = {
+  /**
+   * If false, disables the line under the label. True by default.
+   */
   underline: PropTypes.bool,
-  color: PropTypes.oneOf(Object.keys(colors.button)),
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Set a color theme which changes the text color and line color accordingly. 'light' is default.
+   */
+  color: PropTypes.oneOf(Object.keys(colors.fieldLabel)),
+  /**
+   * Set a size, one of small, medium, large.
+   */
+  size: PropTypes.oneOf(Object.keys(sizes)),
+  /**
+   * Any additional styling of the text component.
+   */
   style: PropTypes.array,
-  z: PropTypes.oneOf(Object.keys(SHADOW).map(number => parseInt(number))),
 };
 
 FieldLabel.defaultProps = {
-  underline: false,
+  underline: true,
   color: 'light',
   size: 'medium',
-  z: 1,
 };
 export default FieldLabel;
-
-//   color: ${props => props.theme.button[props.buttonTheme].text};
