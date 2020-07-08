2d66311ea7b51e1ae3c39f70f36ab8da1d107592
Jacob Winding <jacobkwinding@gmail.com>
2020-07-08 11:16:24 +0200

    Add button that opens a sub-form, first version.

--- source/screens/LoginScreen.js
+++ source/screens/LoginScreen.js
@@ -29,7 +29,7 @@ const Link = styled(Text)`
   margin-bottom: 8px;
 `;
 
-const LoginKeyboardAvoidingView = styled.KeyboardAvoidingView`
+const LoginSafeAreaView = styled.SafeAreaView`
   flex: 1;
   align-items: stretch;
 `;
@@ -151,8 +151,13 @@ class LoginScreen extends Component {
     }
 
     return (
+<<<<<<< HEAD
       <LoginScreenWrapper>
         <LoginKeyboardAvoidingView enabled>
+=======
+      <LoginSafeAreaView behavior="padding" enabled>
+        <LoginScreenWrapper>
+>>>>>>> 91b1370... Add button that opens a sub-form, first version.
           <LoginHeader>
             {hideLogo ? null : <Logo source={SLIDES.STADSVAPEN_PNG} resizeMode="contain" />}
           </LoginHeader>
@@ -205,8 +210,8 @@ class LoginScreen extends Component {
               </LoginFormField>
             </LoginForm>
           </LoginBody>
-        </LoginKeyboardAvoidingView>
-      </LoginScreenWrapper>
+        </LoginScreenWrapper>
+      </LoginSafeAreaView>
     );
   }
 }
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -1,73 +1,76 @@
-/* eslint-disable global-require */
 // Auto-generated file created by react-native-storybook-loader
 // Do not edit.
 //
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../source/components/atoms/Button/Button.stories');
-  require('../source/components/atoms/ChatBubble/ChatBubble.stories');
-  require('../source/components/atoms/Checkbox/Checkbox.stories');
-  require('../source/components/atoms/FieldLabel/FieldLabel.stories');
-  require('../source/components/atoms/Heading/Heading.stories');
-  require('../source/components/atoms/Icon/Icon.stories');
-  require('../source/components/atoms/Input/Input.stories');
-  require('../source/components/atoms/Select/Select.stories');
-  require('../source/components/atoms/Stepper/Stepper.stories');
-  require('../source/components/atoms/Text/Text.stories');
-  require('../source/components/molecules/BackNavigation/BackNavigation.stories');
-  require('../source/components/molecules/Banner/Banner.stories');
-  require('../source/components/molecules/DateTimePicker/DateTimePickerForm.stories');
-  require('../source/components/molecules/EditableList/EditableList.stories');
-  require('../source/components/molecules/FooterAction/FooterAction.stories');
-  require('../source/components/molecules/FormField/FormField.stories');
-  require('../source/components/molecules/Header/Header.stories');
-  require('../source/components/molecules/ImagePicker/ImagePicker.stories');
-  require('../source/components/molecules/InfoLink/InfoButton.stories');
-  require('../source/components/molecules/InfoText/InfoButton.stories');
-  require('../source/components/molecules/ListItem/Lists.stories');
-  require('../source/components/molecules/Markdown.stories');
-  require('../source/components/molecules/Modal.stories');
-  require('../source/components/molecules/StepDescription/StepDescription.stories');
-  require('../source/components/molecules/ButtonField/ButtonField.stories');
-  require('../source/components/organisms/FieldList/FieldList.stories');
-  require('../source/components/organisms/FormAgent/FormAgent.stories');
-  require('../source/components/organisms/Step/Step.stories');
-  require('../source/components/organisms/WatsonAgent/WatsonAgent.stories');
-  require('../source/components/molecules/CheckboxField/CheckboxField.stories');
+	require('../source/components/atoms/Button/Button.stories');
+	require('../source/components/atoms/ChatBubble/ChatBubble.stories');
+	require('../source/components/atoms/Checkbox/Checkbox.stories');
+	require('../source/components/atoms/FieldLabel/FieldLabel.stories');
+	require('../source/components/atoms/Heading/Heading.stories');
+	require('../source/components/atoms/Icon/Icon.stories');
+	require('../source/components/atoms/Input/Input.stories');
+	require('../source/components/atoms/Select/Select.stories');
+	require('../source/components/atoms/Stepper/Stepper.stories');
+	require('../source/components/atoms/Text/Text.stories');
+	require('../source/components/molecules/BackNavigation/BackNavigation.stories');
+	require('../source/components/molecules/Banner/Banner.stories');
+	require('../source/components/molecules/ButtonField/ButtonField.stories');
+	require('../source/components/molecules/ButtonListField/ButtonListField.stories');
+	require('../source/components/molecules/CheckboxField/CheckboxField.stories');
+	require('../source/components/molecules/DateTimePicker/DateTimePickerForm.stories');
+	require('../source/components/molecules/EditableList/EditableList.stories');
+	require('../source/components/molecules/FooterAction/FooterAction.stories');
+	require('../source/components/molecules/FormField/FormField.stories');
+	require('../source/components/molecules/Header/Header.stories');
+	require('../source/components/molecules/ImagePicker/ImagePicker.stories');
+	require('../source/components/molecules/InfoLink/InfoButton.stories');
+	require('../source/components/molecules/InfoText/InfoButton.stories');
+	require('../source/components/molecules/ListItem/Lists.stories');
+	require('../source/components/molecules/Markdown.stories');
+	require('../source/components/molecules/Modal.stories');
+	require('../source/components/molecules/StepDescription/StepDescription.stories');
+	require('../source/components/molecules/SubstepButton/SubstepButton.stories');
+	require('../source/components/organisms/FieldList/FieldList.stories');
+	require('../source/components/organisms/FormAgent/FormAgent.stories');
+	require('../source/components/organisms/Step/Step.stories');
+	require('../source/components/organisms/WatsonAgent/WatsonAgent.stories');
 }
 
 const stories = [
-  '../source/components/atoms/Button/Button.stories',
-  '../source/components/atoms/ChatBubble/ChatBubble.stories',
-  '../source/components/atoms/Checkbox/Checkbox.stories',
-  '../source/components/atoms/FieldLabel/FieldLabel.stories',
-  '../source/components/atoms/Heading/Heading.stories',
-  '../source/components/atoms/Icon/Icon.stories',
-  '../source/components/atoms/Input/Input.stories',
-  '../source/components/atoms/Select/Select.stories',
-  '../source/components/atoms/Stepper/Stepper.stories',
-  '../source/components/atoms/Text/Text.stories',
-  '../source/components/molecules/BackNavigation/BackNavigation.stories',
-  '../source/components/molecules/Banner/Banner.stories',
-  '../source/components/molecules/DateTimePicker/DateTimePickerForm.stories',
-  '../source/components/molecules/EditableList/EditableList.stories',
-  '../source/components/molecules/FooterAction/FooterAction.stories',
-  '../source/components/molecules/FormField/FormField.stories',
-  '../source/components/molecules/Header/Header.stories',
-  '../source/components/molecules/ImagePicker/ImagePicker.stories',
-  '../source/components/molecules/InfoLink/InfoButton.stories',
-  '../source/components/molecules/InfoText/InfoButton.stories',
-  '../source/components/molecules/ListItem/Lists.stories',
-  '../source/components/molecules/Markdown.stories',
-  '../source/components/molecules/Modal.stories',
-  '../source/components/molecules/StepDescription/StepDescription.stories',
-  '../source/components/molecules/ButtonField/ButtonField.stories',
-  '../source/components/organisms/FieldList/FieldList.stories',
-  '../source/components/organisms/FormAgent/FormAgent.stories',
-  '../source/components/organisms/Step/Step.stories',
-  '../source/components/organisms/WatsonAgent/WatsonAgent.stories',
-  '../source/components/molecules/CheckboxField/CheckboxField.stories',
+	'../source/components/atoms/Button/Button.stories',
+	'../source/components/atoms/ChatBubble/ChatBubble.stories',
+	'../source/components/atoms/Checkbox/Checkbox.stories',
+	'../source/components/atoms/FieldLabel/FieldLabel.stories',
+	'../source/components/atoms/Heading/Heading.stories',
+	'../source/components/atoms/Icon/Icon.stories',
+	'../source/components/atoms/Input/Input.stories',
+	'../source/components/atoms/Select/Select.stories',
+	'../source/components/atoms/Stepper/Stepper.stories',
+	'../source/components/atoms/Text/Text.stories',
+	'../source/components/molecules/BackNavigation/BackNavigation.stories',
+	'../source/components/molecules/Banner/Banner.stories',
+	'../source/components/molecules/ButtonField/ButtonField.stories',
+	'../source/components/molecules/ButtonListField/ButtonListField.stories',
+	'../source/components/molecules/CheckboxField/CheckboxField.stories',
+	'../source/components/molecules/DateTimePicker/DateTimePickerForm.stories',
+	'../source/components/molecules/EditableList/EditableList.stories',
+	'../source/components/molecules/FooterAction/FooterAction.stories',
+	'../source/components/molecules/FormField/FormField.stories',
+	'../source/components/molecules/Header/Header.stories',
+	'../source/components/molecules/ImagePicker/ImagePicker.stories',
+	'../source/components/molecules/InfoLink/InfoButton.stories',
+	'../source/components/molecules/InfoText/InfoButton.stories',
+	'../source/components/molecules/ListItem/Lists.stories',
+	'../source/components/molecules/Markdown.stories',
+	'../source/components/molecules/Modal.stories',
+	'../source/components/molecules/StepDescription/StepDescription.stories',
+	'../source/components/molecules/SubstepButton/SubstepButton.stories',
+	'../source/components/organisms/FieldList/FieldList.stories',
+	'../source/components/organisms/FormAgent/FormAgent.stories',
+	'../source/components/organisms/Step/Step.stories',
+	'../source/components/organisms/WatsonAgent/WatsonAgent.stories'
 ];
 
 module.exports = {
