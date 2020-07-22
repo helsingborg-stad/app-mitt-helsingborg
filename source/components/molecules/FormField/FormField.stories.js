ada04654d477b3728a7edf617d98d0ab2e477199
Jacob Winding <jacobkwinding@gmail.com>
2020-07-22 14:43:49 +0200

    Clean up the form field story file

--- source/components/molecules/FormField/FormField.stories.js
+++ source/components/molecules/FormField/FormField.stories.js
@@ -14,19 +14,16 @@ const categories = {
 const items = [
   {
     category: 'fruit',
-    component: <Text>Banana!</Text>,
     title: 'Banana',
     formId: 'e366f250-c5a2-11ea-9e21-b7b32d0793c5',
   },
   {
     category: 'fruit',
-    component: <Text>Pear!</Text>,
     title: 'Pear',
     formId: 'e366f250-c5a2-11ea-9e21-b7b32d0793c5',
   },
   {
     category: 'vegetable',
-    component: <Text>Carrot!</Text>,
     title: 'Carrot',
     formId: 'e366f250-c5a2-11ea-9e21-b7b32d0793c5',
   },
