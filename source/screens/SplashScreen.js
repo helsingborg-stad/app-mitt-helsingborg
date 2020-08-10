b7305a1654ed1fd7cc2e350a0601841c1f4b24c2
Teddy Andersson <teddyandersson95@gmail.com>
2020-08-10 16:47:56 +0200

    Destructing the authContext to only include used methods in useEffect hook

--- source/screens/SplashScreen.js
+++ source/screens/SplashScreen.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { ActivityIndicator } from 'react-native';
 import AuthContext from '../store/AuthContext';
-import StorageService, { SHOW_SPLASH_SCREEN } from '../services/StorageService';
+import StorageService, { SHOW_SPLASH_SCREEN, TOKEN_KEY } from '../services/StorageService';
 
 const SplashContainer = styled.View`
   flex: 1;
@@ -17,8 +17,21 @@ function SplashScreen(props) {
     navigation: { navigate },
   } = props;
 
-  const authContext = useContext(AuthContext);
+  const {
+    handleLogin,
+    handleLogout,
+    handleAddProfile,
+    handleRemoveProfile,
+    isUserAuthenticated,
+  } = useContext(AuthContext);
 
+  useEffect(() => {
+    const boostrapAsync = async () => {
+      console.log(await StorageService.getData(TOKEN_KEY));
+    };
+
+    boostrapAsync();
+  }, []);
   /**
    * Returns if onboarding screen is disabled
    */
@@ -28,25 +41,28 @@ function SplashScreen(props) {
   };
 
   useEffect(() => {
-    const navigateAsync = async () => {
-      const showOnboarding = await showOnboardingScreen();
-      navigate(showOnboarding ? 'Onboarding' : 'Login');
-    };
-
     const authCheck = async () => {
-      if (await authContext.isUserAuthenticated()) {
-        authContext.handleLogin();
-        await authContext.handleAddProfile();
+      if (await isUserAuthenticated()) {
+        handleLogin();
+        await handleAddProfile();
         navigate('Chat');
       } else {
-        await authContext.handleLogout();
-        authContext.handleRemoveProfile();
-        await navigateAsync();
+        await handleLogout();
+        handleRemoveProfile();
+        const showOnboarding = await showOnboardingScreen();
+        navigate(showOnboarding ? 'Onboarding' : 'Login');
       }
     };
 
     authCheck();
-  }, [authContext, navigate]);
+  }, [
+    handleAddProfile,
+    handleLogin,
+    handleLogout,
+    handleRemoveProfile,
+    isUserAuthenticated,
+    navigate,
+  ]);
 
   return (
     <SplashContainer>
