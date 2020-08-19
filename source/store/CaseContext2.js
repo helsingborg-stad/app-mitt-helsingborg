ce04879d43fd68735d82d775dfc2d74e2b3ab6be
Jacob Winding <jacobkwinding@gmail.com>
2020-08-19 12:58:09 +0200

    Add a new case context that uses a reducer. Not really working as intended right now

---
+++ source/store/CaseContext2.js
@@ -0,0 +1,71 @@
+import React, { useContext, useReducer, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import AuthContext from 'app/store/AuthContext';
+import useReducerWithEffects from 'source/helpers/useReducerWithEffects';
+import CaseReducer, { initialState as defaultInitialState } from './reducers/CaseReducer';
+import {
+  updateCase as update,
+  createCase as create,
+  deleteCase as delCase,
+  fetchCases as fetch,
+} from './actions/CaseActions';
+
+// const CaseContext2 = React.createContext();
+
+const CaseState = React.createContext();
+const CaseDispatch = React.createContext();
+
+function CaseProvider2({ children, initialState = defaultInitialState }) {
+  const [cases, dispatch] = useReducerWithEffects(CaseReducer, initialState, {});
+  const { user } = useContext(AuthContext);
+
+  async function createCase(formId, callback = response => {}) {
+    dispatch(await create(formId, user, cases, callback));
+  }
+
+  async function updateCase(caseId, data, status, currentStep) {
+    dispatch(await update(caseId, data, status, currentStep, user));
+  }
+
+  function getCase(caseId) {
+    return cases[caseId];
+  }
+
+  async function deleteCase(caseId) {
+    dispatch(await delCase(caseId));
+  }
+
+  const fetchCases = useCallback(
+    async function loadCases(callback = newCases => {}) {
+      dispatch(await fetch(user, callback));
+    },
+    [dispatch, user]
+  );
+
+  useEffect(() => {
+    if (user) {
+      fetchCases();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  return (
+    <CaseState.Provider value={{ cases, getCase }}>
+      <CaseDispatch.Provider value={{ createCase, updateCase, deleteCase }}>
+        {children}
+      </CaseDispatch.Provider>
+    </CaseState.Provider>
+  );
+  // return <CaseContext2.Provider value={contextValues}>{children}</CaseContext2.Provider>;
+}
+
+CaseProvider2.propTypes = {
+  children: PropTypes.node,
+  initialState: PropTypes.shape({}),
+};
+
+CaseProvider2.defaultProps = {
+  initialState: defaultInitialState,
+};
+
+export { CaseProvider2, CaseState, CaseDispatch };
