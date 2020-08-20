8e0b9f4a9fe5f4a26027887e583c97575f81f565
Jacob Winding <jacobkwinding@gmail.com>
2020-08-20 16:18:13 +0200

    Fix a use-effect bug in caseContext

--- source/navigator/AuthStack.js
+++ source/navigator/AuthStack.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import LoginScreen from '../screens/LoginScreen';
