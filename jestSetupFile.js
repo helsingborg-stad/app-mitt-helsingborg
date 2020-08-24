c6727ff674d859bdaafea3c3b310c97d41973534
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-08-24 15:54:03 +0200

    Mock async-storage

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -1,3 +1,5 @@
-import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+/**
+ * Async Storage
+ */
+jest.mock('@react-native-community/async-storage', () => jest.requireActual('@react-native-community/async-storage/jest/async-storage-mock'));
 
-jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
