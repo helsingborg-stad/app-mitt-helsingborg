de24d8c2a1be2316e7ed0c13db6a8956f8916ed1
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-08-24 16:07:41 +0200

    Add custom render method with common wrapper components

---
+++ test-utils.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { AuthProvider } from 'app/store/AuthContext';
+import ScreenWrapper from 'app/components/molecules/ScreenWrapper';
+
+const RenderWrapper = ({children}) => (
+    <AuthProvider>
+        <ScreenWrapper>
+            {children}
+        </ScreenWrapper>
+    </AuthProvider>
+);
+  
+
+const customRender = (ui, options) => render(ui, { wrapper: RenderWrapper });
+
+// re-export everything
+export * from '@testing-library/react-native';
+
+// override render method
+export { customRender as render };
