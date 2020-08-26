361aaebc2507e9f887f024c34d02db04fc4e7170
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-08-26 13:53:48 +0200

    Removed PoshNotificationService.

--- source/services/index.js
+++ source/services/index.js
@@ -1,6 +1,5 @@
 import * as ChatFormService from './ChatFormService';
-import * as PushNotificationService from './PushNotificationService';
 import * as StorageService from './StorageService';
 import * as UserService from './UserService';
 
-export { ChatFormService, PushNotificationService, StorageService, UserService };
+export { ChatFormService, StorageService, UserService };
