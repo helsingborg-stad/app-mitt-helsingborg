bb1d650023cd08fb200fa4e3648c3677fc1f92d8
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-08-26 13:45:17 +0200

    Removed TaskDetailScreen.

--- source/screens/index.js
+++ source/screens/index.js
@@ -2,7 +2,6 @@ export { default as ChatScreen } from './ChatScreen';
 export { default as LoginScreen } from './LoginScreen';
 export { default as ProfileScreen } from './ProfileScreen';
 export { default as SplashScreen } from './SplashScreen';
-export { default as TaskDetailScreen } from './TaskDetailScreen';
 export { default as CaseArchiveScreen } from './CaseArchiveScreen';
 export { default as HomeScreen } from './HomeScreen';
 export { default as FormCaseScreen } from './FormCaseScreen';
