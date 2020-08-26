bec3da9339ca13ffe0bbc75fdb19cfe3acbc84ce
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-08-26 14:42:30 +0200

    Reverted Select.

--- source/components/atoms/index.js
+++ source/components/atoms/index.js
@@ -11,3 +11,4 @@ export { default as Text } from './Text';
 export { default as Stepper } from './Stepper';
 export { default as Checkbox } from './Checkbox';
 export { default as FieldLabel } from './FieldLabel';
+export { default as Select } from './Select';
