1023bc34eac4683a7e82c8e5564d1a36771c9a2e
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-08-26 11:39:45 +0200

    Removed TabBarIcon.

--- source/components/molecules/TabBarIcon/TabBarIcon.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Icon } from 'react-native-elements';
-// TODO: Move Badge to own component file
-const TabBarIcon = (iconName, colorFocused) => ({ focused }) => (
-  <Icon name={iconName} color={focused ? colorFocused : 'gray'} />
-);
-
-export default TabBarIcon;
