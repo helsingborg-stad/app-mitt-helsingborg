9586e6441375b96cf1be96987d8491f51ed80675
nRamstedt <nikolas.ramstedt@gmail.com>
2020-09-01 09:43:34 +0200

    Mock env

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -1,6 +1,22 @@
 import { NativeModules } from 'react-native';
 
 /**
+ * Mock react-native-config (ENVS)
+ */
+jest.mock('react-native-config-node', () => ({
+  ...jest.requireActual('react-native-config-node'),
+  // Override env variables for testing below
+
+  // No base URL to prevent actual API calls
+  // Targeting axios calls when mocking only requires endpoint
+  MITTHELSINGBORG_IO: '',
+  MITTHELSINGBORG_IO_APIKEY: '',
+  FAKE_PERSONAL_NUMBER: '201111111111',
+  FAKE_TOKEN:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+}));
+
+/**
  * Async Storage
  */
 jest.mock('@react-native-community/async-storage', () =>
