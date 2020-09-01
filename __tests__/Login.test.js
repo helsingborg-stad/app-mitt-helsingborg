dab90c62e6dcd071484a1cf32b5eed617a600b27
nRamstedt <nikolas.ramstedt@gmail.com>
2020-09-01 09:44:40 +0200

    Add: bankid mock & tests

--- __tests__/Login.test.js
+++ __tests__/Login.test.js
@@ -7,10 +7,76 @@ import { Alert } from 'react-native';
 import LoginScreen from 'source/screens/LoginScreen';
 import MockNavigator from 'app/navigator/MockNavigator';
 
-jest.mock('axios');
+import MockAdapter from 'axios-mock-adapter';
 
-it('Validates personalnumber and triggers an alert', async () => {
-  // TODO: Mock axios bankid requests
+jest.mock('jwt-decode', () => () => ({
+  exp: 123,
+}));
+
+jest.mock('react-native-network-info');
+
+jest.mock('../source/helpers/UrlHelper.js', () => ({
+  ...jest.requireActual('../source/helpers/UrlHelper.js'),
+  canOpenUrl: jest.fn().mockReturnValue(false),
+}));
+
+const mock = new MockAdapter(axios);
+
+mock.onPost('/auth/bankid/auth?apikey=').reply(200, {
+  success: true,
+  data: {
+    attributes: {
+      order_ref: 'theOrderRef',
+      auto_start_token: 'theAutoStartToken',
+    },
+  },
+});
+
+mock.onPost('/auth/bankid/collect?apikey=').reply(200, {
+  success: true,
+  data: {
+    attributes: {
+      completion_data: {
+        user: {
+          personal_number: '199803312389',
+        },
+      },
+    },
+  },
+});
+
+mock.onPost('/auth/token?apikey=').reply(200, {
+  success: true,
+  data: {
+    attributes: {
+      token: '123',
+    },
+  },
+});
+
+it('Navigates to start screen after successfull authentication', async () => {
+  const { getByPlaceholderText, getByText } = render(<MockNavigator component={LoginScreen} />);
+
+  fireEvent.changeText(getByPlaceholderText('ÅÅÅÅMMDDXXXX'), '199803312389');
+  fireEvent.press(getByText('Logga in med mobilt BankID'));
+
+  await act(async () => {});
+
+  expect(getByText('Welcome to the start screen')).toBeTruthy();
+});
+
+it('Returns to login screen when user press the cancel button during an on-going auth', async () => {
+  const { getByPlaceholderText, getByText } = render(<MockNavigator component={LoginScreen} />);
+
+  fireEvent.changeText(getByPlaceholderText('ÅÅÅÅMMDDXXXX'), '199803312389');
+  fireEvent.press(getByText('Logga in med mobilt BankID'));
+
+  await act(async () => fireEvent.press(getByText('Avbryt')));
+
+  expect(getByPlaceholderText('ÅÅÅÅMMDDXXXX')).toBeTruthy();
+});
+
+it('Triggers an alert when user personalnumber is incorrect format', async () => {
   const spy = jest.spyOn(Alert, 'alert');
   const { getByPlaceholderText, getByText } = render(<MockNavigator component={LoginScreen} />);
 
@@ -19,24 +85,3 @@ it('Validates personalnumber and triggers an alert', async () => {
 
   await waitFor(() => expect(spy).toHaveBeenCalled());
 });
-
-it('Returns to login screen when cancelling ', async () => {
-  const { getByPlaceholderText, getByText, debug } = render(
-    <MockNavigator component={LoginScreen} />
-  );
-
-  fireEvent.changeText(getByPlaceholderText('ÅÅÅÅMMDDXXXX'), '199803312389');
-  fireEvent.press(getByText('Logga in med mobilt BankID'));
-
-  // TODO: Mock axios bankid requests
-  axios.request(async params => {
-    console.log(params);
-
-    return Promise.resolve();
-  });
-
-  await act(async () => fireEvent.press(getByText('Avbryt')));
-  expect(getByPlaceholderText('ÅÅÅÅMMDDXXXX')).toBeTruthy();
-});
-
-// TODO: it('Sign in user after successfull authentication')
