a9b72128c7dacac346b4e9c47890f6cb2d755e7f
Jacob Winding <jacobkwinding@gmail.com>
2020-09-10 12:27:02 +0200

    Add some types for Form, Case and User, to be used in various places

--- source/types/CaseType.ts
+++ source/types/CaseType.ts
@@ -1,11 +1,11 @@
 export interface Case {
-    createdAt: number;
-    updatedAt: number;
-    currentStep: number;
-    data: Record<string, any>;
-    formId: string;
-    id: string;
-    type: string;
-    personalNumber: string;
-    status: 'ongoing' | 'submitted'; // do we have/need other statuses?
+  createdAt: number;
+  updatedAt: number;
+  currentStep: number;
+  data: Record<string, any>;
+  formId: string;
+  id: string;
+  type: string;
+  personalNumber: string;
+  status: 'ongoing' | 'submitted'; // do we have/need other statuses?
 }
