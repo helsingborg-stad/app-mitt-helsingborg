7ceac9ad8174ce9ee0e4d73b370bc403bb3b65b9
Jacob Winding <jacobkwinding@gmail.com>
2020-09-14 12:34:20 +0200

    Add some types for Form, Case and User (#231)
    * Add some types for Form, Case and User, to be used in various places
    
    * Add some types for Form, Case and User, to be used in various places

---
+++ source/types/FormTypes.ts
@@ -0,0 +1,50 @@
+export interface Question {
+    label: string;
+    type: string;
+    id: string;
+    description?: string;
+    conditionalOn?: string;
+    placeholder?: string;
+    explainer?: string;
+    loadPrevious?: string[];  
+    items?: SubstepItem[];
+    inputs?: ListInput[];
+  }
+  
+  export interface SubstepItem {
+    category: string;
+    title: string;
+    formId: string;
+    loadPrevious?: string[];
+  }
+  
+  export interface ListInput {
+    type: 'text' | 'number';
+    key: string;
+    label: string;
+    loadPrevious?: string[];
+  }
+  export interface Action {
+    type: string;
+    label: string;
+  }
+  
+  export interface Step {
+    title: string;
+    description: string;
+    id?: string;
+    group: string;
+    questions?: Question[];
+    actions?: Action[];
+  }
+  
+  export interface Form {
+    name: string;
+    description: string;
+    steps?: Step[];
+    id: string;
+    subform?: boolean;
+    formType?: string;
+    provider?: string;
+  }
+  
