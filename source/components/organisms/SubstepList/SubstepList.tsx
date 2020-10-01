fe0d8d8b52a87626f0af501c332893e4f8826008
Jacob Winding <jacobkwinding@gmail.com>
2020-10-01 10:40:26 +0200

    Fix a variety of things for Substep List (#243)
    * Fixing bugs regarding removing and editing items
    * Small refactor for improved separation of logic between list item and list component

---
+++ source/components/organisms/SubstepList/SubstepList.tsx
@@ -0,0 +1,255 @@
+/* eslint-disable no-nested-ternary */
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
+import { Text, FieldLabel } from '../../atoms';
+import SubstepButton from '../../molecules/SubstepButton/SubstepButton';
+import GroupedList from '../../molecules/GroupedList/GroupedList';
+import colors from '../../../styles/colors';
+import { excludePropetiesWithKey } from '../../../helpers/Objects';
+import SubstepListItem from './SubstepListItem';
+
+const Wrapper = styled(View)`
+  margin-bottom: 25px;
+`;
+
+const LargeText = styled(Text)`
+  height: 40px;
+  font-size: 22px;
+  font-weight: 800;
+  padding-top: 11px;
+  padding-bottom: 8px;
+  padding-left: 17px;
+`;
+
+const FieldLabelContainer = styled.View`
+  margin-top: 24px;
+  margin-bottom: 8px;
+  margin-left: 8px;
+`;
+export interface ShownField {
+  heading: string;
+  fieldId: string;
+  category: string;
+}
+
+export interface Item {
+  key: string;
+  title: string;
+  category: string;
+  formId: string;
+  shownFields: ShownField[];
+  [x: string]: any;
+}
+
+interface Category {
+  category: string;
+  description: string;
+}
+
+interface Props {
+  heading: string;
+  items: Item[];
+  categories: Category[];
+  value: Record<string, any>;
+  onChange: (val: Record<string, any>) => void;
+  summary: boolean;
+  color: string;
+  buttonHelpText?: string;
+  buttonHelpHeading?: string;
+  buttonHelpTagline?: string;
+  buttonHelpUrl?: string;
+}
+
+const SubstepList: React.FC<Props> = ({
+  heading,
+  items,
+  categories,
+  value,
+  onChange,
+  summary,
+  color,
+  buttonHelpText,
+  buttonHelpHeading,
+  buttonHelpTagline,
+  buttonHelpUrl,
+}) => {
+  const [editable, setEditable] = useState(!summary);
+
+  // aggregate the helper for the label above the buttons to add items.
+  const buttonHelp = {
+    text: buttonHelpText,
+    heading: buttonHelpHeading,
+    tagline: buttonHelpTagline,
+    url: buttonHelpUrl,
+  };
+
+  const updateAnswer = (itemKey: string) => (data: Record<string, any>) => {
+    const newAnswers = JSON.parse(JSON.stringify(typeof value === 'string' ? {} : value));
+    newAnswers[itemKey] = data;
+    onChange(newAnswers);
+  };
+
+  const changeFromInput = (item: Item, shownField: ShownField) => (text: string) => {
+    const newAnswers = JSON.parse(JSON.stringify(typeof value === 'string' ? {} : value));
+    newAnswers[item.key][shownField.fieldId] = text;
+    onChange(newAnswers);
+  };
+
+  const removeItem = (item: Item, shownField: ShownField) => () => {
+    const newItem = excludePropetiesWithKey(typeof value === 'string' ? {} : value[item.key], [
+      shownField.fieldId,
+    ]);
+    const newAnswers = JSON.parse(JSON.stringify(value));
+    newAnswers[item.key] = newItem;
+    onChange(newAnswers);
+  };
+
+  // generate the list items
+  const listItems: { category: string; component: React.ReactNode }[] = [];
+  items.forEach(item => {
+    if (item.shownFields && item.shownFields.length > 0) {
+      item.shownFields
+        .filter(
+          shownField =>
+            Object.keys(typeof value === 'string' ? {} : value).includes(item.key) &&
+            Object.keys(value[item.key]).includes(shownField.fieldId)
+        )
+        .forEach(shownField => {
+          const props = {
+            item,
+            shownField,
+            value,
+            summary,
+            editable,
+            color,
+            updateAnswer: updateAnswer(item.key),
+            removeItem: removeItem(item, shownField),
+            changeFromInput: changeFromInput(item, shownField),
+          };
+          listItems.push({
+            category: shownField.category,
+            component: <SubstepListItem {...props} />,
+          });
+        });
+    }
+  });
+
+  // Add the sum if we are in summary mode
+  if (summary) {
+    if (listItems.length > 0) {
+      if (!categories.find(c => c.category === 'sum')) {
+        categories.push({ category: 'sum', description: 'Summa' });
+      }
+      const sum = items.reduce(
+        (sum: number, item: Item) =>
+          item.shownFields
+            ? sum +
+              item.shownFields.reduce((partialSum: number, shownField: ShownField) => {
+                if (value[item.key] && value[item.key][shownField.fieldId]) {
+                  const term = parseFloat(value[item.key][shownField.fieldId]);
+                  // eslint-disable-next-line no-restricted-globals
+                  if (isNaN(term)) {
+                    return partialSum;
+                  }
+                  return partialSum + term;
+                }
+                return partialSum;
+              }, 0)
+            : sum,
+        0
+      );
+      listItems.push({
+        category: 'sum',
+        component: <LargeText>{`${sum} kr`}</LargeText>,
+      });
+    }
+  }
+
+  return (
+    <Wrapper>
+      {listItems.length > 0 && (
+        <GroupedList
+          heading={heading}
+          items={listItems}
+          categories={categories}
+          color={color}
+          onEdit={() => setEditable(!editable)}
+        />
+      )}
+      {editable && !summary && (
+        <>
+          <FieldLabelContainer>
+            <FieldLabel color={color} help={buttonHelp}>
+              LÄGG TILL
+            </FieldLabel>
+          </FieldLabelContainer>
+          <ScrollView horizontal>
+            {items.map((item: Record<string, any>, index: number) => (
+              <SubstepButton
+                key={`${index}-${item.title}`}
+                text={item.title}
+                iconName="add"
+                iconColor={colors.substepList[color].addButtonIconColor}
+                value={value[item.key] || {}}
+                color={colors.substepList[color].addButtonColor}
+                onChange={updateAnswer(item.key)}
+                formId={item.formId}
+                size="medium"
+              />
+            ))}
+          </ScrollView>
+        </>
+      )}
+    </Wrapper>
+  );
+};
+
+SubstepList.propTypes = {
+  /**
+   * The header text of the list.
+   */
+  heading: PropTypes.string,
+  /**
+   * List of all items, corresponding to all subforms
+   */
+  items: PropTypes.array,
+  /**
+   * The categories of the grouping
+   */
+  categories: PropTypes.array,
+  /**
+   * The values of the entire list object
+   */
+  value: PropTypes.object,
+  /**
+   * What should happen to update the values
+   */
+  onChange: PropTypes.func,
+  /**
+   * If the list acts as a summary; default is false.
+   */
+  summary: PropTypes.bool,
+  /**
+   * Sets the color scheme of the list. default is red.
+   */
+  color: PropTypes.string,
+  /** help-object for the questionmark on the label above the buttons to add things. */
+  buttonHelpUrl: PropTypes.string,
+  /** help-object for the questionmark on the label above the buttons to add things. */
+  buttonHelpText: PropTypes.string,
+  /** help-object for the questionmark on the label above the buttons to add things. */
+  buttonHelpHeading: PropTypes.string,
+  /** help-object for the questionmark on the label above the buttons to add things. */
+  buttonHelpTagline: PropTypes.string,
+};
+
+SubstepList.defaultProps = {
+  items: [],
+  summary: false,
+  color: 'red',
+  onChange: () => {},
+};
+export default SubstepList;
---
+++ source/components/organisms/SubstepList/SubstepListItem.tsx
@@ -0,0 +1,161 @@
+/* eslint-disable no-nested-ternary */
+import React from 'react';
+import { View } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
+import { Input, Text, Icon } from '../../atoms';
+import SubstepButton from '../../molecules/SubstepButton/SubstepButton';
+import colors from '../../../styles/colors';
+import { Item, ShownField } from './SubstepList';
+
+const ItemWrapper = styled(View)`
+  flex-direction: row;
+  align-items: flex-end;
+  height: 46px;
+`;
+const InputWrapper = styled.View`
+  align-items: center;
+  justify-content: flex-end;
+  flex: 1;
+  padding-left: 50px;
+`;
+const TextWrapper = styled.View`
+  align-items: flex-end;
+  justify-content: flex-end;
+  flex: 10;
+  padding-left: 0px;
+`;
+const SmallInput = styled(Input)`
+  height: 40px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+`;
+const SmallText = styled(Text)`
+  height: 40px;
+  font-size: 14px;
+  padding-top: 11px;
+  padding-bottom: 8px;
+  padding-left: 17px;
+`;
+const DeleteButton = styled(Icon)`
+  padding-top: 5px;
+  padding-left: 5px;
+  padding-right: 0px;
+  margin-left: 5px;
+  margin-right: 0px;
+  margin-bottom: 15px;
+  color: black;
+`;
+
+interface Props {
+  item: Item;
+  shownField: ShownField;
+  value: Record<string, any>;
+  updateAnswer: (data: Record<string, any>) => void;
+  changeFromInput: (text: string) => void;
+  removeItem: () => void;
+  color: string;
+  editable: boolean;
+  summary: boolean;
+}
+
+const SubstepListItem: React.FC<Props> = ({
+  item,
+  shownField,
+  value,
+  updateAnswer,
+  changeFromInput,
+  removeItem,
+  color,
+  editable,
+  summary,
+}) => (
+  <ItemWrapper key={`${item.title}`}>
+    {editable && !summary ? (
+      <>
+        <SubstepButton
+          text={shownField.heading}
+          value={value[item.key] || {}}
+          onChange={updateAnswer}
+          formId={item.formId}
+          color={colors.substepList[color].listButtonColor}
+          size="small"
+        />
+        <InputWrapper>
+          <SmallInput
+            textAlign="right"
+            keyboardType="numeric"
+            value={value[item.key][shownField.fieldId]}
+            onChangeText={changeFromInput}
+          />
+        </InputWrapper>
+        <TouchableHighlight activeOpacity={1} onPress={removeItem}>
+          <DeleteButton name="clear" />
+        </TouchableHighlight>
+      </>
+    ) : editable && summary ? (
+      <>
+        <SmallText>{shownField.heading}</SmallText>
+        <TextWrapper>
+          <InputWrapper>
+            <SmallInput
+              textAlign="right"
+              keyboardType="numeric"
+              value={value[item.key][shownField.fieldId]}
+              onChangeText={changeFromInput}
+            />
+          </InputWrapper>
+        </TextWrapper>
+      </>
+    ) : (
+      <>
+        <SmallText>{shownField.heading}</SmallText>
+        <TextWrapper>
+          <SmallText>
+            {value[item.key][shownField.fieldId]
+              ? `${value[item.key][shownField.fieldId]} kr`
+              : '0 kr'}
+          </SmallText>
+        </TextWrapper>
+      </>
+    )}
+  </ItemWrapper>
+);
+
+SubstepListItem.propTypes = {
+  /**
+   * The header text of the list.
+   */
+  item: PropTypes.any,
+  shownField: PropTypes.any,
+  /**
+   * The values of the entire list object
+   */
+  value: PropTypes.object,
+  /**
+   * What should happen to update the values
+   */
+  updateAnswer: PropTypes.func,
+  changeFromInput: PropTypes.func,
+  removeItem: PropTypes.func,
+  /**
+   * If the list acts as a summary; default is false.
+   */
+  summary: PropTypes.bool,
+  /**
+   * If the list should be editable, default is false.
+   */
+  editable: PropTypes.bool,
+  /**
+   * Sets the color scheme of the list. default is red.
+   */
+  color: PropTypes.string,
+};
+SubstepListItem.defaultProps = {
+  summary: false,
+  editable: false,
+  color: 'light',
+  updateAnswer: () => {},
+};
+export default SubstepListItem;
