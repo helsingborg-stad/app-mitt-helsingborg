2163b0ebb975ef858b0da1fe9ee5dfb726163656
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-10-02 09:28:49 +0200

    Added images to slides. Fixed position for footer (pagination dots and next button).

--- source/screens/onboarding/components/Button.tsx
+++ source/screens/onboarding/components/Button.tsx
@@ -4,11 +4,13 @@ import { RectButton } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 25,
-    height: 50,
-    width: 245,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // borderRadius: 25,
+    // height: 50,
+    // width: 245,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    height: 20,
+    backgroundColor: 'transparent',
   },
   label: {
     fontSize: 15,
@@ -22,15 +24,13 @@ interface ButtonProps {
   onPress: () => void;
 }
 
-const Button = ({ label, variant, onPress }: ButtonProps) => {
-  const backgroundColor = variant === 'primary' ? '#2CB9B0' : 'rgba(12, 13, 52, 0.05)';
-  const color = variant === 'primary' ? 'white' : '#0C0D34';
-  return (
-    <RectButton style={[styles.container, { backgroundColor }]} {...{ onPress }}>
-      <Text style={[styles.label, { color }]}>{label}</Text>
-    </RectButton>
-  );
-};
+const Button = ({ label, variant, onPress }: ButtonProps) => (
+  // const backgroundColor = variant === 'primary' ? '#2CB9B0' : 'rgba(12, 13, 52, 0.05)';
+  // const color = variant === 'primary' ? 'white' : '#0C0D34';
+  <RectButton style={[styles.container]} {...{ onPress }}>
+    <Text style={[styles.label]}>{label}</Text>
+  </RectButton>
+);
 
 Button.defaultProps = { variant: 'default' };
 
