e16f07ce8d807cb2fe3e9cd2fd4982945e35efdf
Teddy Andersson <teddyandersson95@gmail.com>
2020-10-08 16:45:19 +0200

    feat: new component fieldset, for visually grouping inputs in a form

---
+++ source/components/atoms/Fieldset/Fieldset.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components/native";
+import { View } from "react-native";
+import Text from "../Text/Text";
+import Icon from "../Icon";
+
+const FieldsetContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+  border-radius: 9.5px;
+  overflow: hidden;
+  margin-bottom: 24px;
+  margin-top: 16px;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: #faeeec;
+`;
+
+const FieldsetHeader = styled.View`
+  padding-left: 4px;
+  position: relative;
+  flex-direction: row;
+`;
+const FieldsetHeaderSection = styled.View`
+  flex: 1;
+  flex-direction: column;
+  align-items: ${({ alignItems }) => alignItems};
+`;
+
+const FieldsetBody = styled.View``;
+
+const FieldsetIcon = styled(Icon)`
+  margin-top: -5px;
+`;
+
+const FieldsetLegend = styled(Text)`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.8);
+  padding-bottom: 12px;
+  font-weight: bold;
+`;
+
+const FieldsetLegendBorder = styled.View`
+  border-bottom-color: rgba(0,0,0,0.2);
+  border-bottom-width: 2px
+  align-self: flex-start;
+`;
+
+interface FieldsetProps {
+  children: React.ReactNode;
+  legend: string;
+  onIconPress: () => void;
+  iconName: string;
+}
+
+export default function Fieldset({
+  children,
+  legend,
+  onIconPress,
+  iconName,
+}: React.FC<FieldsetProps>) {
+  const showIcon = onIconPress && iconName;
+  return (
+    <FieldsetContainer>
+      <FieldsetHeader>
+        <FieldsetHeaderSection alignItems="flex-start">
+          <FieldsetLegendBorder>
+            <FieldsetLegend>{legend.toUpperCase()}</FieldsetLegend>
+          </FieldsetLegendBorder>
+        </FieldsetHeaderSection>
+
+        <FieldsetHeaderSection alignItems="flex-end">
+          {showIcon && (
+            <FieldsetIcon onPress={onIconPress} name={iconName} size={22} />
+          )}
+        </FieldsetHeaderSection>
+      </FieldsetHeader>
+      <FieldsetBody>{children}</FieldsetBody>
+    </FieldsetContainer>
+  );
+}
