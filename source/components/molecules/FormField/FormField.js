dba57c2729cafba9ac94f36d21b088885e03a2e2
Jacob Winding <jacobkwinding@gmail.com>
2020-10-15 17:15:49 +0200

    remove unused imports

--- source/components/molecules/FormField/FormField.js
+++ source/components/molecules/FormField/FormField.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Input, FieldLabel, Select, Text, Button } from 'source/components/atoms';
+import { Input, FieldLabel, Select, Text } from 'source/components/atoms';
 import { CheckboxField, EditableList, GroupListWithAvatar } from 'source/components/molecules';
 import SubstepList from 'source/components/organisms/SubstepList';
 import { View } from 'react-native';
 import ConditionalTextField from 'app/components/molecules/ConditinalTextField';
-import SubstepButton from '../SubstepButton';
 import colors from '../../../styles/colors';
 import DateTimePickerForm from '../DateTimePicker';
 import NavigationButtonField from '../NavigationButtonField/NavigationButtonField';
