1b2a4d14e0df159a0271ebc0259181b8a7ae5c65
Jacob Winding <jacobkwinding@gmail.com>
2020-10-15 13:09:22 +0200

    remove usage of substep modal

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -39,6 +39,7 @@ function loadStories() {
 	require('../source/components/organisms/FormList/FormList.stories');
 	require('../source/components/organisms/Step/Step.stories');
 	require('../source/components/organisms/SubstepList/SubstepList.stories');
+	require('../source/components/organisms/SummaryList/SummaryList.stories');
 	require('../source/components/organisms/WatsonAgent/WatsonAgent.stories');
 }
 
@@ -78,6 +79,7 @@ const stories = [
 	'../source/components/organisms/FormList/FormList.stories',
 	'../source/components/organisms/Step/Step.stories',
 	'../source/components/organisms/SubstepList/SubstepList.stories',
+	'../source/components/organisms/SummaryList/SummaryList.stories',
 	'../source/components/organisms/WatsonAgent/WatsonAgent.stories'
 ];
 
