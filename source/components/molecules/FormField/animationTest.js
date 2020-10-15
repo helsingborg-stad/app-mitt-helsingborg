be01ac398180517114be404a3f2f838539a63dda
Jacob Winding <jacobkwinding@gmail.com>
2020-10-15 13:27:07 +0200

    remove references to ButtonField and update storyloader

---
+++ source/components/molecules/FormField/animationTest.js
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import { Animated, Text, View } from 'react-native';
+
+const AppearView = props => {
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+  React.useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+    }).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View // Special animatable View
+      style={{
+        ...props.style,
+        transform: [{ scale: fadeAnim }], // Bind opacity to animated value
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+};
+
+// You can then use your `FadeInView` in place of a `View` in your components:
+export default AppearView;
