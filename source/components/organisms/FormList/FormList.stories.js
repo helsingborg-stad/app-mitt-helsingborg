570eb1502060374a10fdd0d119753e4343fbe88b
Jacob Winding <jacobkwinding@gmail.com>
2020-10-15 13:09:22 +0200

    remove usage of substep modal

--- source/components/organisms/FormList/FormList.stories.js
+++ source/components/organisms/FormList/FormList.stories.js
@@ -1,27 +1,16 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react-native';
-import { SubstepModal } from 'app/components/molecules';
 import StoryWrapper from '../../molecules/StoryWrapper';
 import FormList from './FormList';
 
 const FormListStory = () => {
-  const [modalVisible, setModalVisible] = useState(false);
   const [formId, setFormId] = useState('');
   return (
-    <>
-      <FormList
-        onClickCallback={async id => {
-          setFormId(id);
-          setModalVisible(true);
-        }}
-      />
-      <SubstepModal
-        visible={modalVisible}
-        setVisible={setModalVisible}
-        value={{}}
-        formId={formId}
-      />
-    </>
+    <FormList
+      onClickCallback={async id => {
+        setFormId(id);
+      }}
+    />
   );
 };
 
