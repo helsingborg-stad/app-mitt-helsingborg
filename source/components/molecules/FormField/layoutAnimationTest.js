957198c0edde05792ac081e80c33e95d9b9b24c6
Jacob Winding <jacobkwinding@gmail.com>
2020-10-16 13:18:33 +0200

    Revert "remove experiment files"
    This reverts commit 6853cc0cb8dfc34b464e1b0c61a956e03e87f249.

---
+++ source/components/molecules/FormField/layoutAnimationTest.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import {
+  LayoutAnimation,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  UIManager,
+  View,
+} from 'react-native';
+
+if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+const App = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <View style={style.container}>
+      <TouchableOpacity
+        onPress={() => {
+          LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+          setExpanded(!expanded);
+        }}
+      >
+        <Text>Press me to {expanded ? 'collapse' : 'expand'}!</Text>
+      </TouchableOpacity>
+      {expanded && (
+        <View style={style.tile}>
+          <Text>I disappear sometimes!</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  tile: {
+    background: 'lightGrey',
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+});
+
+export default App;
