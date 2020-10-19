a8fdbf29d634dfa02bd955562de80314b2fd1bd5
Jacob Winding <jacobkwinding@gmail.com>
2020-10-19 09:04:16 +0200

    Add layout animation to form field components

--- source/components/molecules/FormField/FormField.js
+++ source/components/molecules/FormField/FormField.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Input, FieldLabel, Select, Text } from 'source/components/atoms';
 import { CheckboxField, EditableList, GroupListWithAvatar } from 'source/components/molecules';
 import SubstepList from 'source/components/organisms/SubstepList';
-import { View } from 'react-native';
+import { View, LayoutAnimation } from 'react-native';
 import ConditionalTextField from 'app/components/molecules/ConditinalTextField';
 import colors from '../../../styles/colors';
 import DateTimePickerForm from '../DateTimePicker';
@@ -136,6 +136,7 @@ const FormField = props => {
     );
 
   if (checkCondition(conditionalOn)) {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     return (
       <View>
         {label ? (
