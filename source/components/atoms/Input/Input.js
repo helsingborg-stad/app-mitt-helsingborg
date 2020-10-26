2286a71fc8fa3c05139a48670efcf7cbabfa0637
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-10-26 08:23:23 +0100

    Created validation helper and added validation checks for input component.

--- source/components/atoms/Input/Input.js
+++ source/components/atoms/Input/Input.js
@@ -3,12 +3,13 @@ import { TextInput } from 'react-native';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components/native';
 import colors from '../../../styles/colors';
+import { validateInput } from '../../../helpers/ValidationHelper';
 
 const input = css`
   width: 100%;
   background-color: ${({ color }) => colors.input[color].background};
   border-radius: 17.5px;
-  border: solid 1px ${({ color }) => colors.input[color].border};
+  border: solid 1px ${({ color, isValid }) => isValid ? colors.input[color].border : colors.input['red'].border};
   padding-top: 16px;
   padding-bottom: 16px;
   padding-left: 16px;
@@ -23,9 +24,12 @@ const inputText = styled(TextInput)`
 `;
 
 const Input = props => {
-  const { color } = props;
+  const { color, validation } = props;
+  const isValid = validation.isRequired ? validateInput(props.value, validation.rules)[0] : true;
+
   return React.createElement(inputText, {
     placeholderTextColor: colors.input[color].placeholder,
+    isValid,
     ...props,
   });
 };
