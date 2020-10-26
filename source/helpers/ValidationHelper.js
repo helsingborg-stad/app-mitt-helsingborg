2bda2e4cb9a31ac19d07d653eb2913f3debb23ca
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-10-26 08:23:23 +0100

    Created validation helper and added validation checks for input component.

--- source/helpers/ValidationHelper.js
+++ source/helpers/ValidationHelper.js
@@ -1,3 +1,5 @@
+import validator from 'validator';
+
 /**
  * Checks if a string is a valid personal identity number
  * @param {string} pin Personal identity number
@@ -26,3 +28,42 @@ export const sanitizePin = pin => {
 
   return sanitizedPin;
 };
+
+export const validateInput = (value, rules) =>
+  rules.reduce(
+    (acc, rule) => {
+      const [valid] = acc;
+      let valueArray = acc;
+
+      /**
+       * Validator only accepts strings.
+       */
+      const valueToValidate = String(value);
+
+      /**
+       * Retrive the validation method defined in the rule from the validator.js package and execute
+       */
+      const validationMethodArgs = rule.args || [];
+      const arrayOfArgs = Object.keys(validationMethodArgs).map(key => validationMethodArgs[key]);
+      const validationMethod =
+        typeof rule.method === 'string' ? validator[rule.method] : rule.method;
+      const isValidationRuleMeet =
+        validationMethod(valueToValidate, arrayOfArgs) === rule.validWhen;
+
+      /**
+       * Only return true if the current and previous rule is met
+       */
+      if (valid === true && isValidationRuleMeet) {
+        valueArray = [true, ''];
+      }
+
+      /**
+       * Only change the  true if the current and previous rule is met
+       */
+      if (!isValidationRuleMeet) {
+        valueArray = [false, rule.message];
+      }
+      return valueArray;
+    },
+    [true, '']
+  );
