50772063bc61e8afcaf4c8a9226bc739c628c897
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-10-27 17:47:35 +0100

    Fixed eslint error.

--- source/components/atoms/Input/Input.js
+++ source/components/atoms/Input/Input.js
@@ -8,7 +8,9 @@ const input = css`
   width: 100%;
   background-color: ${({ color }) => colors.input[color].background};
   border-radius: 17.5px;
-  border: solid 1px ${({ color, error }) => error?.isValid || error === undefined ? colors.input[color].border : colors.input['red'].border};
+  border: solid 1px
+    ${({ color, error }) =>
+      error?.isValid || error === undefined ? colors.input[color].border : colors.input.red.border};
   padding-top: 16px;
   padding-bottom: 16px;
   padding-left: 16px;
