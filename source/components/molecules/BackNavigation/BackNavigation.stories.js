6a81739ff8bda337f5b88fb9fec99b5e760bb03f
Teddy Andersson <teddyandersson95@gmail.com>
2020-10-29 11:43:57 +0100

    style: added colorSchema to back navigation

--- source/components/molecules/BackNavigation/BackNavigation.stories.js
+++ source/components/molecules/BackNavigation/BackNavigation.stories.js
@@ -3,14 +3,37 @@ import { storiesOf } from '@storybook/react-native';
 import StoryWrapper from '../StoryWrapper';
 import BackNavigation from './BackNavigation';
 
-storiesOf('BackNavigation', module)
-  .add('Default', props => (
-    <StoryWrapper {...props}>
-      <BackNavigation />
-    </StoryWrapper>
-  ))
-  .add('Hide Back Button', props => (
-    <StoryWrapper {...props}>
-      <BackNavigation isBackBtnVisible={false} />
-    </StoryWrapper>
-  ));
+const backNavigationStories = storiesOf('BackNavigation', module);
+
+backNavigationStories.add('Default', props => (
+  <StoryWrapper {...props}>
+    <BackNavigation />
+  </StoryWrapper>
+));
+
+backNavigationStories.add('Hide Back Button', props => (
+  <StoryWrapper {...props}>
+    <BackNavigation showBackButton={false} />
+  </StoryWrapper>
+));
+
+backNavigationStories.add('Color Schema Blue', props => (
+  <StoryWrapper {...props}>
+    <BackNavigation isBackBtnVisible={false} />
+  </StoryWrapper>
+));
+backNavigationStories.add('Color Schema Purple', props => (
+  <StoryWrapper {...props}>
+    <BackNavigation colorSchema="purple" isBackBtnVisible={false} />
+  </StoryWrapper>
+));
+backNavigationStories.add('Color Schema Green', props => (
+  <StoryWrapper {...props}>
+    <BackNavigation colorSchema="green" isBackBtnVisible={false} />
+  </StoryWrapper>
+));
+backNavigationStories.add('Color Schema Red', props => (
+  <StoryWrapper {...props}>
+    <BackNavigation colorSchema="red" isBackBtnVisible={false} />
+  </StoryWrapper>
+));
