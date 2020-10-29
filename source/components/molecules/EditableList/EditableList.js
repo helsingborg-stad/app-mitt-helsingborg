c0c0db2f597984e1b908a011b0e66fe883a972fd
Teddy Andersson <teddyandersson95@gmail.com>
2020-10-29 15:00:23 +0100

    style: new styling and theming on editable list component

--- source/components/molecules/EditableList/EditableList.js
+++ source/components/molecules/EditableList/EditableList.js
@@ -1,69 +1,32 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
-import { Text, Button } from '../../atoms';
+import { Text, Button, Fieldset } from '../../atoms';
 
 /**
  * EditableList
  * A Molecule Component to use for rendering a list with the possibility of editing the list values.
  */
 
-const EditableListWrapper = styled.View`
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-  height: auto;
-
-  background: ${props => props.bg};
-  border-radius: 9.5px;
-  overflow: hidden;
-  margin-bottom: 16px;
-  margin-top: 16px;
-`;
-
-const EditableListHeader = styled.View`
-  background: ${props => props.bg};
-  color: ${props => props.color};
-  padding-left: 24px;
-  padding-right: 12px;
-  padding-top: 12px;
-  padding-bottom: 10px;
-  position: relative;
-  flex-direction: row;
-  align-items: center;
-  min-height: 58px;
-`;
-
-const HeaderButtonWrapper = styled.View`
-  justify-content: flex-end;
-  flex-direction: row;
-  flex: 1;
-`;
-
-const HeaderTitleWrapper = styled.View`
-  flex-direction: row;
-  flex: 1;
-`;
-
-const HeaderTitle = styled(Text)`
-  font-weight: 900;
-  font-size: 14px;
-  letter-spacing: 0.8px;
-  text-transform: uppercase;
-`;
-
 const EditableListBody = styled.View`
-  padding-top: 12px;
-  padding-bottom: 20px;
-  padding-left: 20px;
-  padding-right: 20px;
+  padding-top: 33px;
   height: auto;
 `;
 
 const EditableListItem = styled.View`
+  font-size: ${props => props.theme.fontSizes[4]}px;
   flex-direction: row;
-  height: 38px;
+  height: auto;
+  background-color: transparent;
+  border-radius: 4.5px;
+  margin-bottom: 10px;
+  ${props =>
+    props.editable
+      ? `
+      background-color: ${props.theme.colors.complementary[props.colorSchema][2]};
+      padding: 10px;
+      `
+      : 'color: blue;'};
 `;
 
 const EditableListItemLabelWrapper = styled.View`
@@ -72,9 +35,9 @@ const EditableListItemLabelWrapper = styled.View`
 `;
 
 const EditableListItemLabel = styled.Text`
-  font-size: 16px;
-  font-weight: normal;
-  color: ${props => props.color};
+  padding: 4px;
+  font-weight: ${props => props.theme.fontWeights[1]};
+  color: ${props => props.theme.colors.neutrals[1]};
 `;
 
 const EditableListItemInputWrapper = styled.View`
@@ -88,10 +51,13 @@ const EditableListItemInputWrapper = styled.View`
 const EditableListItemInput = styled.TextInput`
   text-align: right;
   min-width: 80%;
-  font-size: 16px;
-  font-weight: bold;
-  color: ${props => props.color};
+  font-weight: 500;
+  color: ${props => props.theme.colors.neutrals[1]};
   padding: 6px;
+`;
+
+const FieldsetButton = styled(Button)`
+  margin-left: 26px;
 `;
 
 const getInitialState = (inputs, value) => {
@@ -101,7 +67,7 @@ const getInitialState = (inputs, value) => {
   return inputs.reduce((prev, current) => ({ ...prev, [current.key]: current.value }), {});
 };
 
-function EditableList({ theme, title, inputs, value, onInputChange, inputIsEditable }) {
+function EditableList({ colorSchema, title, inputs, value, onInputChange, inputIsEditable }) {
   const [editable, setEditable] = useState(false);
   const [state, setState] = useState(getInitialState(inputs, value));
 
@@ -114,41 +80,29 @@ function EditableList({ theme, title, inputs, value, onInputChange, inputIsEdita
     setState(updatedState);
   };
 
-  const editStyle = {
-    borderColor: 'gray',
-    borderRadius: 4,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    backgroundColor: 'white',
-  };
-
   return (
-    <EditableListWrapper bg={theme.list.bg}>
-      <EditableListHeader {...theme.list.header}>
-        <HeaderTitleWrapper>
-          <HeaderTitle> {title}</HeaderTitle>
-        </HeaderTitleWrapper>
-        {inputIsEditable && (
-          <HeaderButtonWrapper>
-            <Button z={0} size="small" onClick={changeEditable}>
-              <Text>{editable ? 'Spara' : 'Ändra'}</Text>
-            </Button>
-          </HeaderButtonWrapper>
-        )}
-      </EditableListHeader>
-
+    <Fieldset
+      colorSchema={colorSchema}
+      legend={title}
+      onIconPress={() => console.log('Icon is pressed')}
+      iconName="help-outline"
+      renderHeaderActions={() =>
+        inputIsEditable && (
+          <FieldsetButton colorSchema={colorSchema} z={0} size="small" onClick={changeEditable}>
+            <Text>{editable ? 'Färdig' : 'Ändra'}</Text>
+          </FieldsetButton>
+        )
+      }
+    >
       <EditableListBody>
         {inputs.map(input => (
-          <EditableListItem key={input.key}>
+          <EditableListItem colorSchema={colorSchema} editable={editable} key={input.key}>
             <EditableListItemLabelWrapper>
-              <EditableListItemLabel color={theme.list.item.label.color}>
-                {input.label}
-              </EditableListItemLabel>
+              <EditableListItemLabel>{input.label}</EditableListItemLabel>
             </EditableListItemLabelWrapper>
             <EditableListItemInputWrapper>
               <EditableListItemInput
-                style={editable ? editStyle : {}}
-                color={theme.list.item.input.color}
+                colorSchema={colorSchema}
                 editable={editable}
                 onChangeText={text => onChange(input.key, text)}
                 value={value && value !== '' ? value[input.key] : state[input.key]}
@@ -157,7 +111,7 @@ function EditableList({ theme, title, inputs, value, onInputChange, inputIsEdita
           </EditableListItem>
         ))}
       </EditableListBody>
-    </EditableListWrapper>
+    </Fieldset>
   );
 }
 
@@ -192,46 +146,15 @@ EditableList.propTypes = {
   inputIsEditable: PropTypes.bool,
 
   /**
-   * The theming of the component
+   * The color schema/theme of the component
    */
-  theme: PropTypes.shape({
-    list: PropTypes.shape({
-      bg: PropTypes.string,
-      header: PropTypes.shape({
-        bg: PropTypes.string,
-        color: PropTypes.string,
-      }),
-      item: PropTypes.shape({
-        label: PropTypes.shape({
-          color: PropTypes.string,
-        }),
-        input: PropTypes.shape({
-          color: PropTypes.string,
-        }),
-      }),
-    }),
-  }),
+  colorSchema: PropTypes.oneOf('blue', 'green', 'red', 'purple'),
 };
+
 EditableList.defaultProps = {
   inputIsEditable: true,
   inputs: [],
-  theme: {
-    list: {
-      bg: '#fbf7f0',
-      header: {
-        bg: '#f5e0d8',
-        color: '#5C3D38',
-      },
-      item: {
-        label: {
-          color: '#855851',
-        },
-        input: {
-          color: '#00213f',
-        },
-      },
-    },
-  },
+  colorSchema: 'blue',
 };
 
 export default EditableList;
