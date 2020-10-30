ec79f00f85b0b49fa3bf82c01ca6e0e8301ef1f5
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-10-30 09:31:51 +0100

    home screen fixes

--- source/screens/HomeScreen.js
+++ source/screens/HomeScreen.js
@@ -45,66 +45,53 @@ const HomeScreen = ({ navigation }) => {
 
   return (
     <>
-      <ChatScreenWrapper>
-        {showChat && (
-          <Chat
-            ChatAgent={props => <WatsonAgent {...props} initialMessages="remote" />}
-            inputComponents={{
-              type: 'text',
-              placeholder: 'Skriv något...',
-              autoFocus: false,
-              display: 'none',
-            }}
-            ChatUserInput={false}
-            keyboardVerticalOffset={0}
-            isInputVisible={isInputVisible}
-          />
-        )}
-
-        <View style={{ padding: 20, marginTop: 40, height: '73%' }}>
-          <FormList
-            heading="Ansökningsformulär"
-            onClickCallback={async form => {
-              createCase(
-                form,
-                async newCase => {
-                  navigation.navigate('Form', { caseData: newCase });
-                },
-                true
-              );
-            }}
-          />
-        </View>
-
-        <ButtonContainer>
-          {showChat ? (
-            <HomeScreenButton color="purpleLight" onClick={() => toggleInput()} block>
-              <Text>Ställ en fråga</Text>
-            </HomeScreenButton>
-          ) : null}
-          {/* <HomeScreenButton
-            color="purple"
-            block
-            onClick={() => {
-              createCase(recurringFormId, newCase => {
+      {showChat && (
+        <Chat
+          ChatAgent={props => <WatsonAgent {...props} initialMessages="remote" />}
+          inputComponents={{
+            type: 'text',
+            placeholder: 'Skriv något...',
+            autoFocus: false,
+            display: 'none',
+          }}
+          ChatUserInput={false}
+          keyboardVerticalOffset={0}
+          isInputVisible={isInputVisible}
+        />
+      )}
+      <View style={{ padding: 20, marginTop: 40, height: '73%' }}>
+        <FormList
+          heading="Ansökningsformulär"
+          onClickCallback={async form => {
+            createCase(
+              form,
+              async newCase => {
                 navigation.navigate('Form', { caseData: newCase });
-              });
-            }}
-          >
-            <Text>Starta ny Ekonomiskt Bistånd ansökan</Text>
-          </HomeScreenButton> */}
-          {/* <Button
-            color="purple"
-            block
-            style={styles.button}
-            onClick={() => {
-              navigation.navigate('Form', { caseData: { hello: 'world' } });
-            }}
-          >
-            <Text>Fortsätt senaste ansökan</Text>
-          </Button> */}
-        </ButtonContainer>
-      </ChatScreenWrapper>
+              },
+              true
+            );
+          }}
+        />
+      </View>
+
+      <ButtonContainer>
+        {showChat ? (
+          <HomeScreenButton color="purpleLight" onClick={() => toggleInput()} block>
+            <Text>Ställ en fråga</Text>
+          </HomeScreenButton>
+        ) : null}
+        {/* <HomeScreenButton
+          color="purple"
+          block
+          onClick={() => {
+            createCase(recurringFormId, newCase => {
+              navigation.navigate('Form', { caseData: newCase });
+            });
+          }}
+        >
+          <Text>Starta ny Ekonomiskt Bistånd ansökan</Text>
+        </HomeScreenButton> */}
+      </ButtonContainer>
     </>
   );
 };
