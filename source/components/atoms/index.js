4ee77c77b061144dedb688ffd451e22398944ae3
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-11-04 21:31:06 +0100

    feat: 🎸 add initial files and code
    Generic box atom component

--- source/components/atoms/index.js
+++ source/components/atoms/index.js
@@ -1,3 +1,4 @@
+export { default as Box } from './Box';
 export { default as Button } from './Button';
 export { default as Fieldset } from './Fieldset';
 export { default as Heading } from './Heading';
