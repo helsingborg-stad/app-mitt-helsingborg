2eb95cdad0cb6bacfc68fe6c52f74341afa71f19
Jacob Winding <jacobkwinding@gmail.com>
2020-11-06 16:14:14 +0100

    Update some things in the story

--- source/components/molecules/CheckboxField/CheckboxField.stories.js
+++ source/components/molecules/CheckboxField/CheckboxField.stories.js
@@ -29,49 +29,47 @@ const CheckboxFields = () => {
   return (
     <View>
       <CheckboxField
-        text="Do you feel it? Light, small"
-        color="light"
+        text="Do you feel it? Blue theme, small"
+        color="blue"
         size="small"
-        checked={checkboxValues.box1}
+        value={checkboxValues.box1}
         onChange={handleChange('box1')}
       />
       <CheckboxField
         text="Does it feel good? Can I ask longer questions here? How does it handle long strings?"
-        color="gray"
+        color="red"
         size="small"
-        checked={checkboxValues.box2}
+        value={checkboxValues.box2}
         onChange={handleChange('box2')}
       />
       <CheckboxField
-        text="Is it happening? Light, medium"
-        color="light"
+        text="Is it happening? Green theme, medium"
+        color="green"
         size="medium"
-        checked={checkboxValues.box5}
+        value={checkboxValues.box5}
         onChange={handleChange('box5')}
       />
-      <View backgroundColor="rgb(29,32,37)">
-        <CheckboxField
-          text="Do you feel it? Dark theme"
-          color="dark"
-          size="small"
-          checked={checkboxValues.box3}
-          onChange={handleChange('box3')}
-        />
-        <CheckboxField
-          text="Is it happening? Blue theme"
-          color="blue"
-          size="large"
-          checked={checkboxValues.box4}
-          onChange={handleChange('box4')}
-        />
-        <CheckboxField
-          text="Is it happening? Blue theme"
-          color="blue"
-          size="large"
-          checked={checkboxValues.box6}
-          onChange={handleChange('box6')}
-        />
-      </View>
+      <CheckboxField
+        text="Do you feel it? Purple theme, small"
+        color="purple"
+        size="small"
+        value={checkboxValues.box3}
+        onChange={handleChange('box3')}
+      />
+      <CheckboxField
+        text="Is it happening? Red theme, large"
+        color="red"
+        size="large"
+        value={checkboxValues.box4}
+        onChange={handleChange('box4')}
+      />
+      <CheckboxField
+        text="Is it happening? Green theme, large"
+        color="green"
+        size="large"
+        value={checkboxValues.box6}
+        onChange={handleChange('box6')}
+      />
     </View>
   );
 };
