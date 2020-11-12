1b6d4e6be8a056f085e78454caf20e50e0bab46a
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-11-12 16:35:59 +0100

    cloned children and add color prop

--- source/components/molecules/Card/Card.stories.js
+++ source/components/molecules/Card/Card.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import { Text, Icon } from 'app/components/atoms';
 import StoryWrapper from '../StoryWrapper';
@@ -7,12 +7,29 @@ import Card from './Card';
 
 storiesOf('Card', module).add('default', () => (
   <StoryWrapper>
-    <View style={{ backgroundColor: '#efefef', flex: 1, padding: 16 }}>
-      <Text strong>Aktuell period</Text>
+    <ScrollView style={{ backgroundColor: '#efefef', flex: 1, padding: 16 }}>
+      <Text style={{ paddingBottom: 8 }} strong>
+        Aktiva
+      </Text>
+
+      <Card colorSchema="red">
+        <Card.Body>
+          {/* <Card.Image src={movie.image} alt={movie.title} /> */}
+          <Card.Title>Ekonomiskt bistånd</Card.Title>
+          <Card.SubTitle>Ofullständig</Card.SubTitle>
+          <Card.Button>
+            <Text>Ange hyra</Text>
+            <Icon name="arrow-forward" />
+          </Card.Button>
+        </Card.Body>
+      </Card>
+
+      <Text style={{ paddingBottom: 8 }} strong>
+        Aktuell period
+      </Text>
 
       <Card>
         <Card.Body>
-          {/* <Card.Image src={movie.image} alt={movie.title} /> */}
           <Card.Title>Oktober</Card.Title>
           <Card.SubTitle>Ansökan inlämnad</Card.SubTitle>
           <Card.Text>Vi har mottagit din ansökan för perioden 1-31 oktober.</Card.Text>
@@ -24,20 +41,24 @@ storiesOf('Card', module).add('default', () => (
         </Card.Body>
       </Card>
 
-      <Text strong>Mina kontaktpersoner</Text>
+      <Text style={{ paddingBottom: 8 }} strong>
+        Mina kontaktpersoner
+      </Text>
 
       <Card>
         <Card.Body>
-          <Card.Title>Title</Card.Title>
-          <Card.Text>Text here</Card.Text>
+          <Card.Title>Anna Andersson</Card.Title>
+          <Card.SubTitle>Socialsekreterare</Card.SubTitle>
+          <Card.Text>042 - 52 52 52</Card.Text>
         </Card.Body>
       </Card>
 
-      <Text strong>Tidigare ansökningar</Text>
+      <Text style={{ paddingBottom: 8 }} strong>
+        Tidigare ansökningar
+      </Text>
 
       <Card>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>Text here</Card.Text>
+        <Card.Text italic>Här kan du titta på dina tidigare ansökningar.</Card.Text>
         <Card.Button>
           <Text>Link</Text>
           <Icon name="arrow-forward" />
@@ -47,6 +68,6 @@ storiesOf('Card', module).add('default', () => (
           <Icon name="arrow-forward" />
         </Card.Button>
       </Card>
-    </View>
+    </ScrollView>
   </StoryWrapper>
 ));
