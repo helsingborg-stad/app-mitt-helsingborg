b7a4d0d30cf722aeb3cc2f7dc700632f4782bea8
Ehsan Zilaei <zzilaei@gmail.com>
2020-11-12 08:30:27 +0100

    Added comments for ErrorHandler.

--- source/helpers/error-handler/ErrorHandler.tsx
+++ source/helpers/error-handler/ErrorHandler.tsx
@@ -3,6 +3,14 @@ import Config from 'react-native-config';
 import RNRestart from 'react-native-restart';
 import { string } from 'prop-types';
 
+/**
+ * Handler for boundary errors.
+ * This handler shall take care of boundary errors in both storybook, app in dev mode and app in release mode
+ * (run-xxx --configuration Release).
+ * Set .env flag "ENABLE_DEV_ERROR_BOUNDARY=true" to enable boundary errors in dev mode (non release builds of app).
+ *
+ * @param error Object with error message
+ */
 const boundaryErrorHandler = (error = { message: string }) => {
   if (Config.IS_STORYBOOK === 'true') {
     Alert.alert('Something went wrong in storybook', `${error.message}`, [{ text: 'OK' }]);
