d111136ae807c3567f3d96ec10b2c816bbf892d8
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-11-16 16:28:15 +0100

    Implemented neutral color scheme and added ability to set custom color for Body

--- source/components/molecules/Card/Card.js
+++ source/components/molecules/Card/Card.js
@@ -11,8 +11,22 @@ const Container = styled.View`
 `;
 
 const Body = styled.View`
+  background-color: ${props =>
+    props.colorSchema === 'neutral'
+      ? props.theme.colors.neutrals[7]
+      : props.theme.colors.complementary[props.colorSchema][3]};
+
+  ${props => {
+    if (props.color) {
+      switch (props.color) {
+        case 'neutral':
+          return `background-color: ${props.theme.colors.neutrals[7]};`;
+        default:
+          return `background-color: ${props.theme.colors.complementary[props.color][3]};`;
+      }
+    }
+  }}
   padding: 24px;
-  background-color: ${props => props.theme.colors.neutrals[7]};
   border-radius: 8px;
   display: flex;
   flex-direction: column;
@@ -20,32 +34,36 @@ const Body = styled.View`
   shadow-offset: 0px 2px;
   shadow-color: ${props => props.theme.colors.neutrals[4]};
   shadow-opacity: 0.5;
-  shadow-radius: 2;
+  shadow-radius: 2px;
 `;
 
 const CardHeading = styled(Heading)`
-  color: ${props => props.theme.colors.primary[props.colorSchema][0]};
+  color: ${props =>
+    props.colorSchema === 'neutral' ? 'black' : props.theme.colors.primary[props.colorSchema][0]};
 `;
 
 const CardSubTitle = styled(Text)`
-  color: ${props => props.theme.colors.primary[props.colorSchema][3]};
-  font-size: ${props => props.theme.fontSizes[2]};
+  color: ${props =>
+    props.colorSchema === 'neutral' ? 'black' : props.theme.colors.primary[props.colorSchema][3]};
+  font-size: ${props => props.theme.fontSizes[2]}px;
   font-weight: ${props => props.theme.fontWeights[1]};
 `;
 
 const CardText = styled(Text)`
-  font-size: ${props => props.theme.fontSizes[3]};
+  font-size: ${props => props.theme.fontSizes[3]}px;
   ${props => props.italic && `color: ${props.theme.colors.neutrals[3]};`}
 `;
 
 const Outset = styled.View`
   padding-top: 8px;
   padding-bottom: 8px;
-  ${props => props.firstChild && `padding-top: 0px;`}
-  ${props => props.lastChild && `padding-bottom: 0px;`}
+  ${({ firstChild }) => firstChild && `padding-top: 0px;`}
+  ${({ lastChild }) => lastChild && `padding-bottom: 0px;`}
 `;
 
-const CardImage = styled.Image``;
+const CardImage = styled.Image`
+  ${({ circle }) => circle && `border-radius: 40px;`}
+`;
 
 const Card = ({ children, colorSchema, ...props }) => {
   // Clone child elements and add additional props
@@ -60,7 +78,7 @@ const Card = ({ children, colorSchema, ...props }) => {
   return <Container {...props}>{childrenWithProps}</Container>;
 };
 
-Card.Body = ({ children, colorSchema, ...props }) => {
+Card.Body = ({ children, colorSchema, color, ...props }) => {
   const childrenWithProps = React.Children.map(children, (child, index) =>
     React.cloneElement(child, {
       colorSchema,
@@ -70,7 +88,7 @@ Card.Body = ({ children, colorSchema, ...props }) => {
   );
 
   return (
-    <Body colorSchema={colorSchema} {...props}>
+    <Body colorSchema={colorSchema} color={color} {...props}>
       {childrenWithProps}
     </Body>
   );
@@ -95,13 +113,17 @@ Card.Text = ({ children, lastChild, firstChild, ...props }) => (
 );
 
 // TODO: Implement new button variant "Link" when its done
-Card.Button = ({ children, colorSchema, firstChild, lastChild, ...props }) => (
-  <Outset lastChild={lastChild} firstChild={firstChild}>
-    <Button variant="outlined" colorSchema={colorSchema} size="small" block {...props}>
-      {children}
-    </Button>
-  </Outset>
-);
+Card.Button = ({ children, colorSchema, firstChild, lastChild, ...props }) => {
+  const buttonColors = ['blue', 'red', 'purple', 'green'];
+  const color = buttonColors.includes(colorSchema) ? colorSchema : buttonColors[0];
+  return (
+    <Outset lastChild={lastChild} firstChild={firstChild}>
+      <Button variant="outlined" colorSchema={color} size="small" block {...props}>
+        {children}
+      </Button>
+    </Outset>
+  );
+};
 
 Card.Image = ({ children, firstChild, lastChild, ...props }) => (
   <Outset lastChild={lastChild} firstChild={firstChild}>
@@ -111,24 +133,27 @@ Card.Image = ({ children, firstChild, lastChild, ...props }) => (
 
 Card.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
-  colorSchema: PropTypes.oneOf('blue', 'red', 'purple', 'green'),
+  colorSchema: PropTypes.oneOf(['neutral', 'blue', 'red', 'purple', 'green']),
 };
 
 Card.defaultProps = {
-  colorSchema: 'purple',
+  colorSchema: 'neutral',
 };
 
 Card.Body.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
-  colorSchema: PropTypes.oneOf('blue', 'red', 'purple', 'green'),
+  colorSchema: PropTypes.oneOf(['neutral', 'blue', 'red', 'purple', 'green']),
+  color: PropTypes.oneOf(['neutral', 'blue', 'red', 'purple', 'green']),
 };
 
 Card.Title.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  colorSchema: PropTypes.oneOf(['neutral', 'blue', 'red', 'purple', 'green']),
 };
 
 Card.SubTitle.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  colorSchema: PropTypes.oneOf(['neutral', 'blue', 'red', 'purple', 'green']),
 };
 
 Card.Text.propTypes = {
@@ -141,7 +166,7 @@ Card.Image.propTypes = {
 
 Card.Button.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
-  colorSchema: PropTypes.oneOf('blue', 'red', 'purple', 'green'),
+  colorSchema: PropTypes.oneOf(['blue', 'red', 'purple', 'green']),
 };
 
 export default Card;
