f2b59c706cf9bd1206f34573a32df947b8d869c3
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-11-17 15:23:49 +0100

    Added placeholder cards

--- source/screens/UserEvents.js
+++ source/screens/UserEvents.js
@@ -1,18 +1,19 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components/native';
 import { Button, Text, Heading, Box, Icon } from 'app/components/atoms';
-import { ScreenWrapper, Header } from 'app/components/molecules';
+import { ScreenWrapper, Header, Card } from 'app/components/molecules';
 import AuthContext from 'app/store/AuthContext';
 import PropTypes from 'prop-types';
+
+const ILLU_WALLET = require('source/assets/images/icons/icn_inkomster_red_1x.png');
 
 const Container = styled.ScrollView`
   flex: 1;
   padding: 16px;
 `;
 
-const Card = styled(Box)`
-  background-color: ${props => props.theme.colors.neutrals[7]};
-  border-color: ${props => props.theme.border.default};
+const CaseHeading = styled(Text)`
+  margin-bottom: 12px;
 `;
 
 function ProfileScreen(props) {
@@ -24,28 +25,44 @@ function ProfileScreen(props) {
     <ScreenWrapper>
       <Header title="Dina ärenden" />
       <Container>
-        <Text strong>Aktiva</Text>
-        <Card
-          borderStyle="solid"
-          borderWidth="1px"
-          borderRadius="8px"
-          mb="10px"
-          p="10px"
-          height="100px"
-        >
-          <Text>This box has a border radius</Text>
-          <Button colorSchema="red" block size="small" rounded>
-            <Text>Icon right</Text>
-            <Icon name="arrow-forward" />
-          </Button>
+        <Card colorSchema="red">
+          <Card.Body outlined>
+            <Card.Title>Hej!</Card.Title>
+            <Card.Text>
+              Helsingborgs Stad testar att göra självservice lite mer personlig och i första steget
+              så är det just Ekonomiskt Bistånd som står i fokus.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card colorSchema="red">
+          <Card.Body shadow color="neutral">
+            <Card.Image source={ILLU_WALLET} />
+            <Card.Title>Ekonomiskt{'\n'}bistånd</Card.Title>
+            <Card.Button>
+              <Text>Starta ansökan</Text>
+              <Icon name="arrow-forward" />
+            </Card.Button>
+          </Card.Body>
+        </Card>
+
+        <CaseHeading type="h5" strong>
+          Aktiva
+        </CaseHeading>
+        <Card colorSchema="red">
+          <Card.Body shadow color="neutral">
+            <Card.Image source={ILLU_WALLET} />
+            <Card.Title>Ekonomiskt{'\n'}bistånd</Card.Title>
+            <Card.SubTitle>Steg 3 / 8</Card.SubTitle>
+            <Card.Button>
+              <Text>Fortsätt ansökan</Text>
+              <Icon name="arrow-forward" />
+            </Card.Button>
+          </Card.Body>
         </Card>
       </Container>
     </ScreenWrapper>
   );
 }
-
-ProfileScreen.propTypes = {
-  navigation: PropTypes.object,
-};
 
 export default ProfileScreen;
