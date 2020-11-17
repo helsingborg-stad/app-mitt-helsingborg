d98050895806582fe5212bd83a757184654c9355
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-11-17 16:03:55 +0100

    Added a card with ability to start a new form

--- source/screens/UserEvents.js
+++ source/screens/UserEvents.js
@@ -1,10 +1,13 @@
+import PropTypes from 'prop-types';
+import { Icon, Text } from 'app/components/atoms';
+import { Card, Header, ScreenWrapper } from 'app/components/molecules';
+import FormContext from 'app/store/FormContext';
 import React, { useContext } from 'react';
 import styled from 'styled-components/native';
-import { Button, Text, Heading, Box, Icon } from 'app/components/atoms';
-import { ScreenWrapper, Header, Card } from 'app/components/molecules';
-import AuthContext from 'app/store/AuthContext';
-import PropTypes from 'prop-types';
+import { CaseDispatch } from 'app/store/CaseContext';
 
+// TODO: Temp fix, handle this better
+const FORM_ID = 'f94790e0-0c86-11eb-bf56-efbb7e9336b3';
 const ILLU_WALLET = require('source/assets/images/icons/icn_inkomster_red_1x.png');
 
 const Container = styled.ScrollView`
@@ -12,20 +15,20 @@ const Container = styled.ScrollView`
   padding: 16px;
 `;
 
-const CaseHeading = styled(Text)`
-  margin-bottom: 12px;
+const WelcomeMessage = styled(Card)`
+  margin-top: 32px;
+  margin-bottom: 32px;
 `;
 
-function ProfileScreen(props) {
-  const {
-    navigation: { navigate },
-  } = props;
+function ProfileScreen({ navigation }) {
+  const { createCase } = useContext(CaseDispatch);
+  const { getForm } = useContext(FormContext);
 
   return (
     <ScreenWrapper>
-      <Header title="Dina ärenden" />
+      <Header title="Mina ärenden" />
       <Container>
-        <Card colorSchema="red">
+        <WelcomeMessage colorSchema="red">
           <Card.Body outlined>
             <Card.Title>Hej!</Card.Title>
             <Card.Text>
@@ -33,29 +36,25 @@ function ProfileScreen(props) {
               så är det just Ekonomiskt Bistånd som står i fokus.
             </Card.Text>
           </Card.Body>
-        </Card>
+        </WelcomeMessage>
 
         <Card colorSchema="red">
           <Card.Body shadow color="neutral">
             <Card.Image source={ILLU_WALLET} />
             <Card.Title>Ekonomiskt{'\n'}bistånd</Card.Title>
-            <Card.Button>
+            <Card.Button
+              onClick={async () => {
+                const form = await getForm(FORM_ID);
+                createCase(
+                  form,
+                  async newCase => {
+                    navigation.navigate('Form', { caseData: newCase });
+                  },
+                  true
+                );
+              }}
+            >
               <Text>Starta ansökan</Text>
-              <Icon name="arrow-forward" />
-            </Card.Button>
-          </Card.Body>
-        </Card>
-
-        <CaseHeading type="h5" strong>
-          Aktiva
-        </CaseHeading>
-        <Card colorSchema="red">
-          <Card.Body shadow color="neutral">
-            <Card.Image source={ILLU_WALLET} />
-            <Card.Title>Ekonomiskt{'\n'}bistånd</Card.Title>
-            <Card.SubTitle>Steg 3 / 8</Card.SubTitle>
-            <Card.Button>
-              <Text>Fortsätt ansökan</Text>
               <Icon name="arrow-forward" />
             </Card.Button>
           </Card.Body>
@@ -64,5 +63,11 @@ function ProfileScreen(props) {
     </ScreenWrapper>
   );
 }
+
+ProfileScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }),
+};
 
 export default ProfileScreen;
