19f539d5685e88f25e1e7bfda96ce4086446606e
Ehsan Zilaei <zzilaei@gmail.com>
2020-12-09 14:30:41 +0100

    Text file with user agreement (markdown formatted).

---
+++ source/assets/text/userAgreementText.js
@@ -0,0 +1,56 @@
+/**
+ * Text for user agreement.
+ * This text is formatted with markdown.
+ *
+ * @type {string} Markdown formatted user agreement.
+ */
+const userAgreementText = `
+**Användaravtal**
+I texten nedan finns information om hur Helsingborgs stad (nedan Staden) behandlar dina personuppgifter när du använder appen Mitt Helsingborg.
+
+ 
+
+I appen Mitt Helsingborg får du tillgång till tjänster och information från kommunen, du kan följa dina ärenden, få personlig service och få svar på frågor om tjänsterna i appen. Appen innehåller också e-tjänster. Detaljerad information om den personuppgiftsbehandling som sker i respektive e-tjänst och vilken nämnd inom Staden som ansvarar för behandlingen finner du under rubriken ”Behandling av personuppgifter i e-tjänsten” när du startar respektive tjänst.
+
+
+
+**Varifrån får vi de personuppgifter som behandlas om dig när du använder appen?**
+För att använda appen Mitt Helsingborg måste du ha en e-legitimation, mobilt bank-ID. På webbplatsen e-legitimation.se kan du läsa mer om e-legitimation och hur du skaffar den.
+
+ 
+
+När du loggar in i appen första gången hämtar vi personlig information om dig, så som namn, personnummer och adress från Skatteverket och ber dig att bekräfta att denna information är korrekt. Om uppgifterna inte stämmer måste du ta kontakt med Skatteverket. Vill du komplettera med ytterligare information, så som e-postadress eller telefonnummer, gör du det genom att fylla i din personliga profil i appen. Den information som vi hämtar från Skatteverket tillsammans med de uppgifter du själv lämnar i appen kommer att föreslås i de e-tjänster som du använder i appen, för att underlätta din användning av våra tjänster. 
+
+ 
+
+Utöver den personliga information som anges ovan kan vi komma att automatiskt samla in och behandla följande information när du använder appen:
+  + Teknisk information inklusive ID för mobil, enhetstyp och valt språk.
+  + Information om tjänsterna som du använt hos oss, tidsåtgång, vilka etjänster och funktioner du har använt.
+
+Den rättsliga grunden för behandling av teknisk information enligt ovan är avtal med den registrerade.
+
+
+**Allmänna handlingar och bevarandetid**
+Tänk på att handlingar och uppgifter som du skickar in till Staden via appen blir allmänna handlingar här. Vem som helst kan begära att få ta del av dessa. Vi gör alltid en sekretessprövning enligt offentlighets- och sekretesslagen innan vi lämnar ut handlingar som innehåller personuppgifter. [Läs mer om allmänna handlingar.](https://helsingborg.se/kommun-och-politik/kontakt-och-paverkan/diarium-handlingar-och-protokoll/allmanna-handlingar/)
+Helsingborgs stad följer arkivlagen. Det finns dokumenthanteringsplaner hos nämnderna som anger vad som gäller om gallring. Där framgår om och när handlingar, som kan innehålla personuppgifter, får gallras (slängas).
+
+
+
+**Dina rättigheter**
+Du har rätt att få tillgång till de personuppgifter som rör dig och som finns hos förvaltningarna inom Helsingborgs stad. Den informationen får du i form av ett så kallat registerutdrag som beställs via helsingborg.se.
+Du kan under vissa förutsättningar dessutom ha rätt att
+  + få dina uppgifter raderade
+  + få felaktiga uppgifter rättade
+  + komplettera ofullständiga personuppgifter
+  + begära en begränsning av vår behandling av dina personuppgifter
+  + invända mot vår behandling av dina personuppgifter
+  
+[Läs mer om hur du utövar dina rättigheter och om hur Helsingborgs stad behandlar personuppgifter.](https://helsingborg.se/toppmeny/om-webbplatsen/sa-har-behandlar-vi-dina-personuppgifter/)
+Om du vill komma i kontakt med Stadens dataskyddsombud ring 042-10 50 00 (växel) eller mejla till dataskyddsombud@helsingborg.se 
+
+ 
+
+Om du vill klaga på vår behandling av dina personuppgifter kan du vända dig till Datainspektionen, [http://www.datainspektionen.se/](http://www.datainspektionen.se/)
+`;
+
+export default userAgreementText;
