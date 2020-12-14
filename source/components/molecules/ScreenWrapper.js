35e1a23e1d96989bc984979d24dfe0ee0356356f
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro-2.local>
2020-12-14 09:29:28 +0100

    refactor(component): ScreenWrapper now supports passing of additional props

--- source/components/molecules/ScreenWrapper.js
+++ source/components/molecules/ScreenWrapper.js
@@ -7,6 +7,16 @@ const ScreenWrapper = styled(KeyboardAwareScrollView)`
   background-color: ${(props) => props.theme.colors.neutrals[6]};
 `;
 
+const ScreenWrapper = props => {
+  const { style, children, ...rest } = props;
+
+  return (
+    <Container style={style} {...rest}>
+      {children}
+    </Container>
+  );
+};
+
 ScreenWrapper.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
   style: PropTypes.array,
