7560f96bcbb32ad50e579fc5649a046500e2cd26
Jacob Winding <jacobkwinding@gmail.com>
2020-12-29 14:36:14 +0100

    NavigationButtonGroup: fix so that colorSchema works

--- source/components/molecules/NavigationButtonGroup/NavigationButtonGroup.tsx
+++ source/components/molecules/NavigationButtonGroup/NavigationButtonGroup.tsx
@@ -4,6 +4,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 import NavigationButtonField, {
   Props as ButtonProps,
 } from '../NavigationButtonField/NavigationButtonField';
+import { PrimaryColor } from '../../../styles/themeHelpers';
 
 interface Props {
   buttons: ButtonProps[];
@@ -14,12 +15,22 @@ interface Props {
     down: (targetStepId: string) => void;
     up: () => void;
   };
+  colorSchema?: PrimaryColor;
 }
 
-const NavigationButtonGroup: React.FC<Props> = ({ buttons, horizontal, formNavigation }) => (
+const NavigationButtonGroup: React.FC<Props> = ({
+  buttons,
+  horizontal,
+  formNavigation,
+  colorSchema,
+}) => (
   <ScrollView horizontal={horizontal}>
     {buttons.map((button, index) => (
-      <NavigationButtonField {...button} formNavigation={formNavigation} key={`button${index}`} />
+      <NavigationButtonField
+        {...{ colorSchema, ...button }}
+        formNavigation={formNavigation}
+        key={`button${index}`}
+      />
     ))}
   </ScrollView>
 );
@@ -28,6 +39,7 @@ NavigationButtonGroup.propTypes = {
   horizontal: PropTypes.bool,
   buttons: PropTypes.array,
   formNavigation: PropTypes.any,
+  colorSchema: PropTypes.oneOf(['blue', 'red', 'green', 'purple', 'neutral']),
 };
 
 export default NavigationButtonGroup;
