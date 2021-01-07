f21ae4b238bd55f72840378a70636641b5545ec0
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-01-07 22:36:12 +0100

    Implemented useModal hook in modal story

--- source/components/molecules/Modal/Modal.stories.js
+++ source/components/molecules/Modal/Modal.stories.js
@@ -1,29 +1,30 @@
-import React, { Component, useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import styled from 'styled-components/native';
+import { Modal, useModal } from './';
 import StoryWrapper from '../StoryWrapper';
-import Modal from './Modal';
 import Button from '../../atoms/Button/Button';
 import Text from '../../atoms/Text';
 
 const ModalButton = styled(Button)`
   margin-top: 24px;
 `;
+
 const ModalExample = () => {
-  const [visible, setVisible] = useState(false);
+  const [visible, toggleModal] = useModal();
 
   return (
     <View>
-      <Modal color="purple" visible={visible} setVisibility={(isVisible) => setVisible(isVisible)}>
+      <Modal color="purple" visible={visible} hide={toggleModal}>
         <Text>Children 1</Text>
         <Text>Children 2</Text>
-        <Button onClick={() => setVisible(false)}>
+        <Button onClick={toggleModal}>
           <Text>Close</Text>
         </Button>
       </Modal>
 
-      <ModalButton color="purple" onClick={() => setVisible(true)}>
+      <ModalButton color="purple" onClick={toggleModal}>
         <Text>Show modal</Text>
       </ModalButton>
     </View>
