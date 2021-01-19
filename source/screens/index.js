936722257f495fcae063a6ae8b5d99b68c6028e8
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-01-19 16:38:08 +0100

    export dev features screen

--- source/screens/index.js
+++ source/screens/index.js
@@ -1,5 +1,6 @@
+export { default as DevFeaturesScreen } from './DevFeaturesScreen';
+export { default as FormCaseScreen } from './FormCaseScreen';
+export { default as ChatScreen } from './ChatScreen';
 export { default as LoginScreen } from './LoginScreen';
 export { default as ProfileScreen } from './ProfileScreen';
 export { default as SplashScreen } from './SplashScreen';
-export { default as HomeScreen } from './HomeScreen';
-export { default as FormCaseScreen } from './FormCaseScreen';
