bcecb95a44d6d22142881b76f6c74017d8a0baee
Jacob Winding <jacobkwinding@gmail.com>
2021-01-21 11:31:25 +0100

    fix(case-back-button): first changes to always show back button, work in progress

--- source/navigator/CaseNavigator.js
+++ source/navigator/CaseNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import styled from 'styled-components/native';
-import { Platform } from 'react-native';
+import { TouchableOpacity, Platform } from 'react-native';
 import CaseOverview from '../screens/caseScreens/CaseOverview';
 import CaseSummary from '../screens/caseScreens/CaseSummary';
 import { Icon } from '../components/atoms';
@@ -13,27 +13,39 @@ const BackIcon = styled(Icon)`
   ${Platform.OS === 'ios' && 'margin-left: 16px;'}
 `;
 
-const CaseNavigator = () => (
-  <Stack.Navigator screenOptions={{ headerShown: true }}>
-    <Stack.Screen
-      name="CaseOverview"
-      component={CaseOverview}
-      options={{ title: 'Ärenden', headerShown: false }}
-    />
-    <Stack.Screen
-      name="CaseSummary"
-      component={CaseSummary}
-      options={({ route }) => ({
-        title: route.params.name || 'Ärenden',
-        headerBackImage: () => <BackIcon name="arrow-back" />,
-        headerBackTitle: '',
-        headerTruncatedBackTitle: '',
-        headerStyle: {
-          backgroundColor: theme.colors.neutrals[5],
-        },
-      })}
-    />
-  </Stack.Navigator>
-);
+const CaseNavigator = ({ navigation }) => {
+  const BackButton = () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('CaseOverview');
+      }}
+    >
+      <BackIcon name="arrow-back" />
+    </TouchableOpacity>
+  );
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: true }}>
+      <Stack.Screen
+        name="CaseOverview"
+        component={CaseOverview}
+        options={{ title: 'Ärenden', headerShown: false }}
+      />
+      <Stack.Screen
+        name="CaseSummary"
+        component={CaseSummary}
+        options={({ route }) => ({
+          title: route.params.name || 'Ärenden',
+          // headerBackImage: () => <BackIcon name="arrow-back" />,
+          headerLeft: () => <BackButton />,
+          headerBackTitle: '',
+          headerTruncatedBackTitle: '',
+          headerStyle: {
+            backgroundColor: theme.colors.neutrals[5],
+          },
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default CaseNavigator;
