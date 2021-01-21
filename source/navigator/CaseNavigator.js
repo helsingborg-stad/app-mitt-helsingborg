481f042ead73ae6e3c28263185f10f5a97be4094
Jacob Winding <jacobkwinding@gmail.com>
2021-01-21 11:52:23 +0100

    fix(case-back-button): fix styling of the back button, and remove unused options

--- source/navigator/CaseNavigator.js
+++ source/navigator/CaseNavigator.js
@@ -1,28 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { createStackNavigator } from '@react-navigation/stack';
 import styled from 'styled-components/native';
-import { TouchableOpacity, Platform } from 'react-native';
+import { Platform } from 'react-native';
 import CaseOverview from '../screens/caseScreens/CaseOverview';
 import CaseSummary from '../screens/caseScreens/CaseSummary';
 import { Icon } from '../components/atoms';
-import theme from '../styles/theme';
 
 const Stack = createStackNavigator();
 
 const BackIcon = styled(Icon)`
   ${Platform.OS === 'ios' && 'margin-left: 16px;'}
 `;
+const TouchWrapper = styled.TouchableOpacity`
+  margin-left: 16px;
+  margin-top: 3px;
+  border-radius: 17px;
+  padding: 5px;
+`;
 
 const CaseNavigator = ({ navigation }) => {
   const BackButton = () => (
-    <TouchableOpacity
+    <TouchWrapper
+      activeOpacity={0.2}
       onPress={() => {
         navigation.navigate('CaseOverview');
       }}
     >
       <BackIcon name="arrow-back" />
-    </TouchableOpacity>
+    </TouchWrapper>
   );
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: true }}>
       <Stack.Screen
@@ -35,17 +43,13 @@ const CaseNavigator = ({ navigation }) => {
         component={CaseSummary}
         options={({ route }) => ({
           title: route.params.name || 'Ärenden',
-          // headerBackImage: () => <BackIcon name="arrow-back" />,
           headerLeft: () => <BackButton />,
-          headerBackTitle: '',
-          headerTruncatedBackTitle: '',
-          headerStyle: {
-            backgroundColor: theme.colors.neutrals[5],
-          },
         })}
       />
     </Stack.Navigator>
   );
 };
-
+CaseNavigator.propTypes = {
+  navigation: PropTypes.object,
+};
 export default CaseNavigator;
