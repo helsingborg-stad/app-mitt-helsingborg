c42111f6493146ee774430dd6e67d594caedb2d5
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-02-04 11:44:25 +0100

    changed status object types to follow new pattern

--- source/assets/mock/caseStatuses.js
+++ source/assets/mock/caseStatuses.js
@@ -1,6 +1,3 @@
-/**
- * List of case statuses
- */
 const statuses = [
   {
     type: 'notStarted',
@@ -32,47 +29,47 @@ const statuses = [
    * Service: Ekonomiskt bistånd
    */
   {
-    type: 'notStarted.ekb.open',
+    type: 'notStarted.ekb',
     name: 'Öppen',
     description: 'Ansökan är öppen. Du kan nu söka ekonomiskt bistånd för perioden.',
   },
   {
-    type: 'active.ekb.submitted',
+    type: 'active.submitted.ekb',
     name: 'Inskickad',
     description:
       'Ansökan är inskickad. Du kommer att få besked om ansökan när din handläggare har granskat och bedömt den.',
   },
   {
-    type: 'active.ekb.completionRequired',
+    type: 'active.completionRequired.ekb',
     name: 'Stickprovskontroll',
     description:
       'Du måste komplettera din ansökan med bilder som visar dina utgifter och inkomster. Vi behöver din komplettering inom 4 dagar för att kunna betala ut pengar för perioden.',
   },
   {
-    type: 'closed.ekb.approved',
+    type: 'closed.approved.ekb',
     name: 'Godkänd',
     description: 'Din ansökan är godkänd. Pengarna sätts in på ditt konto.',
   },
   {
-    type: 'closed.ekb.partiallyApproved',
+    type: 'closed.partiallyApproved.ekb',
     name: 'Delvis godkänd',
     description:
       'Delar av din ansökan är godkänd, men några av de utgifter du sökt för får du inte bistånd för. Pengarna för godkända utgifter sätts in på ditt konto.',
   },
   {
-    type: 'closed.ekb.rejected',
+    type: 'closed.rejected.ekb',
     name: 'Avslagen',
     description:
       'Din ansökan är inte godkänd och du kommer inte att få någon utbetalning. Vill du överklaga beslutet lämnar du en skriftlig motivering med e-post eller brev till din handläggare.',
   },
   {
-    type: 'closed.ekb.completionRejected',
+    type: 'closed.completionRejected.ekb',
     name: 'Avslagen',
     description:
       'Din ansökan är inte godkänd eftersom vi saknar stickprov för perioden. Därför kan vi inte gå vidare och godkänna din ansökan.',
   },
 ];
 
-export const getStatusByType = (type) => statuses.find((status) => status.type === type);
+export const getStatusByType = type => statuses.find(status => status.type === type);
 
 export default statuses;
