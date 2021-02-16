610dec4833e91b09df68fde09d001b8ce7bdc0b3
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-02-16 16:31:27 +0100

    Added additional stylings

--- source/components/organisms/Step/CloseDialog/CloseDialog.tsx
+++ source/components/organisms/Step/CloseDialog/CloseDialog.tsx
@@ -4,8 +4,8 @@ import { BlurView } from '@react-native-community/blur';
 import PropTypes from 'prop-types';
 import { Modal } from 'react-native';
 import Button from '../../../atoms/Button';
+import Heading from '../../../atoms/Heading';
 import Text from '../../../atoms/Text';
-import Card from '../../../molecules/Card';
 
 const BackgroundBlur = styled(BlurView)`
   position: absolute;
@@ -14,16 +14,22 @@ const BackgroundBlur = styled(BlurView)`
   bottom: 0;
   right: 0;
 `;
+
 const PopupContainer = styled.View`
-  position: absolute;
-  z-index: 1000;
-  top: 33%;
-  left: 10%;
-  right: 10%;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Dialog = styled.View`
   width: 80%;
+  height: auto;
+  z-index: 1000;
+  align-items: center;
+  justify-content: center;
   border-radius: 10px;
-  background: ${(props) => props.theme.colors.neutrals[7]};
-  padding: 10px;
+  background: ${(props) => props.theme.colors.neutrals[5]};
+  padding: 12px;
   elevation: 2;
   shadow-offset: 0px 2px;
   shadow-color: black;
@@ -31,11 +37,44 @@ const PopupContainer = styled.View`
   shadow-radius: 2px;
 `;
 
+const Content = styled.View`
+  padding: 12px;
+`;
+
+const Title = styled(Heading)`
+  color: ${(props) => props.theme.colors.neutrals[1]};
+  font-size: ${(props) => props.theme.fontSizes[4]}px;
+  margin: 0px;
+  text-align: center;
+`;
+
+const DialogText = styled(Text)`
+  margin: 12px;
+  font-size: ${(props) => props.theme.fontSizes[3]}px;
+  text-align: center;
+`;
+
+const DialogButton = styled(Button)`
+  ${({ colorSchema }) => colorSchema === 'neutral' && `background: #e5e5e5; `}
+`;
+
+const ButtonText = styled(Text)`
+  color: ${(props) => props.theme.colors.neutrals[1]};
+  font-weight: ${(props) => props.theme.fontWeights[1]};
+`;
+
 const ButtonRow = styled.View`
   flex-direction: row;
   justify-content: space-evenly;
-  padding: 15px;
-  margin-bottom: 0px;
+  margin: 0px;
+`;
+
+const ButtonWrapper = styled.View`
+  flex: 1;
+`;
+
+const ButtonDivider = styled.View`
+  width: 8px;
 `;
 
 interface Props {
@@ -53,30 +92,37 @@ const CloseDialog: React.FC<Props> = ({ visible, closeForm, closeDialog }) => (
     statusBarTranslucent
   >
     <PopupContainer>
-      <Card colorSchema="neutral">
-        <Card.Body>
-          <Card.Title>Vill du avbryta ansökan?</Card.Title>
-          <Card.Text>
+      <Dialog>
+        <Content>
+          <Title>Vill du avbryta ansökan?</Title>
+          <DialogText>
             Ansökan sparas i 3 dagar. Efter det raderas den och du får starta en ny.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <ButtonRow>
-        <Button colorSchema="red" onClick={closeDialog}>
-          <Text>Nej</Text>
-        </Button>
-        <Button
-          small
-          onClick={() => {
-            closeDialog();
-            closeForm();
-          }}
-        >
-          <Text>Ja</Text>
-        </Button>
-      </ButtonRow>
+          </DialogText>
+        </Content>
+        <ButtonRow>
+          <ButtonWrapper>
+            <DialogButton block z={0} colorSchema="neutral" onClick={closeDialog}>
+              <ButtonText>Nej</ButtonText>
+            </DialogButton>
+          </ButtonWrapper>
+          <ButtonDivider />
+          <ButtonWrapper>
+            <DialogButton
+              block
+              z={0}
+              colorSchema="blue"
+              onClick={() => {
+                closeDialog();
+                closeForm();
+              }}
+            >
+              <Text>Ja</Text>
+            </DialogButton>
+          </ButtonWrapper>
+        </ButtonRow>
+      </Dialog>
+      <BackgroundBlur blurType="dark" blurAmount={15} reducedTransparencyFallbackColor="white" />
     </PopupContainer>
-    <BackgroundBlur blurType="light" blurAmount={15} reducedTransparencyFallbackColor="white" />
   </Modal>
 );
 
