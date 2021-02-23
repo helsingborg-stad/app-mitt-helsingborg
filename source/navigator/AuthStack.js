2c9e53731b98a7c4db74f57efb347c351bf51028
Ehsan Zilaei <zzilaei@gmail.com>
2021-02-23 20:54:11 +0100

    feat(onboarding): removed unused import in AuthStack

--- source/navigator/AuthStack.js
+++ source/navigator/AuthStack.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import OnboardingScreen from '../screens/OnboardingScreen';
+import Onboarding from '../screens/onboarding';
 import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => (
   <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+    <Stack.Screen name="Onboarding" component={Onboarding} />
     <Stack.Screen name="Login" component={LoginScreen} />
   </Stack.Navigator>
 );
