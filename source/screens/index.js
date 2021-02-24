500d9e80814f84749f601b53d384aa373056b348
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-02-24 13:33:59 +0100

    feat(about-screen): export about screen

--- source/screens/index.js
+++ source/screens/index.js
@@ -4,3 +4,4 @@ export { default as ChatScreen } from './ChatScreen';
 export { default as LoginScreen } from './LoginScreen';
 export { default as ProfileScreen } from './ProfileScreen';
 export { default as SplashScreen } from './SplashScreen';
+export { default as AboutScreen } from './AboutScreen';
