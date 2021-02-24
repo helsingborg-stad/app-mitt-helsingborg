87ea4ce5d863c77d9f0f18a0192afebe602a5a6e
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-02-24 13:49:32 +0100

    feat(about-screen): remove chat screen

--- source/screens/index.js
+++ source/screens/index.js
@@ -1,6 +1,5 @@
 export { default as DevFeaturesScreen } from './DevFeaturesScreen';
 export { default as FormCaseScreen } from './FormCaseScreen';
-export { default as ChatScreen } from './ChatScreen';
 export { default as LoginScreen } from './LoginScreen';
 export { default as ProfileScreen } from './ProfileScreen';
 export { default as SplashScreen } from './SplashScreen';
