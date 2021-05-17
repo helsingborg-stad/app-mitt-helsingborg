7966f6ac40cd579271ed618f1ecdafb891acac17
Kenth Ljung <kenth.ljung@helsingborg.se>
2021-05-17 13:49:40 +0200

    fix: correct error handler syntax and only show alert on fatal errors

--- source/helpers/error-handler/ErrorHandler.tsx
+++ source/helpers/error-handler/ErrorHandler.tsx
@@ -9,21 +9,36 @@ import { string } from 'prop-types';
  * (run-xxx --configuration Release).
  * Set .env flag "ENABLE_DEV_ERROR_BOUNDARY=true" to enable boundary errors in dev mode (non release builds of app).
  *
- * @param error Object with error message
+ * Non-fatal errors are triggered by `console.error` calls.
+ * Fatal errors are triggered by unhandled exceptions, and can also be triggered manually with `ErrorUtils.reportFatalError`.
+ *
+ * @param error Error object or error message string
+ * @param isFatal True if error is deemed fatal and cannot be recovered from
  */
-const boundaryErrorHandler = (error = { message: string }) => {
-  if (Config.IS_STORYBOOK === 'true') {
-    Alert.alert('Something went wrong in storybook', `${error.message}`, [{ text: 'OK' }]);
-  } else {
-    Alert.alert('Something went wrong in Mitt Helsingborg', `${error.message}`, [
-      {
-        text: 'Restart',
-        onPress: () => {
-          RNRestart.Restart();
+const boundaryErrorHandler = (error: undefined | string | Error, isFatal: boolean) => {
+  const errorMessage = typeof error === 'string' ? error : error?.message || 'unknown error';
+
+  if (error && isFatal) {
+    console.log('boundary error', typeof error, error);
+
+    if (Config.IS_STORYBOOK === 'true') {
+      Alert.alert('Something went wrong in storybook', `${errorMessage}`, [{ text: 'OK' }]);
+    } else {
+      Alert.alert('Something went wrong in Mitt Helsingborg', `${errorMessage}`, [
+        {
+          text: 'Restart',
+          onPress: () => {
+            RNRestart.Restart();
+          },
         },
-      },
-    ]);
+      ]);
+    }
+  } else {
+    // non-fatal errors go here
+    // they are probably already logged to console so no need to log them again
   }
+
+  // TODO: send error to analytics
 };
 
 export default boundaryErrorHandler;
