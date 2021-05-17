64f00f33ce347d93b9eed2a815fd796faaa127b3
Kenth Ljung <kenth.ljung@helsingborg.se>
2021-05-17 13:45:40 +0200

    fix: set correct state payload for auth errors

--- source/store/actions/AuthActions.js
+++ source/store/actions/AuthActions.js
@@ -140,7 +140,7 @@ export async function startAuth(ssn, launchBankidApp) {
     return {
       type: actionTypes.authError,
       payload: {
-        error: error.data,
+        error,
       },
     };
   }
