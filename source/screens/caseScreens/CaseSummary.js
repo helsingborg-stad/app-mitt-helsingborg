a22f991232d3dd668d6cc3a4eb49df8a632f3558
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-05-25 22:49:01 +0200

    fix: 🐛 display reduction correct
    reductions can be one or more. added all attributes on the reduction
    object

--- source/screens/caseScreens/CaseSummary.js
+++ source/screens/caseScreens/CaseSummary.js
@@ -479,18 +479,28 @@ const CaseSummary = (props) => {
                         )}
 
                         <Card.DetailsTitle type="h5">Reducering</Card.DetailsTitle>
-
                         {calculation?.reductions?.reduction ? (
                           <>
-                            <Card.CalculationRow>
-                              <Card.CalculationRowCell>
-                                <Text>{calculation.reductions.reduction}</Text>
-                              </Card.CalculationRowCell>
-                              <Card.CalculationRowCell />
-                              <Card.CalculationRowCell>
-                                <Text>{formatAmount(calculation.reductions.reduction.amount)}</Text>
-                              </Card.CalculationRowCell>
-                            </Card.CalculationRow>
+                            {convertDataToArray(calculation?.reductions?.reduction).map(
+                              (reduction, index) => (
+                                <Card.CalculationRow key={`${index}-${reduction.type}`}>
+                                  <Card.CalculationRowCell>
+                                    <Text>{reduction?.type}</Text>
+                                  </Card.CalculationRowCell>
+                                  <Card.CalculationRowCell>
+                                    <Text>
+                                      {reduction?.days} {reduction?.days > 1 ? 'dagar' : 'dag'}
+                                    </Text>
+                                  </Card.CalculationRowCell>
+                                  <Card.CalculationRowCell>
+                                    <Text>{reduction?.note}</Text>
+                                  </Card.CalculationRowCell>
+                                  <Card.CalculationRowCell>
+                                    <Text>{formatAmount(reduction.amount)}</Text>
+                                  </Card.CalculationRowCell>
+                                </Card.CalculationRow>
+                              )
+                            )}
                           </>
                         ) : (
                           <Card.Text italic>Det finns inga registrerade reduceringar.</Card.Text>
