5939a7a9b6906a9b6132b01ed5d0fbdc22a70b5c
Ehsan Zilaei <zzilaei@gmail.com>
2021-06-28 09:02:24 +0200

    feat(encryption-service): setting AES password from parameter and refactored service

--- source/services/encryption/EncryptionService.ts
+++ source/services/encryption/EncryptionService.ts
@@ -22,20 +22,50 @@ async function generateAesKey(
   return Aes.pbkdf2(password, salt, cost, length);
 }
 
+const AES_SALT = 'salt4D42bf960Sm1';
+const AES_COST = 5000;
+const AES_LENGTH = 256;
+
+const getStorageKeyword = (user: User) => `${user.personalNumber}AesKey`;
+
+export async function setAesEncryptor(user: User, password: string) {
+  const storageKeyword = getStorageKeyword(user);
+
+  const aesKey = await generateAesKey(password, AES_SALT, AES_COST, AES_LENGTH);
+  const aesEncryptor = { aesKey, initializationVector: aesKey };
+
+  await StorageService.saveData(storageKeyword, aesEncryptor);
+}
+
+export async function getAesEncryptor(user: User) {
+  const storageKeyword = getStorageKeyword(user);
+
+  return await StorageService.getData(storageKeyword);
+}
+
+export async function removeAesEncryptor(user: User) {
+  await StorageService.removeData(getStorageKeyword(user));
+}
+
 export async function encryptWithAesKey(user: User, text: string): Promise<string> {
-  const storageKeyword = `${user.personalNumber}AesKey`;
+  const storageKeyword = getStorageKeyword(user);
 
   let aesEncryptor = await StorageService.getData(storageKeyword);
 
   if (!aesEncryptor) {
+    // const password = await Aes.randomKey(16);
+    // // Salt will be updated in future real.
+    // const aesKey = await generateAesKey(password, AES_SALT, AES_COST, AES_LENGTH);
+    //
+    // const initializationVector = await Aes.randomKey(16);
+    //
+    // aesEncryptor = { aesKey, initializationVector };
+    // await StorageService.saveData(storageKeyword, aesEncryptor);
+
     const password = await Aes.randomKey(16);
-    // Salt will be updated in future real.
-    const aesKey = await generateAesKey(password, 'salt4D42bf960Sm1', 5000, 256);
+    await setAesEncryptor(user, password);
 
-    const initializationVector = await Aes.randomKey(16);
-
-    aesEncryptor = { aesKey, initializationVector };
-    await StorageService.saveData(storageKeyword, aesEncryptor);
+    aesEncryptor = await StorageService.getData(storageKeyword);
   }
 
   return await Aes.encrypt(text, aesEncryptor.aesKey, aesEncryptor.initializationVector);
