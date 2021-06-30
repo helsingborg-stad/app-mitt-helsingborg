fbcc7d61a18d8ce3183800f790cff7d33dfbf187
Ehsan Zilaei <zzilaei@gmail.com>
2021-06-30 13:46:49 +0200

    feat(encryption-service): decrypt answers and sign form with encryption type

--- source/services/encryption/index.js
+++ source/services/encryption/index.js
@@ -1,2 +1,4 @@
 export { encryptWithAesKey } from './EncryptionService';
 export { decryptWithAesKey } from './EncryptionService';
+export { encryptFormAnswers } from './EncryptionService';
+export { decryptFormAnswers } from './EncryptionService';
