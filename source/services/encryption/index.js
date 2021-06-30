74291bf4dead1de02be1367247cf76cb1d67bfe9
Ehsan Zilaei <zzilaei@gmail.com>
2021-06-30 16:04:01 +0200

    fix(encryption-service): remove export of functions only used privately by EncryptionService

--- source/services/encryption/index.js
+++ source/services/encryption/index.js
@@ -1,4 +1,2 @@
-export { encryptWithAesKey } from './EncryptionService';
-export { decryptWithAesKey } from './EncryptionService';
 export { encryptFormAnswers } from './EncryptionService';
 export { decryptFormAnswers } from './EncryptionService';
