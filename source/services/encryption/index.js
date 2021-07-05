f754f579d574b80e2cc76f76f4df6b9e49a08d46
Ehsan Zilaei <zzilaei@gmail.com>
2021-07-05 18:32:51 +0200

    feat(encryption-service): export of EncryptionService.ts functions

--- source/services/encryption/index.js
+++ source/services/encryption/index.js
@@ -1,2 +1,5 @@
 export { encryptFormAnswers } from './EncryptionService';
 export { decryptFormAnswers } from './EncryptionService';
+export { encryptWithAesKey } from './EncryptionService';
+export { decryptWithAesKey } from './EncryptionService';
+export { setupSymmetricKey } from './EncryptionService';
