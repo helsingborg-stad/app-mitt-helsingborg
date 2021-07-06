624826bff07774cf0b40f24afb172a5b4b089e16
Ehsan Zilaei <zzilaei@gmail.com>
2021-07-06 18:11:11 +0200

    feat(encryption-helper): added functions storing symmetric key and getting pseudo keys and integers

--- source/services/encryption/EncryptionHelper.ts
+++ source/services/encryption/EncryptionHelper.ts
@@ -13,3 +13,23 @@ export async function getStoredSymmetricKey(symmetricKeyName) {
   const symmetricKey = await StorageService.getData(symmetricKeyName);
   return Number(symmetricKey);
 }
+
+export async function storeSymmetricKey(symmetricKey, forms) {
+  const storageKeyword = getSymmetricKeyStorageKeyword(forms);
+  await StorageService.saveData(storageKeyword, symmetricKey.toString());
+}
+
+// Prof of concept, will be replaced with elliptic curves.
+export function getPseudoKey(G, privateKey, P) {
+  if (privateKey === 1) {
+    return G;
+  }
+
+  return G ** privateKey % P;
+}
+
+export function getPseudoRandomInteger() {
+  const min = 1;
+  const max = 10;
+  return Math.floor(min + Math.random() * (max - min));
+}
