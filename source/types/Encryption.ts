ede3039e68ec3093ec833d59c574e6340c184271
Kenth Ljung <kenth.ljung@helsingborg.se>
2021-07-29 15:58:09 +0200

    chore: add Encryption type definitions

---
+++ source/types/Encryption.ts
@@ -0,0 +1,89 @@
+import "react-native";
+
+/**
+ * Possible encryption methods.
+ */
+export type EncryptionType = "decrypted" | "privateAesKey" | "symmetricKey";
+
+/**
+ * Number type used for cryptographic numbers.
+ */
+export type CryptoNumber = number;
+
+/**
+ * Type for serialized CryptoNumber values compatible with JSON.
+ */
+export type SerializedCryptoNumber = string;
+
+/**
+ * Tuple for a public-private key pair.
+ */
+export type KeyPair = [CryptoNumber, CryptoNumber];
+
+/**
+ * react-native-aes-crypto types
+ */
+interface AesModule {
+  pbkdf2: (
+    password: string,
+    salt: string,
+    cost: number,
+    length: number
+  ) => Promise<string>;
+  encrypt: (text: string, key: string, iv: string) => Promise<string>;
+  decrypt: (ciphertext: string, key: string, iv: string) => Promise<string>;
+  hmac256: (ciphertext: string, key: string) => Promise<string>;
+  randomKey: (length: number) => Promise<string>;
+  sha1: (text: string) => Promise<string>;
+  sha256: (text: string) => Promise<string>;
+  sha512: (text: string) => Promise<string>;
+}
+
+declare module "react-native" {
+  interface NativeModulesStatic {
+    Aes: AesModule;
+  }
+}
+
+/**
+ * Internal details of the encryption used.
+ */
+export interface EncryptionDetails {
+  /**
+   * Type of encryption used, or `decrypted` if unencrypted.
+   */
+  type: EncryptionType;
+
+  /**
+   * Unique name when using a symmetric key. Usually takes the format of "[main applicant pno]:[co-applicant pno]".
+   */
+  symmetricKeyName?: string;
+
+  /**
+   * Prime numbers used for cryptography.
+   */
+  primes?: {
+    /**
+     * A number P used for cryptography.
+     */
+    P: SerializedCryptoNumber;
+
+    /**
+     * A number G which is a primitive root modulo P, used for cryptography.
+     */
+    G: SerializedCryptoNumber;
+  };
+
+  /**
+   * Map containing the public keys of persons connected to this encryption setup.
+   */
+  publicKeys?: Record<string, SerializedCryptoNumber>;
+}
+
+/**
+ * Aes configuration wrapper.
+ */
+export interface AesEncryptor {
+  aesKey: string;
+  initializationVector: string;
+}
