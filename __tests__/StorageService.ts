3c764f113ec42e0479ceb6328cdc8c401115905a
Kenth Ljung <kenth.ljung@helsingborg.se>
2021-07-30 09:55:33 +0200

    test(jest): add basic StorageService test file

---
+++ __tests__/StorageService.ts
@@ -0,0 +1,28 @@
+import StorageService from "../source/services/StorageService";
+
+describe("StorageService", () => {
+  it("stores and fetches data", async () => {
+    const mockKey = "___jest_mockKey";
+    const mockValue = {
+      test: "Hello World",
+    };
+    await StorageService.saveData(mockKey, JSON.stringify(mockValue));
+    const retrievedDataRaw = await StorageService.getData(mockKey);
+
+    expect(retrievedDataRaw).toBeDefined();
+
+    const retrievedData: typeof mockValue = JSON.parse(retrievedDataRaw);
+
+    expect(retrievedData.test).toEqual("Hello World");
+  });
+
+  it("clears data", async () => {
+    const mockKey = "___jest_mockKey";
+
+    await StorageService.saveData(mockKey, JSON.stringify({ hello: "world" }));
+    await StorageService.clearData();
+    const retrievedData = await StorageService.getData(mockKey);
+
+    expect(retrievedData).toBeNull();
+  });
+});
