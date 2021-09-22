9be30976f161891c6c1846f463ddaf6fb0ff7554
Kenth Ljung <kenth.ljung@helsingborg.se>
2021-09-22 15:08:41 +0200

    feat: add Aes typedefs

--- source/types/Encryption.ts
+++ source/types/Encryption.ts
@@ -1,5 +1,26 @@
 export type EncryptionType = "decrypted" | "privateAesKey" | "symmetricKey";
 
+interface AesModule {
+  pbkdf2: (
+    password: string,
+    salt: string,
+    cost: number,
+    length: number
+  ) => Promise<string>;
+  encrypt: (text: string, key: string, iv: string) => Promise<string>;
+  decrypt: (ciphertext: string, key: string, iv: string) => Promise<string>;
+  hmac256: (ciphertext: string, key: string) => Promise<string>;
+  randomKey: (length: number) => Promise<string>;
+  sha1: (text: string) => Promise<string>;
+  sha256: (text: string) => Promise<string>;
+  sha512: (text: string) => Promise<string>;
+}
+
+declare module "react-native" {
+  interface NativeModulesStatic {
+    Aes: AesModule;
+  }
+}
 export interface EncryptionDetails {
   type: EncryptionType;
   symmetricKeyName?: string;
