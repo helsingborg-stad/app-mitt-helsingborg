e38d9f6661c68544a4b83d5b47feaa2dade58c7a
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-09-23 14:39:08 +0200

    feat: first draft of day picker component

--- source/components/molecules/DayPicker/DayPicker.stories.js
+++ source/components/molecules/DayPicker/DayPicker.stories.js
@@ -1,9 +1,40 @@
 import { storiesOf } from '@storybook/react-native';
 import React, { useState } from 'react';
+import { View } from 'react-native';
+import { H1 } from '../../atoms/Heading/Heading';
 import DayPicker from './DayPicker';
 import StoryWrapper from '../StoryWrapper';
 
-const StoryDayPicker = () => <DayPicker />;
+const StoryDayPicker = () => {
+  const [selectedDate, setSelectedDate] = useState('None');
+
+  const startDate = '2018-04-08';
+  const availableDates = [
+    '2018-04-13',
+    '2018-04-19',
+    '2018-04-28',
+    '2018-05-03',
+    '2018-05-18',
+    '2018-05-20',
+    '2018-06-17',
+    '2018-06-30',
+    '2018-07-04',
+  ];
+  const callback = (dateString) => setSelectedDate(dateString);
+
+  return (
+    <View>
+      <View style={{ padding: 20 }}>
+        <H1>Selected date: {selectedDate}</H1>
+      </View>
+      <DayPicker
+        startDate={startDate}
+        availableDates={availableDates}
+        onDateSelected={(dateString) => callback(dateString)}
+      />
+    </View>
+  );
+};
 
 storiesOf('Day picker', module).add('Default', () => (
   <StoryWrapper>
--- source/components/molecules/DayPicker/DayPicker.tsx
+++ source/components/molecules/DayPicker/DayPicker.tsx
@@ -1,19 +1,143 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components/native';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { View, TouchableOpacity, Platform } from 'react-native';
-import { Calendar } from 'react-native-calendars';
-import Input from '../../atoms/Input';
-import Button from '../../atoms/Button';
-import Text from '../../atoms/Text';
-import Modal from '../Modal/Modal';
-// top: ${props => (props.top ? `${props.top}px` : '40px')};
+import { View, Text } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import moment from 'moment';
 
-const DayPicker = (
-  <View>
-    <Calendar />
-  </View>
-);
+LocaleConfig.locales.se = {
+  monthNames: [
+    'januari',
+    'februari',
+    'mars',
+    'april',
+    'maj',
+    'juni',
+    'juli',
+    'augusti',
+    'september',
+    'october',
+    'november',
+    'december',
+  ],
+  monthNamesShort: [
+    'jan',
+    'feb',
+    'mar',
+    'apr',
+    'maj',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dec',
+  ],
+  dayNames: ['söndag', 'måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag'],
+  dayNamesShort: ['sön', 'mån', 'tis', 'ons', 'tor', 'fre', 'mån'],
+  today: 'idag',
+};
+LocaleConfig.defaultLocale = 'se';
+
+const colors = {
+  selectedBackground: '#ae0b05',
+  selectedText: '#ffffff',
+  availableBackground: '#f5e4e3',
+  availableText: '#770000',
+  todayBorder: '#e84c31',
+  todayText: '#205400',
+  disabledText: '#a3a3a3',
+};
+
+const dayStyles = {
+  availableStyle: {
+    container: {
+      backgroundColor: colors.availableBackground,
+      borderRadius: 10,
+      width: '90%',
+    },
+    text: {
+      color: colors.availableText,
+    },
+  },
+  selectedStyle: {
+    container: {
+      backgroundColor: colors.selectedBackground,
+      borderRadius: 10,
+      width: '90%',
+    },
+    text: {
+      color: colors.selectedText,
+    },
+  },
+  todayStyle: {
+    container: {
+      width: '90%',
+      borderWidth: 2,
+      borderColor: colors.todayBorder,
+      borderRadius: 10,
+    },
+    text: {
+      color: colors.todayText,
+      left: -1,
+      top: -1,
+    },
+  },
+};
+
+const calendarTheme = {
+  arrowColor: 'black',
+  textDayFontWeight: '600',
+  textDisabledColor: colors.disabledText,
+  textSectionTitleColor: colors.disabledText,
+  'stylesheet.calendar.header': {
+    week: {
+      color: 'black',
+    },
+  },
+};
+
+const DayPicker = (props) => {
+  const [selectedDate, setSelectedDate] = useState();
+
+  const { availableDates, startDate, onDateSelected } = props;
+  const minDate = startDate || Date();
+  const minDateString = moment(minDate).format('yyyy-MM-DD');
+  const maxDate = moment(minDate).add(3, 'M').toDate();
+  const markedDates = {};
+  markedDates[minDateString] = {
+    customStyles: dayStyles.todayStyle,
+  };
+  availableDates.forEach((date) => {
+    const isSelected = date === selectedDate;
+    markedDates[date] = {
+      selected: true,
+      customStyles: isSelected ? dayStyles.selectedStyle : dayStyles.availableStyle,
+    };
+  });
+
+  function selectDate(day) {
+    const { dateString } = day;
+    if (availableDates.includes(dateString)) {
+      setSelectedDate(dateString);
+      onDateSelected(dateString);
+    }
+  }
+
+  return (
+    <View>
+      <Calendar
+        markingType="custom"
+        theme={calendarTheme}
+        current={startDate}
+        minDate={minDate}
+        maxDate={maxDate}
+        firstDay={1}
+        markedDates={markedDates}
+        onDayPress={(day) => selectDate(day)}
+        showWeekNumbers
+      />
+    </View>
+  );
+};
 
 export default DayPicker;
