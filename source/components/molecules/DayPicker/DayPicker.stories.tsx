80602f5fa3dc8e0e2600bbb12b61fb1610b2d87c
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-09-27 14:50:08 +0200

    fix: adapt code to new linting config

--- source/components/molecules/DayPicker/DayPicker.stories.tsx
+++ source/components/molecules/DayPicker/DayPicker.stories.tsx
@@ -1,25 +1,25 @@
-import { storiesOf } from '@storybook/react-native';
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import moment from 'moment';
-import { H1 } from '../../atoms/Heading/Heading';
-import DayPicker from './DayPicker';
-import StoryWrapper from '../StoryWrapper';
+import { storiesOf } from "@storybook/react-native";
+import React, { useState } from "react";
+import { View } from "react-native";
+import moment from "moment";
+import { H1 } from "../../atoms/Heading/Heading";
+import DayPicker from "./DayPicker";
+import StoryWrapper from "../StoryWrapper";
 
 const StoryDayPicker = () => {
-  const [selectedDate, setSelectedDate] = useState('None');
+  const [selectedDate, setSelectedDate] = useState("None");
 
-  const startDate = moment('2018-04-08').toDate();
+  const startDate = moment("2018-04-08").toDate();
   const availableDates = [
-    '2018-04-13',
-    '2018-04-19',
-    '2018-04-28',
-    '2018-05-03',
-    '2018-05-18',
-    '2018-05-20',
-    '2018-06-17',
-    '2018-06-30',
-    '2018-07-04',
+    "2018-04-13",
+    "2018-04-19",
+    "2018-04-28",
+    "2018-05-03",
+    "2018-05-18",
+    "2018-05-20",
+    "2018-06-17",
+    "2018-06-30",
+    "2018-07-04",
   ];
   const callback = (dateString) => setSelectedDate(dateString);
 
@@ -37,7 +37,7 @@ const StoryDayPicker = () => {
   );
 };
 
-storiesOf('Day picker', module).add('Default', () => (
+storiesOf("Day picker", module).add("Default", () => (
   <StoryWrapper>
     <StoryDayPicker />
   </StoryWrapper>
--- source/components/molecules/DayPicker/styles.js
+++ source/components/molecules/DayPicker/styles.js
@@ -1,11 +1,11 @@
 const colors = {
-  selectedBackground: '#ae0b05',
-  selectedText: '#ffffff',
-  availableBackground: '#f5e4e3',
-  availableText: '#770000',
-  todayBorder: '#e84c31',
-  todayText: '#205400',
-  disabledText: '#a3a3a3',
+  selectedBackground: "#ae0b05",
+  selectedText: "#ffffff",
+  availableBackground: "#f5e4e3",
+  availableText: "#770000",
+  todayBorder: "#e84c31",
+  todayText: "#205400",
+  disabledText: "#a3a3a3",
 };
 
 /* used in `customStyles` field in the `markedDates` prop in <Calendar/> */
@@ -14,7 +14,7 @@ const dayStyles = {
     container: {
       backgroundColor: colors.availableBackground,
       borderRadius: 10,
-      width: '90%',
+      width: "90%",
     },
     text: {
       color: colors.availableText,
@@ -24,7 +24,7 @@ const dayStyles = {
     container: {
       backgroundColor: colors.selectedBackground,
       borderRadius: 10,
-      width: '90%',
+      width: "90%",
     },
     text: {
       color: colors.selectedText,
@@ -32,7 +32,7 @@ const dayStyles = {
   },
   todayStyle: {
     container: {
-      width: '90%',
+      width: "90%",
       borderWidth: 2,
       borderColor: colors.todayBorder,
       borderRadius: 10,
@@ -47,8 +47,8 @@ const dayStyles = {
 
 /* used as `theme` prop in <Calendar/> for styling internal components */
 const calendarTheme = {
-  arrowColor: 'black',
-  textDayFontWeight: '600',
+  arrowColor: "black",
+  textDayFontWeight: "600",
   textDisabledColor: colors.disabledText,
   textSectionTitleColor: colors.disabledText,
 };
