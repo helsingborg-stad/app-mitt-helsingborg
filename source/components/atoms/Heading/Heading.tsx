a046a42c44ca498a640ab62bb3bec98a3db440f3
Karl <karl.mosenbacher@knowit.se>
2021-09-27 14:45:33 +0200

    fix: add missing h5 string in type
    - format Heading.tsx to correct format

--- source/components/atoms/Heading/Heading.tsx
+++ source/components/atoms/Heading/Heading.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import Text from '../Text/Text';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Text from "../Text/Text";
 
 interface Props {
-  align?: 'left' | 'center' | 'right';
+  align?: "left" | "center" | "right";
   marginBottom?: number;
-  type?: 'h1' | 'h2' | 'h3' | 'h4';
+  type?: "h1" | "h2" | "h3" | "h4" | "h5";
 }
 
 const Heading = styled(Text)<Props>`
   font-style: normal;
-  color: ${props => props.theme.colors.neutrals[0]};
-  text-align: ${props => props.align || 'left'};
-  margin-bottom: ${props => props.marginBottom || '5'}px;
+  color: ${(props) => props.theme.colors.neutrals[0]};
+  text-align: ${(props) => props.align || "left"};
+  margin-bottom: ${(props) => props.marginBottom || "5"}px;
 `;
 
 export const H1: React.FC<Props> = ({ children, ...other }) => (
@@ -44,15 +44,15 @@ H3.propTypes = {
 };
 
 Heading.propTypes = {
-  type: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4']),
+  type: PropTypes.oneOf(["h1", "h2", "h3", "h4"]),
   marginBottom: PropTypes.number,
-  align: PropTypes.oneOf(['left', 'center', 'right']),
+  align: PropTypes.oneOf(["left", "center", "right"]),
 };
 
 Heading.defaultProps = {
-  type: 'h2',
+  type: "h2",
   marginBottom: 0,
-  align: 'left',
+  align: "left",
 };
 
 export default Heading;
