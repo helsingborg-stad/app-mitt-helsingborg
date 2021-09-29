6f58e3a58bf3278a27586943767d6696e45bb702
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-09-29 13:20:50 +0200

    fix: use correct colors from theme

--- source/components/molecules/DayPicker/DayPicker.tsx
+++ source/components/molecules/DayPicker/DayPicker.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import moment from "moment";
+import { ThemeContext } from "styled-components/native";
 import localeConfig from "./localeConfig";
-import { dayStyles, calendarTheme } from "./styles";
 
 LocaleConfig.locales.se = localeConfig;
 LocaleConfig.defaultLocale = "se";
@@ -19,6 +19,61 @@ const DayPicker: React.FC<DayPickerProps> = ({
   startDate,
 }) => {
   const [selectedDate, setSelectedDate] = useState();
+  const theme = useContext(ThemeContext);
+
+  const colors = {
+    selectedBackground: theme.colors.primary.red[0],
+    selectedText: theme.colors.neutrals[7],
+    availableBackground: theme.colors.complementary.red[2],
+    availableText: theme.colors.primary.red[0],
+    todayBorder: theme.colors.primary.red[3],
+    todayText: theme.colors.primary.green[0],
+    disabledText: theme.colors.neutrals[4],
+  };
+
+  const dayStyles = {
+    availableStyle: {
+      container: {
+        backgroundColor: colors.availableBackground,
+        borderRadius: 10,
+        width: "90%",
+      },
+      text: {
+        color: colors.availableText,
+      },
+    },
+    selectedStyle: {
+      container: {
+        backgroundColor: colors.selectedBackground,
+        borderRadius: 10,
+        width: "90%",
+      },
+      text: {
+        color: colors.selectedText,
+      },
+    },
+    todayStyle: {
+      container: {
+        width: "90%",
+        borderWidth: 2,
+        borderColor: colors.todayBorder,
+        borderRadius: 10,
+      },
+      text: {
+        color: colors.todayText,
+        left: -1,
+        top: -1,
+      },
+    },
+  };
+
+  /* used as `theme` prop in <Calendar/> for styling internal components */
+  const calendarTheme = {
+    arrowColor: "black",
+    textDayFontWeight: "600",
+    textDisabledColor: colors.disabledText,
+    textSectionTitleColor: colors.disabledText,
+  };
 
   const minDate = startDate;
   const maxDate = moment(minDate).add(3, "M").toDate(); // Placeholder value of 3 months forward, must be discussed further
