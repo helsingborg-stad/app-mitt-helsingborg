a3933fb0860beab02bb0886312720432e6dd2458
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-09-29 17:17:02 +0200

    fix: remove unnecessary test for DayPicker, update mock data for other tests

--- source/components/molecules/DayPicker/test/DayPicker.test.tsx
+++ source/components/molecules/DayPicker/test/DayPicker.test.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { fireEvent } from "@testing-library/react-native";
 import { render } from "../../../../../test-utils";
 
-import { colorPalette } from "../../../../styles/palette";
-
 import DayPicker from "..";
 
-const mockStartDate = "2018-04-08";
-const mockAvailableDates = ["2018-04-13"];
+const mockStartDate = "2018-08-08";
+const mockAvailableDates = ["2018-08-13"];
 
-it("calls the callback function when available date is clicked", () => {
+it("calls the callback function when an available date is clicked", () => {
   const mockCallback = jest.fn();
 
   const { getByText } = render(
@@ -26,7 +24,7 @@ it("calls the callback function when available date is clicked", () => {
   expect(mockCallback).toHaveBeenCalled();
 });
 
-it("doesn't calls the callback function when unavailable date is clicked", () => {
+it("doesn't call the callback function when an unavailable date is clicked", () => {
   const mockCallback = jest.fn();
 
   const { getByText } = render(
@@ -41,26 +39,4 @@ it("doesn't calls the callback function when unavailable date is clicked", () =>
   fireEvent.press(dayElement);
 
   expect(mockCallback).not.toHaveBeenCalled();
-});
-
-it("changes the color of date element when selected", () => {
-  const { getByText } = render(
-    <DayPicker
-      startDate={mockStartDate}
-      availableDates={mockAvailableDates}
-      onDateSelected={() => true}
-    />
-  );
-
-  const dayElement = getByText("13");
-
-  expect(dayElement).toHaveStyle({
-    backgroundColor: colorPalette.complementary.red[2],
-  });
-
-  fireEvent.press(dayElement);
-
-  expect(dayElement).toHaveStyle({
-    backgroundColor: colorPalette.primary.red[0],
-  });
 });
