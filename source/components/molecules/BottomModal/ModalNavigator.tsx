8f33a9e007020b61f2fa7a884000c37cd4015675
Karl <karl.mosenbacher@knowit.se>
2021-10-05 07:22:19 +0200

    fix: make constants out of floating values

--- source/components/molecules/BottomModal/ModalNavigator.tsx
+++ source/components/molecules/BottomModal/ModalNavigator.tsx
@@ -5,6 +5,22 @@ import Icon from "../../atoms/Icon";
 
 import { NavigatorContainer, IconContainer, ModalText } from "./styled";
 
+const SIZE = {
+  ICON: 24,
+  BACK_BUTTON: 12,
+  TITLE: 18,
+};
+
+const ICON_NAME = {
+  ARROW_BACK: "arrow-back",
+  CLOSE: "close",
+};
+
+const FLEX_DIRECTION = {
+  END: "flex-end",
+  START: "flex-start",
+};
+
 interface ModalNavigatorProps {
   color: string;
   textColor: string;
@@ -18,10 +34,10 @@ const ModalNavigator = (props: ModalNavigatorProps): JSX.Element => {
 
   return (
     <NavigatorContainer background={color}>
-      <IconContainer flexDirection="flex-start">
+      <IconContainer flexDirection={FLEX_DIRECTION.START}>
         <TouchableOpacity onPress={onBack} disabled={!onBack}>
           {onBack && backButtonText && (
-            <ModalText size={12} color={textColor}>
+            <ModalText size={SIZE.BACK_BUTTON} color={textColor}>
               {backButtonText}
             </ModalText>
           )}
@@ -29,23 +45,23 @@ const ModalNavigator = (props: ModalNavigatorProps): JSX.Element => {
           {onBack && !backButtonText && (
             <Icon
               testID="modal-navigator-back-button"
-              name="arrow-back"
-              size={24}
+              name={ICON_NAME.ARROW_BACK}
+              size={SIZE.ICON}
               color={textColor}
             />
           )}
         </TouchableOpacity>
       </IconContainer>
-      <ModalText size={18} color={textColor}>
+      <ModalText size={SIZE.TITLE} color={textColor}>
         {title}
       </ModalText>
-      <IconContainer flexDirection="flex-end">
+      <IconContainer flexDirection={FLEX_DIRECTION.END}>
         <TouchableOpacity disabled={!onClose} onPress={onClose}>
           {onClose && (
             <Icon
               testID="modal-navigator-close-button"
-              name="close"
-              size={24}
+              name={ICON_NAME.CLOSE}
+              size={SIZE.ICON}
               color={textColor}
             />
           )}
