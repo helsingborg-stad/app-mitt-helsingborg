3e3b51c0f308064f61d2fcb07e65e66dd3bbf2ea
MasterKenth <MasterKenth@users.noreply.github.com>
2021-10-05 11:47:12 +0200

    docs: clarify length parameters in Aes functions

--- source/types/Encryption.ts
+++ source/types/Encryption.ts
@@ -14,12 +14,12 @@ export interface AesModule {
     password: string,
     salt: string,
     cost: number,
-    length: number
+    lengthInBits: number
   ) => Promise<string>;
   encrypt: (text: string, key: string, iv: string) => Promise<string>;
   decrypt: (ciphertext: string, key: string, iv: string) => Promise<string>;
   hmac256: (ciphertext: string, key: string) => Promise<string>;
-  randomKey: (length: number) => Promise<string>;
+  randomKey: (lengthInBytes: number) => Promise<string>;
   sha1: (text: string) => Promise<string>;
   sha256: (text: string) => Promise<string>;
   sha512: (text: string) => Promise<string>;
