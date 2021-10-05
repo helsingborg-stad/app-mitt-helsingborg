f2d5f9ad4a99401113a920af53cd97689e2894d7
MasterKenth <MasterKenth@users.noreply.github.com>
2021-10-05 11:54:48 +0200

    test: fix use test with 256-bit encryption for Aes

--- __tests__/Aes.ts
+++ __tests__/Aes.ts
@@ -15,96 +15,83 @@ describe("Aes", () => {
     "venenatis turpis quis consectetur. Suspendisse potenti.";
 
   const testEncryptedText =
-    "9d67ec30d1598a7866209cd555652413" +
-    "d24d174e4e8680e0168ab230c62dbb68" +
-    "c3eef17957d73397f64e3545f456a602" +
-    "68728e91331bbfafef68675074a12d83" +
-    "0e6d35f8dca51fe1f0a9585d741061f8" +
-    "2b0ed2b9b478f938f4497bb4029b5617" +
-    "cabb1029fded4a6e052ff7757224fd56" +
-    "42cb6eb795120481107c23213814e234" +
-    "d777bc95c0b5ca32a9459bdd25ee532e" +
-    "4705e06717fbaca36183ff8b2662a8d7" +
-    "41ccbc2c62e361e1a6bf627c23b8c237" +
-    "70c7bb4f31c17829de0b01ef03fa6111" +
-    "0ce118bf5e17f34f76fcb3b6e286d731" +
-    "c545bfa70410cdbcac853b7da4ee65f5" +
-    "4bab8d551c874b618b70e6ea5aba33ef" +
-    "230bce325a83c19e0ed8b3015e089cb8" +
-    "37aaea72514b891e76f6e1702e6f712c" +
-    "eb60a4c28be8617121cdace6dda86f0a" +
-    "ab6f8fadec8675369f805d9afae22d3f" +
-    "a693cea09aaa1fe6dafb028cbc97349d" +
-    "02a7f3fee1df77970e965558a799f6d2" +
-    "af4db6cc98bcd866ca57d11a8e31c173" +
-    "d8ba63406057b812c804d59b463167de" +
-    "80ec82672f2fcdf1c5c497b99b8a5180" +
-    "ad5e4122d93cb1f6ab6383623bfdde2a" +
-    "52f9b52938ae6bd4a0b60e5d3301e5e0" +
-    "09f7ea46e9c123fe51d931d1f01c34ec" +
-    "198a2f4ebf1a1fe833f6998650a092ed" +
-    "8d70d8fb7287e5a1282ca93ac8cb374d" +
-    "042c8d233999dd6a14f7aa25738ed37d" +
-    "90c32aa33361bf681c0915a5c4818fba" +
-    "f7775cac70f8432ba427851415bb69a6" +
-    "ddd89afdf01484970985c2fc5d877556";
+    "20746fe2518656d98c9093d12cbe3075" +
+    "8a09d142e594d293cef6bcd060271296" +
+    "848f87eb37b20640fa245ded8f528dbc" +
+    "f8c053eadc6ac44935c9a44694bcbb95" +
+    "d1b267f0c4fe6c2e34277ba0297a4dbf" +
+    "1c632ea77823b6a9c6438237614494ad" +
+    "9194b23b59aa542a8519622e81126878" +
+    "1070b1ac4d0a4e59a4e8aba1d75eed57" +
+    "5461a5bf3cb3ede0bc474627e0d4420d" +
+    "e1737add114c0a4d7e5c98e6d50d269c" +
+    "f6db6878a1021addf90231da3ea15516" +
+    "6ecfbee166d5e036be4ad1a12219759f" +
+    "1b985eee712a4440d856c828601e657b" +
+    "c78b199ac265c6f5b3916bce6aeadaaa" +
+    "8c3fbd3f0fe23fa5c62d4d0aa5c57ef9" +
+    "8d68ac96818581241e0f9f99cb868160" +
+    "23519f06be44cbd0bf977cf2b4b3b348" +
+    "9fc6db39570fb83a140ea67f7d786022" +
+    "ae2cce0d566a9db071ff625d12129b3b" +
+    "71a6c766c426c4188d6b68733ac2341b" +
+    "34ae45b22edf0e3106d5f18235808ba7" +
+    "1776870dead368d905ad8f0bb848c4c9" +
+    "6cb77c7a25c9eb2e843a998b438da575" +
+    "bcf75c2ba2b611bdbb5e24a0a27e42d7" +
+    "022e2255678be60fbf07da91b9513636" +
+    "478e0b412bd6e88c2d4126e621697a5b" +
+    "e2410a90851ef6a7c865d8c3923e4f3b" +
+    "f26aa403f354f2ae9fa1ae17af0b7ca0" +
+    "16c65fd0ecefa2a7b9226c092a9708a5" +
+    "aa0e0c20bbccf591272ca90f5234de44" +
+    "92e275b118d9735ae0e0a7fde5abddf3" +
+    "204ff620d693bf2bbf2533eea2de466e" +
+    "adef04a776305dd6efb6cd2f6ebd35c7";
 
-  const testKey = "3a2926938e179e7cfbc6d2162e16dbf1";
+  const testKey =
+    "f952122ebd198f5971a36886d495b45085bc6bf05f9697d4f732aff055db23f5";
   const testIV = "003d8999f6a4bb9800ed24b5d1846523";
   const testPassword = "boll1234";
   const testPBKDF2Salt = "salt4D42bf960Sm1";
   const testPBKDF2Rounds = 5000;
   const testPBKDF2Length = 256;
   const testPBKDF2Digest =
-    "a4451f49223eef2169a04c01951fea15" +
-    "c87dd11dca3aea763c81906e11c5464f" +
-    "12d120f4e7c90275f542ecfc23934226" +
-    "5edd285c0dc91d1235bc4168cee29cb5" +
-    "1a43db0b3f84f27fd6bd359fb361cd50" +
-    "8de94c9a550f3c19fc8188bff92f2f8c" +
-    "4f7cdb86619d54c2a41e788aeb8e9790" +
-    "721f829cc7cdef03b3ccd6f6b391aca8" +
-    "f31c11c17f2f51b5883cd9f8e5c69a91" +
-    "ce902cc2616501c43398fbc05842d580" +
-    "e9c30ab27b348954e8799f707b228cfb" +
-    "4bf6b9cdbaffe6aca618381eb8c234ca" +
-    "7fc46c3fbd6f7be4ae00576faede09d4" +
-    "4c8b8844cf5345ca7c6ae6b7658a4291" +
-    "f83419aede82b64d8a644faf6622546f" +
-    "be24245a140d42896eee2b53d480fb30";
+    "a4451f49223eef2169a04c01951fea15c87dd11dca3aea763c81906e11c5464f";
+  const testSHA1Digest = "8434f837d5dc6c649f56bf56fc239b47682433b2";
+  const testSHA256Digest =
+    "4de60032bc7e7171c6e69bd4a19f540c14f64284f9c67c5fd4d91651a696b577";
+  const testSHA512Digest =
+    "7865f93ceaac9e8b763815b216fbfefb4fef006746d1eac9997dcd74beb556283d5adec198926781398e84ab5400fa804f4d027d287500f36ca640b91b1ba189";
+  const testHMACDigest =
+    "98fdf315561cfad0f6c80373a5936c29dbee774fe52a2a87ad997db2237897eb";
 
   it("generates random key", async () => {
-    const newKey = await Aes.randomKey(16);
+    const newKey = await Aes.randomKey(32);
     expect(typeof newKey).toBe("string");
 
     const buff = Buffer.from(newKey, "hex");
-    expect(buff.byteLength).toBe(16);
+    expect(buff.byteLength).toBe(32);
   });
 
   it("produces a SHA1 hash", async () => {
     const hash = await Aes.sha1(testText);
-    expect(hash).toBe("8434f837d5dc6c649f56bf56fc239b47682433b2");
+    expect(hash).toBe(testSHA1Digest);
   });
 
   it("produces a SHA256 hash", async () => {
     const hash = await Aes.sha256(testText);
-    expect(hash).toBe(
-      "4de60032bc7e7171c6e69bd4a19f540c14f64284f9c67c5fd4d91651a696b577"
-    );
+    expect(hash).toBe(testSHA256Digest);
   });
 
   it("produces a SHA512 hash", async () => {
     const hash = await Aes.sha512(testText);
-    expect(hash).toBe(
-      "7865f93ceaac9e8b763815b216fbfefb4fef006746d1eac9997dcd74beb556283d5adec198926781398e84ab5400fa804f4d027d287500f36ca640b91b1ba189"
-    );
+    expect(hash).toBe(testSHA512Digest);
   });
 
   it("produces a HMAC256 digest", async () => {
     const digest = await Aes.hmac256(testText, testKey);
-    expect(digest).toBe(
-      "c90e9d1469a11d04525575c5973a68a34154ba60531bb1672ed83f1a13e7d050"
-    );
+    expect(digest).toBe(testHMACDigest);
   });
 
   it("encrypts", async () => {
@@ -126,6 +113,6 @@ describe("Aes", () => {
     );
     const buff = Buffer.from(derivedKey, "hex");
     expect(derivedKey).toBe(testPBKDF2Digest);
-    expect(buff.byteLength).toBe(testPBKDF2Length);
+    expect(buff.byteLength).toBe(testPBKDF2Length / 8);
   });
 });
