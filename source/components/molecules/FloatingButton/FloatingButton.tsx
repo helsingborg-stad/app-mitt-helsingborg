3b6e3bc437f6bf922d9f6cc57e33294c1fdac0e5
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-10-07 15:14:40 +0200

    feat: add floating button component

---
+++ source/components/molecules/FloatingButton/FloatingButton.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "styled-components/native";
+
+import MIcon from "../../atoms/Icon";
+
+import { ButtonContainer, ButtonText } from "./styled";
+
+interface FloatingButtonProps {
+  type: "icon" | "text";
+  onClick: () => void;
+  colorSchema?: "red" | "neutral";
+  text?: string;
+  iconName?: string;
+  position?: "left" | "center" | "right";
+}
+
+const FloatingButton = ({
+  type,
+  onClick,
+  colorSchema = "red",
+  text,
+  iconName,
+  position = "center",
+}: FloatingButtonProps): JSX.Element => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <ButtonContainer
+      testID="floatingButton"
+      onPress={onClick}
+      underlayColor={theme.colors.neutrals[4]}
+      colorSchema={colorSchema}
+      position={position}
+      type={type}
+    >
+      {type === "icon" && iconName ? (
+        <MIcon
+          testID="floatingButton_icon"
+          name={iconName}
+          color={theme.colors.neutrals[7]}
+          size={32}
+        />
+      ) : (
+        <ButtonText colorSchema={colorSchema}>{text}</ButtonText>
+      )}
+    </ButtonContainer>
+  );
+};
+
+export default FloatingButton;
