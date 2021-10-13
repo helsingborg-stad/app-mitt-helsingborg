e2cde0ce554048ee858bf252c546dd01ba0f5c64
MasterKenth <MasterKenth@users.noreply.github.com>
2021-10-13 10:28:00 +0200

    feat: pass form encryption to updateCase function

--- source/screens/FormCaseScreen.js
+++ source/screens/FormCaseScreen.js
@@ -94,6 +94,7 @@ const FormCaseScreen = ({ route, navigation, ...props }) => {
         signature,
         currentPosition,
         formQuestions,
+        encryption: initialCase.forms[initialCase.currentFormId].encryption,
       };
 
       // We set the initial case to prevent desync issues with the above logic.
