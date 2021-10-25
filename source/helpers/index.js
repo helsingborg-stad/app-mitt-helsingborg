ef649c482b1889f32b5f6e037a295daf081951fa
MasterKenth <MasterKenth@users.noreply.github.com>
2021-10-25 10:26:24 +0200

    feat: add function to wait for set time

--- source/helpers/index.js
+++ source/helpers/index.js
@@ -1,12 +1,13 @@
-import * as ApiRequest from './ApiRequest';
-import * as CaseDataConverter from './CaseDataConverter';
-import * as EventHandler from './EventHandler';
-import * as MarkdownConstructor from './MarkdownConstructor';
-import * as MessageHelper from './MessageHelper';
-import * as Objects from './Objects';
-import * as LaunchExternalApp from './LaunchExternalApp';
-import * as UrlHelper from './UrlHelper';
-import * as ValidationHelper from './ValidationHelper';
+import * as ApiRequest from "./ApiRequest";
+import * as CaseDataConverter from "./CaseDataConverter";
+import * as EventHandler from "./EventHandler";
+import * as MarkdownConstructor from "./MarkdownConstructor";
+import * as MessageHelper from "./MessageHelper";
+import * as Objects from "./Objects";
+import * as LaunchExternalApp from "./LaunchExternalApp";
+import * as UrlHelper from "./UrlHelper";
+import * as ValidationHelper from "./ValidationHelper";
+import * as Misc from "./Misc";
 
 export {
   ApiRequest,
@@ -18,4 +19,5 @@ export {
   LaunchExternalApp,
   UrlHelper,
   ValidationHelper,
+  Misc,
 };
---
+++ source/helpers/test/Misc.test.tsx
@@ -0,0 +1,22 @@
+import wait from "../Misc";
+
+describe("Misc helper functions", () => {
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test("wait", () => {
+    const DELAY = 500;
+    jest.useFakeTimers();
+    const callback = jest.fn();
+
+    wait(DELAY).then(callback);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(DELAY);
+
+    expect(callback).toHaveBeenCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
