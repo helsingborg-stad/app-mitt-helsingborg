007d01f72379e24f59da4fa73329134d535fbdde
Teddy Andersson <teddyandersson95@gmail.com>
2021-11-02 15:48:06 +0100

    test(case-data-converter): fixed linting and naming of tests for replaceTagPart

--- source/helpers/test/CaseDataConverter.test.tsx
+++ source/helpers/test/CaseDataConverter.test.tsx
@@ -1,23 +1,57 @@
-import {replaceTagPart} from '../CaseDataConverter'
+import { replaceTagPart } from '../CaseDataConverter';
 
 describe('replaceTagPart', () => {
   
-  it('replaces part in tag on single occurence', () => {
+  it('should replace part in tag on single occurence', () => {
     const input = {
       tag: 'test:example:replace',
       part: 'replace',
       value: 'success'
     }
     const output = 'test:example:success'
-    expect(replaceTagPart([...input]).equal(output))
+    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
   });
 
-  it('replaces part in tag on multiple occurences', () => {
-    expect()
+  it('should replace part in tag on multiple occurences', () => {
+    const input = {
+      tag: 'test:replace:example:replace',
+      part: 'replace',
+      value: 'success'
+    }
+    const output = 'test:example:success'
+    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
   });
 
-  it('does not replace part that contains value of other part in tag', () => {
-    expect()
+  it('should replace part in tag if part is only part', () => {
+    const input = {
+      tag: 'replace',
+      part: 'replace',
+      value: 'success'
+    }
+    const output = 'test:example:success'
+    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+  });
+
+  it('should not replace part in tag if a part contains the value of another part', () => {
+    const input = {
+      tag: 'test:examplereplace:replace',
+      part: 'replace',
+      value: 'success'
+    }
+
+    const output = 'test:examplereplace:success'
+    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+  });
+
+  it('should not replace any parts', () => {
+    const input = {
+      tag: 'test:example:other',
+      part: 'replace',
+      value: 'success'
+    }
+
+    const output = input.tag
+    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
   });
 
 })
