e4818ac37c53b478f713b3e07ace385ef1882912
Teddy Andersson <teddyandersson95@gmail.com>
2021-11-03 10:09:04 +0100

    test(case-data-converter): refactoring test declaration to be less dry

--- source/helpers/test/CaseDataConverter.test.tsx
+++ source/helpers/test/CaseDataConverter.test.tsx
@@ -1,67 +1,77 @@
 import { replaceTagPart } from '../CaseDataConverter';
 
+
+
+
+
 describe('replaceTagPart', () => {
-  
-  it('should replace part in tag on single occurence', () => {
-    const input = {
-      tag: 'test:example:replace',
+  let input: { tag: string, part: string, value: string };
+
+  beforeEach(() => {
+    input = {
+      tag: '',
       part: 'replace',
       value: 'success'
-    }
+    };
+  });
+  
+  it('should replace part in tag on single occurence', () => {
+    input.tag = 'test:example:replace'
+
     const output = 'test:example:success'
-    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+
+    const result = replaceTagPart(input.tag, input.part, input.value)
+
+    expect(result).toBe(output);
   });
 
   it('should replace part at beginning of tag', () => {
-    const input = {
-      tag: 'replace:example:test',
-      part: 'replace',
-      value: 'success'
-    }
+    input.tag = 'replace:example:test'
+
     const output = 'success:example:test'
-    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+
+    const result = replaceTagPart(input.tag, input.part, input.value)
+
+    expect(result).toBe(output)
   });
 
   it('should replace part in tag on multiple occurences', () => {
-    const input = {
-      tag: 'test:replace:example:replace',
-      part: 'replace',
-      value: 'success'
-    }
+    input.tag = 'test:replace:example:replace'
+    
     const output = 'test:success:example:success'
-    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+
+    const result = replaceTagPart(input.tag, input.part, input.value);
+
+    expect(result).toBe(output)
   });
 
   it('should replace part in tag if part is only part', () => {
-    const input = {
-      tag: 'replace',
-      part: 'replace',
-      value: 'success'
-    }
+    input.tag = 'replace'
+
     const output = 'success'
-    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+
+    const result = replaceTagPart(input.tag, input.part, input.value);
+    expect(result).toBe(output)
   });
 
   it('should not replace part in tag if a part contains the value of another part', () => {
-    const input = {
-      tag: 'test:examplereplace:replace',
-      part: 'replace',
-      value: 'success'
-    }
+    input.tag = 'test:examplereplace:replace'
 
     const output = 'test:examplereplace:success'
-    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+
+    const result = replaceTagPart(input.tag, input.part, input.value);
+
+    expect(result).toBe(output)
   });
 
   it('should not replace any parts', () => {
-    const input = {
-      tag: 'test:example:other',
-      part: 'replace',
-      value: 'success'
-    }
+    input.tag = 'test:example:other'
 
     const output = input.tag
-    expect(replaceTagPart(input.tag, input.part, input.value)).toEqual(output)
+
+    const result = replaceTagPart(input.tag, input.part, input.value);
+
+    expect(result).toBe(output)
   });
 
 })
