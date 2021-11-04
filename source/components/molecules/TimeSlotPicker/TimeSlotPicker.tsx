0401cb359cab418848c93c9a8de4da499fadda06
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-04 16:58:14 +0100

    fix: remove unused import

--- source/components/molecules/TimeSlotPicker/TimeSlotPicker.tsx
+++ source/components/molecules/TimeSlotPicker/TimeSlotPicker.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import stringify from "json-stable-stringify";
 import { DayPicker, TimeSpanButton } from "..";
 
 interface TimeSpan {
