846301be3adabae123ec1d5cd73bdfe4b0243e5a
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-05 10:12:36 +0100

    fix: rename start and end to startTime and endTime

--- source/helpers/MockBookingData.ts
+++ source/helpers/MockBookingData.ts
@@ -2,7 +2,7 @@ import moment from "moment";
 
 export interface BookingItem {
   date: string;
-  time: { start: string; end: string };
+  time: { startTime: string; endTime: string };
   title: string;
   status: string;
 }
@@ -10,25 +10,25 @@ export interface BookingItem {
 const mockBookingData: BookingItem[] = [
   {
     date: moment().add(2, "days").format("yyyy-MM-DD"),
-    time: { start: "11.00", end: "12.00" },
+    time: { startTime: "11.00", endTime: "12.00" },
     title: "Event 1",
     status: "Busy",
   },
   {
     date: moment().add(10, "days").format("yyyy-MM-DD"),
-    time: { start: "11.00", end: "12.00" },
+    time: { startTime: "11.00", endTime: "12.00" },
     title: "Event 2",
     status: "Busy",
   },
   {
     date: "2022-02-18",
-    time: { start: "13.00", end: "14.00" },
+    time: { startTime: "13.00", endTime: "14.00" },
     title: "Event 4",
     status: "Busy",
   },
   {
     date: moment().add(45, "days").format("yyyy-MM-DD"),
-    time: { start: "10.00", end: "11.00" },
+    time: { startTime: "10.00", endTime: "11.00" },
     title: "Event 3",
     status: "Busy",
   },
