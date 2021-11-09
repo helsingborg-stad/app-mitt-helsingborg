30c33fe4e12aab171b3960db86145a3282ea0acf
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-09 09:33:47 +0100

    fix: desctructure props object in styled components

--- source/components/molecules/AddressCard/styled.ts
+++ source/components/molecules/AddressCard/styled.ts
@@ -2,15 +2,15 @@ import styled from "styled-components/native";
 import { Button } from "../../atoms";
 
 const TitleText = styled.Text`
-  font-size: ${(props) => props.theme.fontSizes[2]}px;
-  font-weight: ${(props) => props.theme.fontWeights[1]};
-  ${(props) => `color: ${props.theme.colors.primary.red[0]};`}
+  font-size: ${({ theme }) => theme.fontSizes[2]}px;
+  font-weight: ${({ theme }) => theme.fontWeights[1]};
+  ${({ theme }) => `color: ${theme.colors.primary.red[0]};`}
   padding: 5px;
 `;
 
 const AddressText = styled.Text`
-  font-size: ${(props) => props.theme.fontSizes[5]}px;
-  font-weight: ${(props) => props.theme.fontWeights[0]};
+  font-size: ${({ theme }) => theme.fontSizes[5]}px;
+  font-weight: ${({ theme }) => theme.fontWeights[0]};
   padding: 2px 0px;
   margin: 0;
 `;
