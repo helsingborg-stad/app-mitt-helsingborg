664b18e137b20ae6afdca48debe41df5a66d83f1
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-11 15:53:03 +0100

    feat: add CalendarScreen to CalendarNavigator stack

--- source/navigator/CalendarNavigator.tsx
+++ source/navigator/CalendarNavigator.tsx
@@ -1,20 +1,57 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import styled from "styled-components/native";
+import { Icon } from "../components/atoms";
 import CalendarScreen from "../screens/bookingScreens/CalendarScreen";
+import BookingSummary from "../screens/bookingScreens/BookingSummary";
+
+const TouchWrapper = styled.TouchableOpacity`
+  margin-left: 16px;
+  margin-top: 3px;
+  border-radius: 17px;
+  padding: 5px;
+`;
 
 const Stack = createStackNavigator();
 
-const CalendarNavigator = (): JSX.Element => (
-  <Stack.Navigator
-    screenOptions={{ headerShown: false }}
-    initialRouteName="CalendarScreen"
-  >
-    <Stack.Screen
-      name="CalendarScreen"
-      component={CalendarScreen}
-      options={{ title: "Kalender" }}
-    />
-  </Stack.Navigator>
-);
+interface CalendarNavigatorProps {
+  navigation: any;
+}
+
+const CalendarNavigator = ({
+  navigation,
+}: CalendarNavigatorProps): JSX.Element => {
+  const BackButton = () => (
+    <TouchWrapper
+      activeOpacity={0.2}
+      onPress={() => {
+        navigation.navigate("CaseOverview");
+      }}
+    >
+      <Icon name="arrow-back" />
+    </TouchWrapper>
+  );
+
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="CalendarScreen"
+    >
+      <Stack.Screen
+        name="CalendarScreen"
+        component={CalendarScreen}
+        options={{ title: "Kalender" }}
+      />
+      <Stack.Screen
+        name="BookingSummary"
+        component={BookingSummary}
+        options={{
+          title: "Bokning",
+          headerLeft: () => <BackButton />,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default CalendarNavigator;
