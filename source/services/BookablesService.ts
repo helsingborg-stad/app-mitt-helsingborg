9f53bf536dc57555b9b3cd9902ed3a7b565c7a04
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-11 11:46:14 +0100

    feat: add bookables service

---
+++ source/services/BookablesService.ts
@@ -0,0 +1,27 @@
+import { get } from "../helpers/ApiRequest";
+
+export type BookableItem = {
+  name: string;
+  sharedMailbox: string;
+  address: string;
+  formId: string;
+};
+
+async function getBookables(): Promise<BookableItem[]> {
+  return new Promise((resolve, reject) => {
+    get("/bookables")
+      .then((response) => {
+        if (response.status !== 200) {
+          reject(new Error(response.message));
+        }
+        const bookables = response?.data?.data as BookableItem[];
+        if (bookables) return resolve(bookables);
+        return reject(
+          new Error("getBookables: Response does not contain data.data")
+        );
+      })
+      .catch((error) => reject(error));
+  });
+}
+
+export { getBookables };
