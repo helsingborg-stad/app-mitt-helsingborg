a19ff2c109b51faf48b6f20d1ed4800178985763
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-11 12:24:34 +0100

    fix: add BookablesService to services index

--- source/services/index.js
+++ source/services/index.js
@@ -1,4 +1,5 @@
-import * as ChatFormService from './ChatFormService';
-import * as StorageService from './StorageService';
+import * as ChatFormService from "./ChatFormService";
+import * as StorageService from "./StorageService";
+import * as BookablesService from "./BookablesService";
 
-export { ChatFormService, StorageService };
+export { ChatFormService, StorageService, BookablesService };
