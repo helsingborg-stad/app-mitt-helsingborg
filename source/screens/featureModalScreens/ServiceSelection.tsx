4ba136a594ecf746f239460916eb9b809eadd795
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-11 12:33:51 +0100

    feat: update serviceselection to retireve from backend

--- source/screens/featureModalScreens/ServiceSelection.tsx
+++ source/screens/featureModalScreens/ServiceSelection.tsx
@@ -1,49 +1,63 @@
-import React, { useMemo } from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import styled from "styled-components/native";
+import { Text } from "../../components/atoms";
+import { BookableItem } from "../../services/BookablesService";
+import { BookablesService } from "../../services";
+import theme from "../../styles/theme";
 
 import ButtonList from "../../components/organisms/ButtonList";
+
+const ErrorText = styled(Text)`
+  margin: 16px;
+`;
 
 interface Props {
   onNavigate: (newRoute: string) => void;
 }
+
+type ButtonType = {
+  buttonText: string;
+  icon: string;
+  onClick: () => void;
+};
+
 const ServiceSelection = ({ onNavigate }: Props): JSX.Element => {
-  const buttons = useMemo(
-    () => [
-      {
-        buttonText: "Mina kontakter",
-        icon: "person",
-        onClick: () => onNavigate("App"),
-        colorSchema: "red",
-        variant: "contained",
-      },
-      {
-        buttonText: "Budget- och skuldrådgivning",
-        icon: "photo-camera",
-        onClick: () => onNavigate("App"),
-      },
-      {
-        buttonText: "Kostnadsfri och oberoende energi- och klimatrådgivning",
-        icon: "photo-camera",
-        onClick: () => onNavigate("App"),
-      },
-      {
-        buttonText: "Konsumentrådgivning",
-        icon: "photo-camera",
-        onClick: () => onNavigate("App"),
-      },
-      {
-        buttonText: "Rådgivare för personer med funktionsnedsättning",
-        icon: "photo-camera",
-        onClick: () => onNavigate("App"),
-      },
-      {
-        buttonText: "Seniorrådgivning",
-        icon: "photo-camera",
-        onClick: () => onNavigate("App"),
-      },
-    ],
-    [onNavigate]
-  );
+  const [isLoading, setLoading] = useState(true);
+  const [buttons, setButtons] = useState<ButtonType[]>([]);
+  const [error, setError] = useState<Error | undefined>(undefined);
+
+  useEffect(() => {
+    let canceled = false;
+    BookablesService.getBookables()
+      .then((bookables: BookableItem[]) => {
+        if (!canceled) {
+          setButtons(
+            bookables.map(
+              (bookable: BookableItem) =>
+                ({
+                  buttonText: bookable.name,
+                  icon: "photo-camera",
+                  onClick: () => true,
+                } as ButtonType)
+            )
+          );
+          setLoading(false);
+        }
+      })
+      .catch((serviceError) => {
+        setError(serviceError);
+        setLoading(false);
+      });
+    return () => {
+      canceled = true;
+    };
+  }, []);
 
   return (
     <ScrollView
@@ -51,11 +65,27 @@ const ServiceSelection = ({ onNavigate }: Props): JSX.Element => {
       style={{ backgroundColor: "white" }}
     >
       <SafeAreaView style={{ backgroundColor: "white" }}>
-        <ButtonList
-          buttonList={buttons}
-          defaultColorSchema="red"
-          defaultVariant="link"
-        />
+        {isLoading && (
+          <ActivityIndicator
+            size="large"
+            color={
+              Platform.OS === "ios" ? undefined : theme.colors.primary.red[0]
+            }
+            style={{ marginTop: 30 }}
+          />
+        )}
+        {error && (
+          <ErrorText type="h5">
+            Ett fel har inträffat. Vänligen försök igen.
+          </ErrorText>
+        )}
+        {!error && !isLoading && (
+          <ButtonList
+            buttonList={buttons}
+            defaultColorSchema="red"
+            defaultVariant="link"
+          />
+        )}
       </SafeAreaView>
     </ScrollView>
   );
