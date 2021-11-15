dffb316dc4a5778da0fcd66ac36835c8b7001965
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-15 15:57:15 +0100

    feat: add shared mailbox administrator lookup endpoint

--- source/services/BookablesService.ts
+++ source/services/BookablesService.ts
@@ -7,14 +7,32 @@ export type BookableItem = {
   formId: string;
 };
 
-async function getBookables(): Promise<BookableItem[]> {
+const getBookables = async (): Promise<BookableItem[]> => {
   const response = await get("/bookables");
   if (response.status !== 200) {
     throw new Error(response.message);
   }
   const bookables = response?.data?.data as BookableItem[];
   if (bookables) return bookables;
-  throw new Error("getBookables: Response does not contain data.data");
-}
+  throw new Error(
+    "getBookables: Response does not contain data.data.attributes"
+  );
+};
 
-export { getBookables };
+const getAdministratorsBySharedMailbox = async (
+  sharedMailbox: string
+): Promise<string[]> => {
+  const response = await get(
+    `/bookables/getAdministratorsByEmail/${sharedMailbox}`
+  );
+  if (response.status !== 200) {
+    throw new Error(response.message);
+  }
+  const admins = response?.data?.data?.attributes;
+  if (admins) return admins;
+  throw new Error(
+    "getAdministratorsBySharedMailbox: Response does not contain data.data.attributes"
+  );
+};
+
+export { getBookables, getAdministratorsBySharedMailbox };
