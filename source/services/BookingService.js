097ef8dc4c5966d44dc13cde9191d726b73f30c1
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-11-15 16:19:09 +0100

    feat: add endpoints required for booking flow

--- source/services/BookingService.js
+++ source/services/BookingService.js
@@ -1,6 +1,7 @@
-import { post } from "../helpers/ApiRequest";
+import moment from "moment";
+import { get, post, remove } from "../helpers/ApiRequest";
 
-async function createBooking(
+const createBooking = async (
   requiredAttendees,
   optionalAttendees,
   startTime,
@@ -8,34 +9,76 @@ async function createBooking(
   refCode,
   address,
   message
-) {
-  return new Promise((resolve, reject) => {
-    const body = {
-      requiredAttendees,
-      optionalAttendees,
-      startTime,
-      endTime,
-      subject: "Mitt Helsingborg bokning",
-      referenceCode: refCode,
-      body: `Du har fått en bokning ifrån Mitt Helsingborg. Klicka på Acceptera för att bekräfta bokningen.\n\n${message}`,
-      location: address,
-    };
+) => {
+  const body = {
+    requiredAttendees,
+    optionalAttendees,
+    startTime: moment(startTime).format(),
+    endTime: moment(endTime).format(),
+    subject: "Mitt Helsingborg bokning",
+    referenceCode: refCode,
+    body: `Du har fått en bokning ifrån Mitt Helsingborg. Klicka på Acceptera för att bekräfta bokningen.\n\n${message}`,
+    location: address,
+  };
 
-    post("/booking", body)
-      .then((response) => {
-        if (response.status !== 200) {
-          throw new Error(
-            response?.message ||
-              `createBooking: Recieved error ${response.status}`
-          );
-        }
+  const response = await post("/booking", body);
+  if (response.status !== 200) {
+    throw new Error(
+      response?.message || `createBooking: Recieved error ${response.status}`
+    );
+  }
 
-        const booked = response?.data?.data;
-        if (booked) return resolve(booked);
-        throw new Error("createBooking: Response has no data.data property");
-      })
-      .catch((error) => reject(error));
+  const booked = response?.data?.data?.attributes;
+  if (booked) return booked;
+  throw new Error(
+    "createBooking: Response does not contain data.data.attributes"
+  );
+};
+
+const getTimeSlots = async (attendees, startTime, endTime) => {
+  const response = await post(`/timeslots/getTimeSlots`, {
+    attendees,
+    startTime,
+    endTime,
   });
-}
+  if (response.status !== 200) {
+    throw new Error(
+      response?.message || `getTimeSlots: Recieved error ${response.status}`
+    );
+  }
 
-export { createBooking };
+  const timeSlots = response?.data?.data?.attributes;
+  if (timeSlots) return timeSlots;
+  throw new Error(
+    "getTimeSlots: Response does not contain data.data.attributes"
+  );
+};
+
+const deleteBooking = async (id) => {
+  const response = await remove(`/booking/${encodeURIComponent(id)}`);
+  if (response.status !== 200) {
+    throw new Error(
+      response?.message || `deleteBooking: Recieved error ${response.status}`
+    );
+  }
+
+  const success = response?.data?.data?.attributes;
+  if (success) return success;
+  throw new Error(
+    "deleteBooking: Response does not contain data.data.attributes"
+  );
+};
+
+const searchBookings = async (referenceCode, startTime, endTime) => {
+  const response = await get(
+    `/booking/search/${encodeURIComponent(referenceCode)}` +
+      `?startTime=${startTime}&endTime=${endTime}`
+  );
+  const bookings = response?.data?.data?.attributes;
+  if (bookings) return bookings;
+  throw new Error(
+    "searchBookings: Response does not contain data.data.attributes"
+  );
+};
+
+export { createBooking, getTimeSlots, searchBookings, deleteBooking };
