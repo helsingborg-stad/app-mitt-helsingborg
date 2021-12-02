b5a174300d7d9be668a30d9ec552ccbe6f355c62
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-12-02 10:06:17 +0100

    lint: destruct styled component props

--- source/components/molecules/DateTimeCard/styled.ts
+++ source/components/molecules/DateTimeCard/styled.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
+import { ThemeType } from "../../../styles/themeHelpers";
 
-const Container = styled.View<{ align: string }>`
+const Container = styled.View<{ align: string; theme: ThemeType }>`
   margin-top: 10px;
   border-width: 3px;
   border-radius: 5px;
@@ -8,36 +9,30 @@ const Container = styled.View<{ align: string }>`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: ${(props) => props.align};
-  ${(props) => `background-color: ${props.theme.colors.complementary.red[3]};`}
-  ${(props) => `border-color: ${props.theme.colors.complementary.red[2]};`}
+  align-items: ${({ align }) => align};
+  ${({ theme }) => `background-color: ${theme.colors.complementary.red[3]};`}
+  ${({ theme }) => `border-color: ${theme.colors.complementary.red[2]};`}
 `;
 
-const TitleText = styled.Text<{ size: string }>`
-  font-size: ${(props) =>
-    props.size === "large"
-      ? props.theme.fontSizes[2]
-      : props.theme.fontSizes[1]}px;
-  font-weight: ${(props) => props.theme.fontWeights[1]};
-  ${(props) => `color: ${props.theme.colors.primary.red[0]};`}
+const TitleText = styled.Text<{ size: string; theme: ThemeType }>`
+  font-size: ${({ size, theme }) =>
+    size === "large" ? theme.fontSizes[2] : theme.fontSizes[1]}px;
+  font-weight: ${({ theme }) => theme.fontWeights[1]};
+  ${({ theme }) => `color: ${theme.colors.primary.red[0]};`}
   padding: 5px;
 `;
 
-const DateText = styled.Text<{ size: string }>`
-  font-size: ${(props) =>
-    props.size === "large"
-      ? props.theme.fontSizes[6]
-      : props.theme.fontSizes[5]}px;
-  font-weight: ${(props) => props.theme.fontWeights[1]};
+const DateText = styled.Text<{ size: string; theme: ThemeType }>`
+  font-size: ${({ size, theme }) =>
+    size === "large" ? theme.fontSizes[6] : theme.fontSizes[5]}px;
+  font-weight: ${({ theme }) => theme.fontWeights[1]};
   padding: 5px;
 `;
 
-const TimeText = styled.Text<{ size: string }>`
-  font-size: ${(props) =>
-    props.size === "large"
-      ? props.theme.fontSizes[4]
-      : props.theme.fontSizes[3]}px;
-  font-weight: ${(props) => props.theme.fontWeights[0]};
+const TimeText = styled.Text<{ size: string; theme: ThemeType }>`
+  font-size: ${({ size, theme }) =>
+    size === "large" ? theme.fontSizes[4] : theme.fontSizes[3]}px;
+  font-weight: ${({ theme }) => theme.fontWeights[0]};
   padding: 5px;
 `;
 
