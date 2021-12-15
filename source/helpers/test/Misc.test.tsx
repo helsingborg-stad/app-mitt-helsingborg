4086b37776da4a6c9463619cb6e96ebd7b58928d
MasterKenth <MasterKenth@users.noreply.github.com>
2021-12-15 16:04:34 +0100

    feat: add function to construct a user-friendly version string

--- source/helpers/test/Misc.test.tsx
+++ source/helpers/test/Misc.test.tsx
@@ -1,4 +1,4 @@
-import { wait } from "../Misc";
+import { getAPIEnvironmentIdentifierFromUrl, wait } from "../Misc";
 
 describe("Misc helper functions", () => {
   afterEach(() => {
@@ -10,7 +10,7 @@ describe("Misc helper functions", () => {
     jest.useFakeTimers();
     const callback = jest.fn();
 
-    wait(DELAY).then(callback);
+    void wait(DELAY).then(callback);
 
     expect(callback).not.toHaveBeenCalled();
 
@@ -18,5 +18,55 @@ describe("Misc helper functions", () => {
 
     expect(callback).toHaveBeenCalled();
     expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("getAPIEnvironmentIdentifierFromUrl", () => {
+  const devUrl = "https://dev.api.helsingborg.io";
+  const releaseUrl = "https://release.api.helsingborg.io";
+  const prodUrl = "https://api.helsingborg.io";
+
+  it("returns the correct letter for a supported environment", () => {
+    const devLetter = getAPIEnvironmentIdentifierFromUrl(devUrl);
+    const releaseLetter = getAPIEnvironmentIdentifierFromUrl(releaseUrl);
+
+    expect(devLetter).toEqual("d");
+    expect(releaseLetter).toEqual("r");
+  });
+
+  it("returns production letter for production environment", () => {
+    const prodLetter = getAPIEnvironmentIdentifierFromUrl(prodUrl);
+
+    expect(prodLetter).toEqual("p");
+  });
+
+  it("returns null on unexpected urls", () => {
+    const invalidUrl = getAPIEnvironmentIdentifierFromUrl("not a valid url");
+    const unexpectedPrefix = getAPIEnvironmentIdentifierFromUrl(
+      "https://thisdoesnotexist123.api.helsingborg.io"
+    );
+    const malformedUrl = getAPIEnvironmentIdentifierFromUrl(
+      "https://.api.helsingborg.io"
+    );
+
+    expect(invalidUrl).toBeNull();
+    expect(unexpectedPrefix).toBeNull();
+    expect(malformedUrl).toBeNull();
+  });
+
+  it("returns null for unexpected reserved keywords", () => {
+    const constructorValue = getAPIEnvironmentIdentifierFromUrl(
+      "https://constructor.api.helsingborg.io"
+    );
+    const hasOwnPropertyValue = getAPIEnvironmentIdentifierFromUrl(
+      "https://hasOwnProperty.api.helsingborg.io"
+    );
+    const protoValue = getAPIEnvironmentIdentifierFromUrl(
+      "https://__proto__.api.helsingborg.io"
+    );
+
+    expect(constructorValue).toBeNull();
+    expect(hasOwnPropertyValue).toBeNull();
+    expect(protoValue).toBeNull();
   });
 });
