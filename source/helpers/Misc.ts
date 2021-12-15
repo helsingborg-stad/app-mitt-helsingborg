1e9176f11a6fcdc57dfdf837efa34775c7cf93d8
MasterKenth <MasterKenth@users.noreply.github.com>
2021-12-15 16:04:34 +0100

    feat: add function to construct a user-friendly version string

--- source/helpers/Misc.ts
+++ source/helpers/Misc.ts
@@ -1,4 +1,6 @@
 import { Dimensions } from "react-native";
+import Config from "react-native-config";
+import DeviceInfo from "react-native-device-info";
 
 export function wait(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -6,4 +8,57 @@ export function wait(ms: number): Promise<void> {
 
 export function getScreenHeightProportion(proportion: number): number {
   return Math.floor((Dimensions.get("window").height * proportion) / 100);
+}
+
+export function getAPIEnvironmentIdentifierFromUrl(
+  apiUrl: string
+): string | null {
+  const apiUrlPrefixEnvironmentMapping: Record<string, string> = {
+    dev: "d",
+    release: "r",
+    staging: "s",
+    test: "t",
+  };
+
+  const prefixRegex = /^https:\/\/(?:(.*)\.)?api\.helsingborg.io/i;
+
+  const apiUrlLowercase = apiUrl.toLowerCase();
+  const match = apiUrlLowercase.match(prefixRegex);
+
+  if (match) {
+    const apiPrefix = match[1];
+
+    if (apiPrefix === undefined) {
+      return "p";
+    }
+
+    const acceptablePrefixLetter = apiUrlPrefixEnvironmentMapping[apiPrefix];
+
+    if (typeof acceptablePrefixLetter === "string") {
+      return acceptablePrefixLetter;
+    }
+  }
+  return null;
+}
+
+export function getAPIEnvironmentIdentifier(): string | null {
+  const isDev = Config.APP_ENV === "development";
+
+  const apiUrl = isDev
+    ? Config.MITTHELSINGBORG_IO_DEV
+    : Config.MITTHELSINGBORG_IO;
+
+  return getAPIEnvironmentIdentifierFromUrl(apiUrl);
+}
+
+export function getUserFriendlyAppVersion(): string {
+  const SHORT_HASH_LENGTH = 7;
+
+  const semver = DeviceInfo.getVersion();
+  const build = DeviceInfo.getBuildNumber();
+  const envLetter = getAPIEnvironmentIdentifier() ?? "";
+  const hash = Config.GIT_COMMIT_HASH ?? "";
+  const shortHash = hash.length > 7 ? hash.substr(0, SHORT_HASH_LENGTH) : hash;
+
+  return `v${semver}-${build}${envLetter}-${shortHash}`;
 }
