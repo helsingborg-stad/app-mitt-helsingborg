e23c1e5979d1ce54be74e60366afe697c79cbfe1
Petter Andersson <petter.andersson@helsingborg.se>
2021-12-27 14:07:33 +0100

    Refactor and optimize

--- source/services/ConfigurationService.ts
+++ source/services/ConfigurationService.ts
@@ -4,6 +4,7 @@ const URL_SUFFIX = "_MITTHELSINGBORG_IO";
 const API_SUFFIX = "_MITTHELSINGBORG_IO_APIKEY";
 
 interface Endpoint {
+  name: string;
   baseUrl: string;
   apiKey: string;
 }
@@ -15,51 +16,50 @@ interface EnvironmentOption {
 }
 
 export default class ConfigurationService {
-  // eslint-disable-next-line no-useless-constructor
+  private endpoints: Endpoint[];
+
+  private endpoint: Endpoint;
+
   constructor(
-    private readonly source: NativeConfig | { [key: string]: string } = env
-  ) {}
+    readonly configurationNames: string[],
+    readonly source: NativeConfig | { [key: string]: string }
+  ) {
+    this.endpoints = configurationNames
+      .map((name) => ({
+        name,
+        baseUrl: source[`${name}${URL_SUFFIX}`],
+        apiKey: source[`${name}${API_SUFFIX}`],
+      }))
+      .filter(({ baseUrl, apiKey }) => baseUrl && apiKey);
+    [this.endpoint] = this.endpoints;
+  }
 
   /**
    * Returns the environments specified in the API_ENVS variable as an array
    */
   get environments(): string[] {
-    return this.source.API_ENVS?.split(",") ?? ["DEVELOP"];
+    return this.endpoints.map(({ name }) => name);
   }
 
   /**
    * Returns the endpoint first environment specified in the API_ENVS variable
    */
-  get defaultEndpoint(): Endpoint {
-    return this.getNamedEndpoint(this.environments[0]);
+  get activeEndpoint(): Endpoint {
+    return this.endpoint;
+  }
+
+  set activeEndpoint(endpoint: Endpoint) {
+    this.endpoint = endpoint;
   }
 
   /**
    * Returns the complete set of environment options in a format that could be consumed by a picker
    */
   get environmentOptions(): EnvironmentOption[] {
-    return this.environments.map((label, key) => ({
-      label,
-      value: this.getNamedEndpoint(label),
+    return this.endpoints.map((endpoint, key) => ({
+      label: endpoint.name,
+      value: endpoint,
       key,
     }));
-  }
-
-  /**
-   * Extracts endpoint information from a name value set
-   * @param name The name of the environment as it appears in the API_ENVS key
-   * @returns The url and apikey of the endpoint
-   */
-  getNamedEndpoint(name: string): Endpoint {
-    const baseUrl = this.source[`${name}${URL_SUFFIX}`];
-    const apiKey = this.source[`${name}${API_SUFFIX}`];
-
-    if (baseUrl && apiKey) {
-      return {
-        baseUrl,
-        apiKey,
-      };
-    }
-    throw new Error(`Missing Environment variable for key ${name}`);
   }
 }
