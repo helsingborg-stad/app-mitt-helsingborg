21f61e69e202486a403fd58af9e62782d66dbb12
Karl <karl.mosenbacher@knowit.se>
2022-01-10 13:48:30 +0100

    put completions descriptions into one variable before return

--- source/helpers/FormatCompletions.ts
+++ source/helpers/FormatCompletions.ts
@@ -3,7 +3,7 @@ import { RequestedCompletions } from "../types/Case";
 const getUnapprovedCompletionDescriptions = (
   completions: RequestedCompletions[]
 ): string[] => {
-  return completions
+  const completionDescriptions = completions
     .filter(({ approved = false }) => !approved)
     .map(({ description }) => description);
 
