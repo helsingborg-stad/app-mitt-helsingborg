eae123e67e0450863286029b56cd2625b74816d0
Karl <karl.mosenbacher@knowit.se>
2022-01-19 10:15:29 +0100

    change default api status message as described in clickup

--- source/services/ApiStatusService.ts
+++ source/services/ApiStatusService.ts
@@ -4,7 +4,7 @@ const getApiStatus = async (): Promise<string> => {
   const response = await get("/status");
 
   if (response.status !== 200) {
-    return "Just nu har vi tekniska problem i appen. Vi jobbar på att lösa dessa. Kom tillbaka om en liten stund och testa igen.";
+    return "Tjänsten är för nuvarande otillgänglig. Försök igen senare.";
   }
 
   return response?.data?.data?.attributes?.message || "";
