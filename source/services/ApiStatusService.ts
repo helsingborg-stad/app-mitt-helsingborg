c872bbc01fa47c5c315528a1b596df78d0a2de50
Karl <karl.mosenbacher@knowit.se>
2022-01-19 10:47:59 +0100

    fix spelling on default api status message

--- source/services/ApiStatusService.ts
+++ source/services/ApiStatusService.ts
@@ -4,7 +4,7 @@ const getApiStatus = async (): Promise<string> => {
   const response = await get("/status");
 
   if (response.status !== 200) {
-    return "Tjänsten är för nuvarande otillgänglig. Försök igen senare.";
+    return "Tjänsten är för närvarande otillgänglig. Försök igen senare.";
   }
 
   return response?.data?.data?.attributes?.message || "";
