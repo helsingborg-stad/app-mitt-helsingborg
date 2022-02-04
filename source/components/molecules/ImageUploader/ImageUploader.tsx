348f860aa7a868398b0d67d3aa4f2fbf9395c0ca
MasterKenth <MasterKenth@users.noreply.github.com>
2022-02-04 09:33:46 +0100

    style: style ImageUploader.tsx

--- source/components/molecules/ImageUploader/ImageUploader.tsx
+++ source/components/molecules/ImageUploader/ImageUploader.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { TouchableOpacity } from 'react-native';
-import ImagePicker, { Options, ImageOrVideo } from 'react-native-image-crop-picker';
-import styled from 'styled-components/native';
-import { Text, Button, Icon, Label } from '../../atoms';
-import { Modal, useModal } from '../Modal';
-import { getBlob, uploadFile } from '../../../helpers/FileUpload';
-import { getValidColorSchema, PrimaryColor } from '../../../styles/themeHelpers';
-import ImageDisplay, { Image } from '../ImageDisplay/ImageDisplay';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { TouchableOpacity } from "react-native";
+import ImagePicker, { ImageOrVideo } from "react-native-image-crop-picker";
+import styled from "styled-components/native";
+import { Text, Button, Icon, Label } from "../../atoms";
+import { Modal, useModal } from "../Modal";
+import { getBlob, uploadFile } from "../../../helpers/FileUpload";
+import {
+  getValidColorSchema,
+  PrimaryColor,
+} from "../../../styles/themeHelpers";
+import ImageDisplay, { Image } from "../ImageDisplay/ImageDisplay";
 
 const Wrapper = styled.View`
   padding-left: 0;
@@ -35,7 +37,7 @@ const BackgroundBlur = styled.View`
   background-color: rgba(0, 0, 0, 0.25);
 `;
 
-const PopupContainer = styled.View<{colorSchema: PrimaryColor}>`
+const PopupContainer = styled.View<{ colorSchema: PrimaryColor }>`
   position: absolute;
   z-index: 1000;
   top: 60%;
@@ -43,7 +45,8 @@ const PopupContainer = styled.View<{colorSchema: PrimaryColor}>`
   right: 5%;
   padding: 20px;
   width: 90%;
-  background-color: ${(props) => props.theme.colors.complementary[props.colorSchema][0]};
+  background-color: ${(props) =>
+    props.theme.colors.complementary[props.colorSchema][0]};
   border-radius: 6px;
   shadow-offset: 0 0;
   shadow-opacity: 0.1;
@@ -56,8 +59,8 @@ const Row = styled.View`
   justify-content: space-between;
 `;
 
-const PopupLabel = styled(Label)<{colorSchema: PrimaryColor}>`
-  color: ${props => props.theme.colors.primary[props.colorSchema][0]};
+const PopupLabel = styled(Label)<{ colorSchema: PrimaryColor }>`
+  color: ${(props) => props.theme.colors.primary[props.colorSchema][0]};
 `;
 
 const PopupButton = styled(Button)`
@@ -65,37 +68,39 @@ const PopupButton = styled(Button)`
   margin-bottom: 16px;
 `;
 
-export type ImageStatus = 'loading' | 'uploaded' | 'error';
+export type ImageStatus = "loading" | "uploaded" | "error";
 
 export const uploadImage = async (image: Image) => {
-    const imageFileType = (image.path as string).split('.').pop();
+  const imageFileType = (image.path as string).split(".").pop();
 
-    if (!['jpg', 'jpeg', 'png'].includes(imageFileType)) {
-      console.error(`Trying to upload a forbidden type of image, ${imageFileType}, allowed file types are [jpg, jpeg, png].`);
-    }
+  if (!["jpg", "jpeg", "png"].includes(imageFileType)) {
+    console.error(
+      `Trying to upload a forbidden type of image, ${imageFileType}, allowed file types are [jpg, jpeg, png].`
+    );
+  }
 
-    const data: Blob = await getBlob(image.path);
-    const filename = (image.path as string).split('/').pop();
-    const uploadResponse = await uploadFile({
-      endpoint: 'users/me/attachments',
-      fileName: filename,
-      fileType: (imageFileType as 'jpg' | 'jpeg' | 'png'),
-      data
-    });
+  const data: Blob = await getBlob(image.path);
+  const filename = (image.path as string).split("/").pop();
+  const uploadResponse = await uploadFile({
+    endpoint: "users/me/attachments",
+    fileName: filename,
+    fileType: imageFileType as "jpg" | "jpeg" | "png",
+    data,
+  });
 
-    if (uploadResponse.error) {
-      throw uploadResponse?.message;
-    }
+  if (uploadResponse.error) {
+    throw uploadResponse?.message;
+  }
 
-    image.uploadedFileName = uploadResponse.uploadedFileName;
-    image.url = uploadResponse.url;
+  image.uploadedFileName = uploadResponse.uploadedFileName;
+  image.url = uploadResponse.url;
 
   return image;
 };
 
 interface Props {
   buttonText: string;
-  value: Image[] | '';
+  value: Image[] | "";
   answers: Record<string, any>;
   onChange: (value: Record<string, any>[], id?: string) => void;
   colorSchema?: PrimaryColor;
@@ -103,11 +108,20 @@ interface Props {
   id: string;
 }
 
-const ImageUploader: React.FC<Props> = ({ buttonText, value: images, answers, onChange, colorSchema, maxImages, id }) => {
+const ImageUploader: React.FC<Props> = ({
+  buttonText,
+  value: images,
+  answers,
+  onChange,
+  colorSchema,
+  maxImages,
+  id,
+}) => {
   const [choiceModalVisible, toggleModal] = useModal();
 
   const addImagesToState = (newImages: Image[]) => {
-    const updatedImages = images === '' ? [...newImages] : [...images, ...newImages];
+    const updatedImages =
+      images === "" ? [...newImages] : [...images, ...newImages];
 
     if (updatedImages.length > 0 && updatedImages[0].questionId) {
       onChange(updatedImages, updatedImages[0].questionId);
@@ -122,14 +136,15 @@ const ImageUploader: React.FC<Props> = ({ buttonText, value: images, answers, on
     width: rawImage.width,
     height: rawImage.height,
     size: rawImage.size,
-    mime: rawImage?.filename?.split('.')?.pop() ?? rawImage.path?.split('.')?.pop()
+    mime:
+      rawImage?.filename?.split(".")?.pop() ?? rawImage.path?.split(".")?.pop(),
   });
 
   const addImagesFromLibrary = async () => {
     try {
       const rawImages = await ImagePicker.openPicker({
         multiple: true,
-        mediaType: 'photo',
+        mediaType: "photo",
         includeBase64: false,
         compressImageQuality: 0.8,
         writeTempFile: true,
@@ -140,14 +155,12 @@ const ImageUploader: React.FC<Props> = ({ buttonText, value: images, answers, on
       if (rawImages && rawImages.length > 0) {
         addImagesToState(rawImages.map(transformRawImage));
       }
-
     } catch (error) {
-      if (error?.code !== 'E_PICKER_CANCELLED')
-        console.error(error);
+      if (error?.code !== "E_PICKER_CANCELLED") console.error(error);
     }
   };
 
-  const addImageFromCamera =  async () => {
+  const addImageFromCamera = async () => {
     try {
       const rawImage = await ImagePicker.openCamera({
         includeBase64: false,
@@ -159,10 +172,8 @@ const ImageUploader: React.FC<Props> = ({ buttonText, value: images, answers, on
       if (rawImage) {
         addImagesToState([rawImage].map(transformRawImage));
       }
-
-    } catch(error) {
-      if (error?.code !== 'E_PICKER_CANCELLED')
-        console.error(error);
+    } catch (error) {
+      if (error?.code !== "E_PICKER_CANCELLED") console.error(error);
     }
   };
 
@@ -170,22 +181,39 @@ const ImageUploader: React.FC<Props> = ({ buttonText, value: images, answers, on
   return (
     <>
       <Wrapper>
-        {images !== '' && <ImageDisplay images={images} onChange={onChange} answers={answers} />}
+        {images !== "" && (
+          <ImageDisplay images={images} onChange={onChange} answers={answers} />
+        )}
         <ButtonContainer>
-          <Button colorSchema={validColorSchema} onClick={toggleModal} disabled={maxImages && images.length >= maxImages}>
+          <Button
+            colorSchema={validColorSchema}
+            onClick={toggleModal}
+            disabled={maxImages && images.length >= maxImages}
+          >
             <Icon name="add" />
-            <Text> {buttonText && buttonText !== '' ? buttonText : 'Ladda upp bild'}</Text>
+            <Text>
+              {" "}
+              {buttonText && buttonText !== "" ? buttonText : "Ladda upp bild"}
+            </Text>
           </Button>
         </ButtonContainer>
       </Wrapper>
 
-      <Modal visible={choiceModalVisible} hide={toggleModal} presentationStyle="overFullScreen" transparent animationType="fade">
+      <Modal
+        visible={choiceModalVisible}
+        hide={toggleModal}
+        presentationStyle="overFullScreen"
+        transparent
+        animationType="fade"
+      >
         <BackgroundBlur>
-          <PopupContainer colorSchema={validColorSchema} >
+          <PopupContainer colorSchema={validColorSchema}>
             <Row>
-              <PopupLabel colorSchema={validColorSchema}>Lägg till bild</PopupLabel>
+              <PopupLabel colorSchema={validColorSchema}>
+                Lägg till bild
+              </PopupLabel>
               <TouchableOpacity onPress={toggleModal} activeOpacity={1}>
-                <Icon name="clear"/>
+                <Icon name="clear" />
               </TouchableOpacity>
             </Row>
             <PopupButton
@@ -228,7 +256,7 @@ ImageUploader.propTypes = {
   /** Array of image objects to initially populate the list with (i.e. meta-data including uris) */
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
   onChange: PropTypes.func,
-  colorSchema: PropTypes.oneOf(['blue', 'red', 'green', 'purple', 'neutral']),
+  colorSchema: PropTypes.oneOf(["blue", "red", "green", "purple", "neutral"]),
 };
 
 export default ImageUploader;
