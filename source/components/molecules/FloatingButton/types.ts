40fae78ebc2d21eee48aa80b781af20294554d4e
Karl <karl.mosenbacher@knowit.se>
2022-02-17 09:01:40 +0100

    fix: types and smaller refactoring of FloatingButton component

---
+++ source/components/molecules/FloatingButton/types.ts
@@ -0,0 +1,20 @@
+import { ThemeType } from "../../../styles/themeHelpers";
+
+interface CommonProps {
+  colorSchema?: "red" | "neutral";
+  position?: "left" | "right" | "center";
+  borderRadius?: 16 | 27;
+  buttonWidth?: string | number;
+  justifyContent?: "flex-start" | "flex-end" | "center";
+}
+
+export interface StyledProps extends CommonProps {
+  theme: ThemeType;
+}
+
+export interface FloatingButtonProps extends CommonProps {
+  onPress: () => void;
+  text?: string;
+  iconName?: string;
+  iconSize?: 16 | 24 | 32 | 48;
+}
