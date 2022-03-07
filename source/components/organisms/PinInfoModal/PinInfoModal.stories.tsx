48d28a77ceec1801970a71a0ad96a5ba568fd386
Karl <karl.mosenbacher@knowit.se>
2022-03-07 10:05:12 +0100

    fix(pininfo stories): button for closing the pininfomodal

--- source/components/organisms/PinInfoModal/PinInfoModal.stories.tsx
+++ source/components/organisms/PinInfoModal/PinInfoModal.stories.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-native";
 import { storiesOf } from "@storybook/react-native";
 import styled from "styled-components/native";
 
@@ -11,12 +12,26 @@ const FlexContainer = styled.ScrollView`
   flex: 1;
 `;
 
-const OverviewExamples = () => (
-  <FlexContainer>
-    <Text type="h5">Default design</Text>
-    <PinInfoModal visible name="Karlos" pin="1234" onClose={() => true} />
-  </FlexContainer>
-);
+const OverviewExamples = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleShowModal = () => {
+    setShowModal((currentValue) => !currentValue);
+  };
+
+  return (
+    <FlexContainer>
+      <Text type="h5">Default design</Text>
+      <Button onPress={toggleShowModal} title="Show modal" />
+      <PinInfoModal
+        visible={showModal}
+        name="Karlos"
+        pin="1234"
+        onClose={toggleShowModal}
+      />
+    </FlexContainer>
+  );
+};
 
 storiesOf("PinInfoModal", module).add("Overview examples", () => (
   <OverviewExamples />
