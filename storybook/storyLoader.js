084208c8fdabc117a53ef6479b22db7a04d72f56
Karl <karl.mosenbacher@knowit.se>
2022-03-07 09:11:28 +0100

    fix: add storybook for PinInfoModal

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -47,6 +47,7 @@ function loadStories() {
   require("../source/components/organisms/BulletList/BulletList.stories");
   require("../source/components/organisms/ButtonList/ButtonList.stories");
   require("../source/components/organisms/FormList/FormList.stories");
+  require("../source/components/organisms/PinInfoModal/PinInfoModal.stories");
   require("../source/components/organisms/Step/Step.stories");
   require("../source/components/organisms/Step/StepBanner/StepBanner.stories");
   require("../source/components/organisms/Step/StepDescription/StepDescription.stories");
@@ -104,6 +105,7 @@ const stories = [
   "../source/components/organisms/BulletList/BulletList.stories",
   "../source/components/organisms/ButtonList/ButtonList.stories",
   "../source/components/organisms/FormList/FormList.stories",
+  "../source/components/organisms/PinInfoModal/PinInfoModal.stories",
   "../source/components/organisms/Step/Step.stories",
   "../source/components/organisms/Step/StepBanner/StepBanner.stories",
   "../source/components/organisms/Step/StepDescription/StepDescription.stories",
