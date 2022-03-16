d8b4b0e8900ae431ac9d6fe3ed827a08679a7162
MasterKenth <MasterKenth@users.noreply.github.com>
2022-03-16 09:57:41 +0100

    fix(caseoverview): only show pin info if waiting for sign as main applicant

--- source/screens/caseScreens/CaseOverview.tsx
+++ source/screens/caseScreens/CaseOverview.tsx
@@ -240,23 +240,21 @@ const computeCaseCardComponent = (
       } ${getSwedishMonthNameByTimeStamp(payments.payment.givedate, true)}`
     : undefined;
 
-  if (caseData.password) {
-    cardProps.description = `${cardProps.description ?? ""}pinkod: ${
-      caseData.password
-    }`;
-  }
-
   if (shouldEnterPin) {
     buttonProps.onClick = onShowPinInput;
     buttonProps.text = "Ange pinkod";
     cardProps.onClick = undefined;
   }
 
-  if (isWaitingForSign && !isCoApplicant) {
+  const shouldShowPin = isWaitingForSign && !isCoApplicant;
+
+  if (shouldShowPin) {
     const partner = persons.find((person) => person.role === "coApplicant");
     const partnerName = partner?.firstName;
     cardProps.description = `${partnerName} loggar in i appen med BankID och anger koden för att granska och signera er ansökan.\n\nKod till ${partnerName}:`;
   }
+
+  const pinToShow = shouldShowPin ? caseData.password : null;
 
   return (
     <CaseCard
@@ -281,7 +279,7 @@ const computeCaseCardComponent = (
       buttonColorScheme={buttonProps.colorSchema || colorSchema}
       completions={unApprovedCompletionDescriptions}
       completionDuedate={completionDuedate}
-      pin={caseData.password}
+      pin={pinToShow}
     />
   );
 };
