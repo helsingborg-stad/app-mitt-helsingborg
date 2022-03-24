525f6d2ec3aaca97c8e227d0268c2d601ae178b5
Karl <karl.mosenbacher@knowit.se>
2022-03-24 16:08:24 +0100

    fix(imagedisplay): fileType as type

--- source/components/molecules/ImageDisplay/ImageDisplay.tsx
+++ source/components/molecules/ImageDisplay/ImageDisplay.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
-import { Image as CropPickerImage } from 'react-native-image-crop-picker';
-import styled from 'styled-components/native';
-import HorizontalScrollIndicator from '../../atoms/HorizontalScrollIndicator';
-import ImageItem from './ImageItem';
-import { remove } from '../../../helpers/ApiRequest';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { NativeSyntheticEvent, NativeScrollEvent } from "react-native";
+import { Image as CropPickerImage } from "react-native-image-crop-picker";
+import styled from "styled-components/native";
+import HorizontalScrollIndicator from "../../atoms/HorizontalScrollIndicator";
+import ImageItem from "./ImageItem";
+import { remove } from "../../../helpers/ApiRequest";
+import { AllowedFileTypes } from "../../../helpers/FileUpload";
 
 const Wrapper = styled.View`
   padding-left: 0;
@@ -24,6 +25,7 @@ export interface Image extends CropPickerImage {
   url?: string;
   index?: number;
   questionId: string;
+  fileType: AllowedFileTypes;
 }
 
 interface Props {
@@ -33,7 +35,8 @@ interface Props {
 }
 
 const ImageDisplay: React.FC<Props> = ({ images, answers, onChange }) => {
-  const [horizontalScrollPercentage, setHorizontalScrollPercentage] = useState(0);
+  const [horizontalScrollPercentage, setHorizontalScrollPercentage] =
+    useState(0);
 
   const deleteImageFromCloudStorage = async (image: Image) => {
     remove(`users/me/attachments/${image.uploadedFileName}`);
@@ -41,7 +44,9 @@ const ImageDisplay: React.FC<Props> = ({ images, answers, onChange }) => {
   const removeImage = (image: Image) => {
     const answer: Image[] = answers[image.questionId];
     if (answer && Array.isArray(answer)) {
-      const answerWithImageRemoved = answer.filter((img) => img.path !== image.path);
+      const answerWithImageRemoved = answer.filter(
+        (img) => img.path !== image.path
+      );
       onChange(answerWithImageRemoved, image.questionId);
     }
     deleteImageFromCloudStorage(image);
@@ -49,7 +54,9 @@ const ImageDisplay: React.FC<Props> = ({ images, answers, onChange }) => {
 
   const updateImage = (image: Image) => (newImage: Image) => {
     const answer: Image[] = answers[image.questionId];
-    const index = answer.findIndex((img) => img.uploadedFileName === image.uploadedFileName);
+    const index = answer.findIndex(
+      (img) => img.uploadedFileName === image.uploadedFileName
+    );
     answer[index] = newImage;
     onChange(answer, image.questionId);
   };
@@ -57,12 +64,17 @@ const ImageDisplay: React.FC<Props> = ({ images, answers, onChange }) => {
   const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     setHorizontalScrollPercentage(
       event.nativeEvent.contentOffset.x /
-        (event.nativeEvent.contentSize.width - event.nativeEvent.layoutMeasurement.width)
+        (event.nativeEvent.contentSize.width -
+          event.nativeEvent.layoutMeasurement.width)
     );
   };
   return (
     <Wrapper>
-      <Container horizontal onScroll={handleScroll} showsHorizontalScrollIndicator={false}>
+      <Container
+        horizontal
+        onScroll={handleScroll}
+        showsHorizontalScrollIndicator={false}
+      >
         {images.length > 0 &&
           images.map((image, index) => (
             <ImageItem
@@ -75,7 +87,9 @@ const ImageDisplay: React.FC<Props> = ({ images, answers, onChange }) => {
             />
           ))}
       </Container>
-      {images.length > 2 && <HorizontalScrollIndicator percentage={horizontalScrollPercentage} />}
+      {images.length > 2 && (
+        <HorizontalScrollIndicator percentage={horizontalScrollPercentage} />
+      )}
     </Wrapper>
   );
 };
