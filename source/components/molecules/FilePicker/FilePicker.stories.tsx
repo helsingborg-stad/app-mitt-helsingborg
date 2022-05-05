f2c10b7b12547c024608f6bb7def5ed7ff68e664
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-05-05 11:54:27 +0200

    add(filepicker): add new component for picking all files

---
+++ source/components/molecules/FilePicker/FilePicker.stories.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { storiesOf } from "@storybook/react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import StoryWrapper from "../StoryWrapper";
+import FilePicker from "./FilePicker";
+
+const Component = () => {
+  const [values, setValues] = useState([]);
+  const [answers, setAnswers] = useState({});
+
+  const setAnswersAndValues = (files: any[], questionId: string) => {
+    setAnswers((answers) => ({
+      ...answers,
+      [questionId]: files,
+    }));
+
+    setValues(files);
+  };
+
+  return (
+    <FilePicker
+      id="myQuestionId"
+      preferredFileName="preferredName"
+      answers={answers}
+      buttonText="My button text"
+      colorSchema="red"
+      value={values}
+      onChange={setAnswersAndValues}
+    />
+  );
+};
+
+storiesOf("FilePicker", module).add("Default", () => (
+  <StoryWrapper>
+    <ScrollView>
+      <Component />
+    </ScrollView>
+  </StoryWrapper>
+));
---
+++ source/components/molecules/FilePicker/FilePicker.tsx
@@ -0,0 +1,182 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Text, Icon, Button } from "../../atoms";
+import { BackgroundBlurWrapper } from "../../atoms/BackgroundBlur";
+import { Modal, useModal } from "../Modal";
+import {
+  getValidColorSchema,
+  PrimaryColor,
+} from "../../../styles/themeHelpers";
+import FileDisplay, { Image } from "../FileDisplay/FileDisplay";
+import { splitFilePath } from "../../../helpers/FileUpload";
+import { Pdf } from "../PdfDisplay/PdfDisplay";
+
+import useImageUpload from "./useImageUpload";
+import usePdfUpload from "./usePdfUpload";
+
+import {
+  Wrapper,
+  ButtonContainer,
+  PopupContainer,
+  PopupButton,
+  PopupLabel,
+  Row,
+} from "./styled";
+
+export type ImageStatus = "loading" | "uploaded" | "error";
+
+interface Props {
+  buttonText: string;
+  value: (Image | Pdf)[] | "";
+  answers: Record<string, unknown>;
+  colorSchema: PrimaryColor;
+  maxImages?: number;
+  id: string;
+  preferredFileName?: string;
+  onChange: (value: (Image | Pdf)[], id?: string) => void;
+}
+
+const FilePicker: React.FC<Props> = ({
+  buttonText,
+  value: files,
+  answers,
+  onChange,
+  colorSchema,
+  maxImages: maxFiles,
+  id,
+  preferredFileName,
+}) => {
+  const [choiceModalVisible, toggleChoiceModal] = useModal();
+
+  const { addImagesFromLibrary, addImageFromCamera } = useImageUpload();
+  const { addPdfFromLibrary } = usePdfUpload();
+
+  const renameFileWithSuffix = (
+    file: Image | Pdf,
+    baseName: string,
+    ext: string,
+    suffix: string
+  ) => ({
+    ...file,
+    filename: `${baseName}_${suffix}${ext}`,
+  });
+
+  const addFilesToState = (newFiles: (Image | Pdf)[]) => {
+    let updatedFiles = files === "" ? [...newFiles] : [...files, ...newFiles];
+
+    if (preferredFileName) {
+      updatedFiles = updatedFiles.map((file, index) =>
+        renameFileWithSuffix(
+          file,
+          preferredFileName,
+          splitFilePath(file.filename).ext,
+          index.toString()
+        )
+      );
+    }
+
+    if (updatedFiles.length > 0 && updatedFiles[0].questionId) {
+      onChange(updatedFiles, updatedFiles[0].questionId);
+    }
+    return updatedFiles;
+  };
+
+  const handleUploadImageFromLibrary = async () => {
+    const selectedLibraryImages = await addImagesFromLibrary(id);
+    addFilesToState(selectedLibraryImages);
+  };
+
+  const handleUploadImageFromCamera = async () => {
+    const SelectedCameraImages = await addImageFromCamera(id);
+    addFilesToState(SelectedCameraImages);
+  };
+
+  const handleUploadPdf = async () => {
+    const selectedFiles = await addPdfFromLibrary(id);
+    addFilesToState(selectedFiles);
+  };
+
+  const showChoiceModal = (callback: () => void) => {
+    toggleChoiceModal();
+    /** There's an issue on iOS with triggering the library before the modal has closed,
+     * so as a simple fix, we add a timeout (since toggleModal is async) */
+    setTimeout(callback, 700);
+  };
+
+  const validColorSchema = getValidColorSchema(colorSchema);
+  return (
+    <>
+      <Wrapper>
+        {files !== "" && (
+          <FileDisplay files={files} onChange={onChange} answers={answers} />
+        )}
+        <ButtonContainer>
+          <Button
+            colorSchema={validColorSchema}
+            onClick={toggleChoiceModal}
+            disabled={!!maxFiles && files.length >= maxFiles}
+          >
+            <Icon name="add" />
+            <Text>{buttonText || "Ladda upp fil"}</Text>
+          </Button>
+        </ButtonContainer>
+      </Wrapper>
+
+      <Modal
+        visible={choiceModalVisible}
+        hide={toggleChoiceModal}
+        presentationStyle="overFullScreen"
+        transparent
+        animationType="fade"
+      >
+        <BackgroundBlurWrapper>
+          <PopupContainer colorSchema={validColorSchema}>
+            <Row>
+              <PopupLabel colorSchema={validColorSchema}>
+                Lägg till fil
+              </PopupLabel>
+              <TouchableOpacity onPress={toggleChoiceModal} activeOpacity={1}>
+                <Icon name="clear" />
+              </TouchableOpacity>
+            </Row>
+
+            <PopupButton
+              colorSchema={validColorSchema}
+              block
+              variant="outlined"
+              onClick={() => showChoiceModal(handleUploadPdf)}
+            >
+              <Icon name="add-photo-alternate" />
+              <Text>Filer</Text>
+            </PopupButton>
+            <Row>
+              <PopupLabel colorSchema={validColorSchema}>
+                Lägg till bild
+              </PopupLabel>
+            </Row>
+            <PopupButton
+              colorSchema={validColorSchema}
+              block
+              variant="outlined"
+              onClick={() => showChoiceModal(handleUploadImageFromCamera)}
+            >
+              <Icon name="camera-alt" />
+              <Text>Kamera</Text>
+            </PopupButton>
+            <PopupButton
+              colorSchema={validColorSchema}
+              block
+              variant="outlined"
+              onClick={() => showChoiceModal(handleUploadImageFromLibrary)}
+            >
+              <Icon name="add-photo-alternate" />
+              <Text>Bildbibliotek</Text>
+            </PopupButton>
+          </PopupContainer>
+        </BackgroundBlurWrapper>
+      </Modal>
+    </>
+  );
+};
+
+export default FilePicker;
