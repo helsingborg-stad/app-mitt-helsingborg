e4cfc3ecc7486074fd8c93096686b2670cb9d108
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-05-10 09:00:42 +0200

    fix(filepicker.test.tsx): add new test and refactoring

--- source/components/molecules/FilePicker/test/FilePicker.test.tsx
+++ source/components/molecules/FilePicker/test/FilePicker.test.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { fireEvent } from "@testing-library/react-native";
+import { fireEvent, waitFor } from "@testing-library/react-native";
 
+import { AllowedFileTypes } from "../../../../helpers/FileUpload";
 import { render } from "../../../../../test-utils";
 
 import FilePicker, { FileType } from "../FilePicker";
-import { addPdfFromLibrary } from "../pdfUpload";
-import { addImageFromCamera, addImagesFromLibrary } from "../imageUpload";
+import * as Pdf from "../pdfUpload";
+import * as Images from "../imageUpload";
 
 jest.mock("../pdfUpload");
 jest.mock("../imageUpload");
@@ -16,18 +17,31 @@ const addImagesFromCameraButtonText = "Kamera";
 const addImagesFromCameraLibraryButtonText = "Bildbibliotek";
 const mockId = "mockId";
 
-it("renders the component", async () => {
-  const { getByText, queryByText } = render(
+interface Props {
+  fileType?: FileType;
+  onChange?: () => Promise<void>;
+}
+const renderComponent = (props: Props) => {
+  const {
+    onChange = jest.fn().mockResolvedValueOnce(undefined),
+    fileType = FileType.ALL,
+  } = props;
+
+  return render(
     <FilePicker
       answers={{}}
       buttonText={componentButtonText}
       colorSchema="red"
       id={mockId}
-      fileType={FileType.ALL}
-      onChange={jest.fn().mockResolvedValueOnce(undefined)}
+      fileType={fileType}
+      onChange={onChange}
       value={[]}
     />
   );
+};
+
+it("renders the component", () => {
+  const { getByText, queryByText } = renderComponent({});
 
   expect(getByText(componentButtonText)).toHaveTextContent(componentButtonText);
   expect(queryByText(addFileButtonText)).toBeNull();
@@ -35,18 +49,8 @@ it("renders the component", async () => {
   expect(queryByText(addImagesFromCameraLibraryButtonText)).toBeNull();
 });
 
-it("opens the bottom modal on component button click", async () => {
-  const { getByText } = render(
-    <FilePicker
-      answers={{}}
-      buttonText={componentButtonText}
-      colorSchema="red"
-      id={mockId}
-      fileType={FileType.ALL}
-      onChange={jest.fn().mockRejectedValue(undefined)}
-      value={[]}
-    />
-  );
+it("opens the bottom modal on component button click", () => {
+  const { getByText } = renderComponent({});
 
   const componentButton = getByText(componentButtonText);
   fireEvent.press(componentButton);
@@ -85,17 +89,7 @@ test.each([
 ])(
   "it renders the $shownButtons buttons and hides $hiddenButtons for fileType $fileType",
   ({ fileType, shownButtons, hiddenButtons }) => {
-    const { getByText, queryByText } = render(
-      <FilePicker
-        answers={{}}
-        buttonText={componentButtonText}
-        colorSchema="red"
-        id={mockId}
-        fileType={fileType}
-        onChange={jest.fn().mockRejectedValue(undefined)}
-        value={[]}
-      />
-    );
+    const { getByText, queryByText } = renderComponent({ fileType });
 
     const componentButton = getByText(componentButtonText);
     fireEvent.press(componentButton);
@@ -110,26 +104,19 @@ test.each([
 );
 
 test.each([
-  { buttonText: addFileButtonText, callback: addPdfFromLibrary },
-  { buttonText: addImagesFromCameraButtonText, callback: addImageFromCamera },
+  { buttonText: addFileButtonText, callback: Pdf.addPdfFromLibrary },
+  {
+    buttonText: addImagesFromCameraButtonText,
+    callback: Images.addImageFromCamera,
+  },
   {
     buttonText: addImagesFromCameraLibraryButtonText,
-    callback: addImagesFromLibrary,
+    callback: Images.addImagesFromLibrary,
   },
 ])(
   "it calls the $callback callback when $buttonText button is pressed",
   ({ buttonText, callback }) => {
-    const { getByText } = render(
-      <FilePicker
-        answers={{}}
-        buttonText={componentButtonText}
-        colorSchema="red"
-        id={mockId}
-        fileType={FileType.ALL}
-        onChange={jest.fn().mockRejectedValue(undefined)}
-        value={[]}
-      />
-    );
+    const { getByText } = renderComponent({});
 
     const componentButton = getByText(componentButtonText);
     fireEvent.press(componentButton);
@@ -141,3 +128,37 @@ test.each([
     expect(callback).toHaveBeenCalledWith(mockId);
   }
 );
+
+it("calls the onChange callback with correct parameters", async () => {
+  expect.assertions(2);
+
+  const onChangeMock = jest.fn();
+  const mockUploadedPdfFile = {
+    questionId: mockId,
+    fileCopyUri: "string",
+    name: "string",
+    size: 1,
+    type: "string",
+    fileType: "pdf" as AllowedFileTypes,
+    path: "string",
+    filename: "string",
+    id: "string",
+    uri: "string",
+  };
+  jest
+    .spyOn(Pdf, "addPdfFromLibrary")
+    .mockResolvedValueOnce([mockUploadedPdfFile]);
+
+  const { getByText } = renderComponent({ onChange: onChangeMock });
+
+  const componentButton = getByText(componentButtonText);
+  fireEvent.press(componentButton);
+
+  const addFileButton = getByText(addFileButtonText);
+  fireEvent.press(addFileButton);
+
+  await waitFor(() => {
+    expect(onChangeMock).toHaveBeenCalledWith([mockUploadedPdfFile], mockId);
+    expect(onChangeMock).toHaveBeenCalledTimes(1);
+  });
+});
