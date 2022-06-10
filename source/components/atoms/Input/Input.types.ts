ea0caf053f2f7a305efb4dfde1382bb67268c1cb
Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>
2022-06-10 10:08:49 +0200

    fix(input types): add types in separate file

---
+++ source/components/atoms/Input/Input.types.ts
@@ -0,0 +1,26 @@
+import { TextInputProps } from "react-native";
+
+import { PrimaryColor, ThemeType } from "../../../styles/themeHelpers";
+
+import { InputFieldType } from "../../../types/FormTypes";
+
+export interface ErrorValidation {
+  isValid: boolean;
+  message: string;
+}
+
+export type Props = Omit<TextInputProps, "onBlur"> & {
+  onBlur: (value: string) => void;
+  onMount: (value: string) => void;
+  center?: boolean;
+  transparent?: boolean;
+  colorSchema: PrimaryColor;
+  showErrorMessage?: boolean;
+  hidden?: boolean;
+  error?: ErrorValidation;
+  textAlign?: "left" | "center" | "right";
+  inputType?: InputFieldType;
+  theme: ThemeType;
+};
+
+export type KeyboardTypeExtraPropType = Record<InputFieldType, Partial<Props>>;
