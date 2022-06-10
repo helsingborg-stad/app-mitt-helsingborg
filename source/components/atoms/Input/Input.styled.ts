f552a1de335f78f2e1147e815cc3f8b758df2b0a
Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>
2022-06-10 10:09:25 +0200

    fix(input styled): add styled file for input

---
+++ source/components/atoms/Input/Input.styled.ts
@@ -0,0 +1,43 @@
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+
+import { Text } from "..";
+
+import { Props } from "./Input.types";
+
+const StyledTextInput = styled.TextInput<Props>`
+  width: 100%;
+  font-weight: ${({ theme }) => theme.fontWeights[0]};
+  background-color: ${({ theme, colorSchema }) =>
+    theme.colors.complementary[colorSchema][2]};
+  ${({ transparent }) => transparent && `background-color: transparent;`}
+  border-radius: 4.5px;
+  border: solid 1px
+    ${({ theme, error }) =>
+      error?.isValid || error === undefined
+        ? "transparent"
+        : theme.colors.primary.red[0]};
+  padding: 16px;
+  color: ${({ theme }) => theme.colors.neutrals[0]};
+  ${({ center }) => (center ? "text-align: center;" : null)}
+  ${({ hidden }) => (hidden ? "display: none;" : null)}
+`;
+
+const StyledErrorText = styled(Text)`
+  font-size: ${({ theme }) => theme.fontSizes[3]}px;
+  color: ${({ theme }) => theme.textInput.errorTextColor};
+  font-weight: ${({ theme }) => theme.fontWeights[0]};
+  padding-top: 8px;
+`;
+
+const AccesoryViewChild = styled.View`
+  width: ${Dimensions.get("window").width}px;
+  height: 48px;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: #f8f8f8;
+  padding-right: 16px;
+`;
+
+export { StyledTextInput, StyledErrorText, AccesoryViewChild };
