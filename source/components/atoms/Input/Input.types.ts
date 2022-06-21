37e6d46c4ed8eb88632d2e91591595104425090e
Karl Mösenbacher <karlmosenbacher@hotmail.com>
2022-06-21 11:27:05 +0200

    Feat/refactor done button ios keyboard (#708)
    * fix(input types): add types in separate file
    
    * fix(input styled): add styled file for input
    
    * fix(input): refactor input component
    
    * fix(input): use forwardref
    
    * fix(input): use dismiss function reference on button onclick
    
    * fix(input styled): create interface of properties that is actually used
    
    * fix(input): default colorschema to blue as before
    
    * fix(input types): make some properties optional
    
    * fix(input.styled): faulty import during tests
    
    * fix(input.types): make properties optional in type
    
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>

---
+++ source/components/atoms/Input/Input.types.ts
@@ -0,0 +1,28 @@
+import { TextInputProps } from "react-native";
+
+import { PrimaryColor, ThemeType } from "../../../styles/themeHelpers";
+
+import { InputFieldType } from "../../../types/FormTypes";
+
+export interface ErrorValidation {
+  isValid: boolean;
+  message: string;
+}
+
+export type Props = Omit<TextInputProps, "onBlur"> & {
+  onBlur?: (value: string) => void;
+  onMount?: (value: string) => void;
+  center?: boolean;
+  transparent?: boolean;
+  colorSchema?: PrimaryColor;
+  showErrorMessage?: boolean;
+  hidden?: boolean;
+  error?: ErrorValidation;
+  textAlign?: "left" | "center" | "right";
+  inputType?: InputFieldType;
+  theme?: ThemeType;
+};
+
+export type KeyboardTypeExtraPropType = Partial<
+  Record<InputFieldType, Partial<Props>>
+>;
