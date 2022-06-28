1a1e632de72742ca487f77d0674b6dd886086d5f
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-06-28 13:18:00 +0200

    fix(sentryservice): read from correct env variable

--- source/services/SentryService.ts
+++ source/services/SentryService.ts
@@ -1,13 +1,14 @@
 import * as Sentry from "@sentry/react-native";
+import env from "react-native-config";
 
 import type { MonitoringService } from "./MonitoringService";
 
 const sentryService: MonitoringService = {
   init: () => {
     Sentry.init({
-      dsn: "http://16bfc3040f8749598b864a3569df8a05@13.49.35.65:9000/2",
+      dsn: env.SENTRY_DSN,
       // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-      // We recommend adjusting this value in production.
+      // Recommended is to adjust this value in production.
       tracesSampleRate: 0.75,
     });
   },
