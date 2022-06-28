bc8c903efc358f82e7260374357b980908fbe236
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-06-28 14:02:29 +0200

    fix(monitoringservice.test.ts): add tests

---
+++ source/services/test/MonitoringService.test.ts
@@ -0,0 +1,35 @@
+import env from "react-native-config";
+import * as Sentry from "@sentry/react-native";
+
+import { getMonitoringService } from "../MonitoringService";
+
+jest.mock("@sentry/react-native", () => ({
+  init: jest.fn(),
+}));
+
+const dsnMock = "test";
+
+it("returns a null service in development environment", () => {
+  env.APP_ENV = "develop";
+  const sentrySpy = jest.spyOn(Sentry, "init");
+
+  const service = getMonitoringService();
+  service.init();
+
+  expect(sentrySpy).not.toHaveBeenCalled();
+});
+
+it("returns sentry service in production environment", () => {
+  env.APP_ENV = "production";
+  env.SENTRY_DSN = dsnMock;
+
+  const sentrySpy = jest.spyOn(Sentry, "init");
+
+  const service = getMonitoringService();
+  service.init();
+
+  expect(sentrySpy).toHaveBeenCalledWith({
+    dsn: dsnMock,
+    tracesSampleRate: 0.75,
+  });
+});
