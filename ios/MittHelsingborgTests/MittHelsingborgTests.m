e00a931a00ff7be08fcf78ababc6c8e903a306bf
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-07-11 13:39:19 +0200

    fix(ios): update ios files for react-native (2/3)

--- ios/MittHelsingborgTests/MittHelsingborgTests.m
+++ ios/MittHelsingborgTests/MittHelsingborgTests.m
@@ -13,7 +13,7 @@
 
 @implementation MittHelsingborgTests
 
-- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
   if (test(view)) {
     return YES;
@@ -34,11 +34,12 @@
 
   __block NSString *redboxError = nil;
 #ifdef DEBUG
-  RCTSetLogFunction(^(RCTLogLevel level, RCTLogSource source, NSString *fileName, NSNumber *lineNumber, NSString *message) {
-    if (level >= RCTLogLevelError) {
-      redboxError = message;
-    }
-  });
+  RCTSetLogFunction(
+      ^(RCTLogLevel level, RCTLogSource source, NSString *fileName, NSNumber *lineNumber, NSString *message) {
+        if (level >= RCTLogLevelError) {
+          redboxError = message;
+        }
+      });
 #endif
 
   while ([date timeIntervalSinceNow] > 0 && !foundElement && !redboxError) {
