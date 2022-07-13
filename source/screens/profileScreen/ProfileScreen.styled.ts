ff259344c53f9221e951ce4bc5e188218816c2bd
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-07-13 13:32:16 +0200

    refactor(profilescreen): refactor profile screen

---
+++ source/screens/profileScreen/ProfileScreen.styled.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components/native";
+
+import { ScreenWrapper } from "../../components/molecules";
+import { Button, Text, Heading, Label } from "../../components/atoms";
+
+const ProfileScreenWrapper = styled(ScreenWrapper)`
+  padding: 0;
+`;
+
+const Container = styled.ScrollView`
+  flex: 1;
+  padding: 0px 16px;
+`;
+
+const BottomContainer = styled.View`
+  margin-top: 32px;
+  margin-bottom: 16px;
+  flex: 1;
+  justify-content: flex-end;
+  padding-bottom: 16px;
+`;
+
+const SignOutButton = styled(Button)`
+  margin-bottom: 16px;
+`;
+
+const ProfileInfoContainer = styled.View`
+  margin: 16px 0px;
+`;
+
+const ProfileInfoHeading = styled(Heading)`
+  color: ${(props) => props.theme.text.darkest};
+  margin-top: 16px;
+  margin-bottom: 0px;
+`;
+
+const ProfileInfoText = styled(Text)`
+  font-size: 16px;
+  color: ${(props) => props.theme.text.darkest};
+  font-weight: normal;
+`;
+
+const ProfileLabel = styled(Label)`
+  font-size: 12px;
+  margin-top: 16px;
+  margin-bottom: 0px;
+  font-weight: ${(props) => props.theme.fontWeights[1]};
+  color: ${(props) => props.theme.text.blue[4]};
+`;
+
+export {
+  ProfileScreenWrapper,
+  Container,
+  BottomContainer,
+  SignOutButton,
+  ProfileInfoContainer,
+  ProfileInfoHeading,
+  ProfileInfoText,
+  ProfileLabel,
+};
