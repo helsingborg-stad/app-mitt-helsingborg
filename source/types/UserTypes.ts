3bc608ddb027fde5ffe572127c7f4197deba7b8b
Sven65 <Sven65@users.noreply.github.com>
2022-07-13 09:23:10 +0200

    Make personalNumber not optional

--- source/types/UserTypes.ts
+++ source/types/UserTypes.ts
@@ -10,7 +10,7 @@ export interface User {
   email: string;
   civilStatus: string; // might not actually be a string...
   address: Address;
-  personalNumber?: string;
+  personalNumber: string;
 }
 
 export interface PartnerInfo {
