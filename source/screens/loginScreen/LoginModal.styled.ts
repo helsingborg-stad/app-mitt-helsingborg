ed32da23d5bc765d69b27b8881265cca43c71c05
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-07-13 15:28:22 +0200

    fix(loginmodal.styled.ts): use interface for separator

--- source/screens/loginScreen/LoginModal.styled.ts
+++ source/screens/loginScreen/LoginModal.styled.ts
@@ -4,6 +4,8 @@ import { Modal as ModalBase, BackNavigation } from "../../components/molecules";
 
 import { Heading, Input, Text } from "../../components/atoms";
 
+import type { ThemeType } from "../../styles/themeHelpers";
+
 const UnifiedPadding = [24, 48];
 
 const FlexView = styled.View`
@@ -24,37 +26,40 @@ const Form = styled.View`
 `;
 
 const Title = styled(Heading)`
-  font-size: ${(props) => props.theme.fontSizes[3]}px;
-  color: ${(props) => props.theme.colors.primary.red[0]};
-  font-weight: ${(props) => props.theme.fontWeights[1]};
+  font-size: ${({ theme }) => theme.fontSizes[3]}px;
+  color: ${({ theme }) => theme.colors.primary.red[0]};
+  font-weight: ${({ theme }) => theme.fontWeights[1]};
 `;
 
 const ModalHeading = styled(Heading)`
-  font-size: ${(props) => props.theme.fontSizes[9]}px;
-  font-weight: ${(props) => props.theme.fontWeights[1]};
+  font-size: ${({ theme }) => theme.fontSizes[9]}px;
+  font-weight: ${({ theme }) => theme.fontWeights[1]};
   line-height: 44px;
-  color: ${(props) => props.theme.colors.neutrals[0]};
+  color: ${({ theme }) => theme.colors.neutrals[0]};
 `;
 
 const ContentText = styled(Text)`
-  font-size: ${(props) => props.theme.fontSizes[4]}px;
+  font-size: ${({ theme }) => theme.fontSizes[4]}px;
   line-height: 30px;
 `;
 
-const Separator = styled.View`
+interface SeparatorProps {
+  theme: ThemeType;
+}
+const Separator = styled.View<SeparatorProps>`
   border-radius: 40px;
   height: 2px;
   width: 25px;
-  background-color: ${(props) => props.theme.colors.complementary.red[0]};
+  background-color: ${({ theme }) => theme.colors.complementary.red[0]};
   margin-bottom: 16px;
 `;
 
 const Modal = styled(ModalBase)`
-  background-color: ${(props) => props.theme.colors.neutrals[6]};
+  background-color: ${({ theme }) => theme.colors.neutrals[6]};
 `;
 
 const Link = styled(Text)`
-  font-size: ${(props) => props.theme.fontSizes[3]}px;
+  font-size: ${({ theme }) => theme.fontSizes[3]}px;
   text-align: center;
   margin-top: 16px;
   font-weight: normal;
