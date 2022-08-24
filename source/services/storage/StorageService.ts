72c068014633ffc1fdc88d08f2357f9572b99469
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-08-24 11:39:01 +0200

    fix(storageservice.ts): remove export keyword

--- source/services/storage/StorageService.ts
+++ source/services/storage/StorageService.ts
@@ -7,7 +7,7 @@
 import { Component } from "react";
 import AsyncStorage from "@react-native-community/async-storage";
 
-export interface IStorage {
+interface IStorage {
   getData(key: string): Promise<string | null>;
   saveData(key: string, payload: string): Promise<void>;
 }
