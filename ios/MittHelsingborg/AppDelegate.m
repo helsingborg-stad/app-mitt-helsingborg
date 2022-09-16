12e0cf8c679e43d7b0a60d96cb42b5bb33508e52
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-09-16 12:39:12 +0200

    refactor(appdelegate) remove uncomment code

--- ios/MittHelsingborg/AppDelegate.m
+++ ios/MittHelsingborg/AppDelegate.m
@@ -38,13 +38,13 @@
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
-// #if RCT_NEW_ARCH_ENABLED
-//   _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
-//   _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
-//   _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
-//   _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
-//   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
-// #endif
+#if RCT_NEW_ARCH_ENABLED
+  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
+  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
+  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
+  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
+  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
+#endif
 
   UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MittHelsingborg", nil);
 
