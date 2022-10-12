fd0976e2eb5fb600096677ed93da6c7437483129
MrSpex <mail@dannilsson.se>
2022-10-12 10:28:55 +0200

    test: (completions duedate) fix failing

--- source/containers/Form/hooks/test/textReplacement.test.ts
+++ source/containers/Form/hooks/test/textReplacement.test.ts
@@ -302,7 +302,7 @@ describe("replaceCaseItemText", () => {
   });
 
   it("replaces text on multiple `to` properties with multiple keys", () => {
-    const expectedDescription = `Month: January, duedate: ${mockCase.details.workflow.application.completionduedate}`;
+    const expectedDescription = "Month: January, duedate: 2022-05-16";
 
     mockCase.status.detailedDescription =
       "Month: #MONTH_NAME, duedate: #COMPLETION_DUEDATE";
