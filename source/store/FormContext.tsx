d5d46bf8566ceb2810f2ef11224e86761b0d7604
Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>
2022-11-25 14:31:41 +0100

    fix(formcontext.tsx): throw is request error exist

--- source/store/FormContext.tsx
+++ source/store/FormContext.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import type { Form } from "../types/FormTypes";
-import { get } from "../helpers/ApiRequest";
+import { get, isRequestError } from "../helpers/ApiRequest";
 
 interface FormContextValue {
   forms: Record<string, Form>;
@@ -41,24 +41,21 @@ export function FormProvider({ children }: FormProviderProps): JSX.Element {
   }, [formSummaries]);
 
   const getForm = useCallback(
-    async (id: string): Promise<Form | null> => {
+    async (id: string): Promise<Form> => {
       if (Object.keys(forms).includes(id)) {
         return forms[id];
       }
 
-      try {
-        const res = await get(`/forms/${id}`);
-        if (res && res.data) {
-          const newForm = res.data.data;
-          setForms((oldForms) => ({ ...oldForms, [newForm.id]: newForm }));
-          return newForm;
-        }
-        console.log("Form data not found");
-      } catch (error) {
-        console.error(error);
+      const res = await get<Form>(`/forms/${id}`);
+
+      if (isRequestError(res)) {
+        console.error("Could not fetch form:", res.message);
+        throw new Error(res.message);
       }
 
-      return null;
+      const newForm = res.data.data;
+      setForms((oldForms) => ({ ...oldForms, [newForm.id]: newForm }));
+      return newForm;
     },
     [forms]
   );
