1d1f05a1ec1e256bdf2fb37997a7a58284b5f283
MrSpex <mail@dannilsson.se>
2022-11-25 13:34:46 +0100

    test: fix

--- source/components/molecules/ApiStatusMessage/ApiStatusMessage.test.tsx
+++ source/components/molecules/ApiStatusMessage/ApiStatusMessage.test.tsx
@@ -3,17 +3,34 @@ import React from "react";
 import { render } from "../../../../test-utils";
 
 import ApiStatusMessage from "./ApiStatusMessage";
+import type { Message } from "../../../types/StatusMessages";
+import { Type } from "../../../types/StatusMessages";
 
 it("renders without crashing", () => {
-  const component = () => render(<ApiStatusMessage message="" />);
+  const component = () =>
+    render(
+      <ApiStatusMessage
+        message={{
+          title: "title",
+          text: "text",
+        }}
+        type={Type.Info}
+      />
+    );
 
   expect(component).not.toThrow();
 });
 
 it("shows the provided message", () => {
-  const apiStatusMessage = "testMessage";
+  const apiStatusMessage: Message = {
+    title: "title",
+    text: "text",
+  };
 
-  const { getByText } = render(<ApiStatusMessage message={apiStatusMessage} />);
+  const { getByText } = render(
+    <ApiStatusMessage message={apiStatusMessage} type={Type.Info} />
+  );
 
-  expect(getByText(apiStatusMessage)).not.toBeNull();
+  expect(getByText(apiStatusMessage.title)).not.toBeNull();
+  expect(getByText(apiStatusMessage.text)).not.toBeNull();
 });
