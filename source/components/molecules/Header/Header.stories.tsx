2b656167f739ca65dd677ab53418ff0e65ebdc1d
Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>
2022-12-29 16:21:40 +0100

    refactor(header.stories): refactor header stories

--- source/components/molecules/Header/Header.stories.tsx
+++ source/components/molecules/Header/Header.stories.tsx
@@ -1,43 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
+import { storiesOf } from "@storybook/react-native";
 import { action } from "@storybook/addon-actions";
 import Header from "./Header";
 import StoryWrapper from "../StoryWrapper";
 
-export default {
-  component: Header,
-  title: "Header",
-};
-
-export const plainHeader = () => (
-  <StoryWrapper>
-    <Header />
-  </StoryWrapper>
-);
-export const headerWithmessage = () => (
-  <StoryWrapper>
-    <Header message="Header Message" />
-  </StoryWrapper>
-);
-export const headerWithTitle = () => (
-  <StoryWrapper>
-    <Header message="Header Message" title="Header Title" />
-  </StoryWrapper>
+const PlainHeader = () => <Header />;
+const HeaderWithmessage = () => <Header message="Header Message" />;
+const HeaderWithTitle = () => (
+  <Header message="Header Message" title="Header Title" />
 );
 
-export const headerWithBackButton = () => (
-  <StoryWrapper>
-    <Header
-      message="Header Message"
-      title="Cool"
-      backButton={{ text: "Go back", onClick: action("clicked back button") }}
-    />
-  </StoryWrapper>
+const HeaderWithBackButton = () => (
+  <Header
+    message="Header Message"
+    title="Cool"
+    backButton={{ text: "Go back", onClick: action("clicked back button") }}
+  />
 );
 
-export const headerTitleColor = () => (
-  <StoryWrapper>
-    <Header themeColor="green" message="Header message" title="Header Title" />
-  </StoryWrapper>
+const HeaderTitleColor = () => (
+  <Header themeColor="green" message="Header message" title="Header Title" />
 );
 
 const navItems = [
@@ -53,8 +36,40 @@ const navItems = [
   },
 ];
 
-export const headerWithNavigation = () => (
+const HeaderWithNavigation = () => (
   <StoryWrapper>
     <Header message="Header Message" title="Header Title" navItems={navItems} />
   </StoryWrapper>
 );
+
+storiesOf("Header", module)
+  .add("Plain header", (props) => (
+    <StoryWrapper {...props}>
+      <PlainHeader />
+    </StoryWrapper>
+  ))
+  .add("Header with message", (props) => (
+    <StoryWrapper {...props}>
+      <HeaderWithmessage />
+    </StoryWrapper>
+  ))
+  .add("Header title color", (props) => (
+    <StoryWrapper {...props}>
+      <HeaderTitleColor />
+    </StoryWrapper>
+  ))
+  .add("Header with title", (props) => (
+    <StoryWrapper {...props}>
+      <HeaderWithTitle />
+    </StoryWrapper>
+  ))
+  .add("Header with back button", (props) => (
+    <StoryWrapper {...props}>
+      <HeaderWithBackButton />
+    </StoryWrapper>
+  ))
+  .add("Header with navigation", (props) => (
+    <StoryWrapper {...props}>
+      <HeaderWithNavigation />
+    </StoryWrapper>
+  ));
