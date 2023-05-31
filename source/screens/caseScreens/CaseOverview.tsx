81395bf0eefa310af15e961e9fb4de84485062a9
MasterKenth <MasterKenth@users.noreply.github.com>
2023-05-31 16:10:00 +0200

    Feat/sort case cards (#862)
    * ✨ Feat(caseoverview): sort case cards by createdAt date (desc)
    
    * 🐛 Bug(caseoverview): sort by period endDate

--- source/screens/caseScreens/CaseOverview.tsx
+++ source/screens/caseScreens/CaseOverview.tsx
@@ -516,33 +516,43 @@ function CaseOverview(props: CaseOverviewProps): JSX.Element {
     }
   };
 
-  const activeCaseCards = activeCases.map((caseData) =>
-    computeCaseCardComponent(
-      {
-        caseItem: caseData,
-        formPassword: passwords[caseData.id] ?? undefined,
-      },
-      { onOpenForm: openForm, onOpenCaseSummary: openCaseSummary },
-      user?.personalNumber,
-      () => openPinInputModal(caseData),
-      () => setShowPdfForCase(caseData.id),
-      () => handleRemoveCase(caseData.id)
+  const activeCaseCards = activeCases
+    .sort(
+      (caseA, caseB) =>
+        caseB.details.period.endDate - caseA.details.period.endDate
     )
-  );
+    .map((caseData) =>
+      computeCaseCardComponent(
+        {
+          caseItem: caseData,
+          formPassword: passwords[caseData.id] ?? undefined,
+        },
+        { onOpenForm: openForm, onOpenCaseSummary: openCaseSummary },
+        user?.personalNumber,
+        () => openPinInputModal(caseData),
+        () => setShowPdfForCase(caseData.id),
+        () => handleRemoveCase(caseData.id)
+      )
+    );
 
-  const closedCaseCards = closedCases.map((caseData) =>
-    computeCaseCardComponent(
-      {
-        caseItem: caseData,
-        formPassword: passwords[caseData.id] ?? undefined,
-      },
-      { onOpenForm: openForm, onOpenCaseSummary: openCaseSummary },
-      user?.personalNumber,
-      () => openPinInputModal(caseData),
-      () => setShowPdfForCase(caseData.id),
-      () => undefined
+  const closedCaseCards = closedCases
+    .sort(
+      (caseA, caseB) =>
+        caseB.details.period.endDate - caseA.details.period.endDate
     )
-  );
+    .map((caseData) =>
+      computeCaseCardComponent(
+        {
+          caseItem: caseData,
+          formPassword: passwords[caseData.id] ?? undefined,
+        },
+        { onOpenForm: openForm, onOpenCaseSummary: openCaseSummary },
+        user?.personalNumber,
+        () => openPinInputModal(caseData),
+        () => setShowPdfForCase(caseData.id),
+        () => undefined
+      )
+    );
 
   const hidePdfModal = () => {
     setShowPdfForCase("");
